7ea59d5bafbb92a22959a7c70ec072f8366bc69c
Emil Bay <github@tixz.dk>
2023-01-06 09:51:12 +0100

    fix: resolve ambient web extension types

--- libs/wallet-service/src/storage/ext-local.ts
+++ libs/wallet-service/src/storage/ext-local.ts
@@ -1,6 +1,6 @@
 import { Serializable } from './types/json'
 
-const storage = (browser ?? chrome)?.storage?.local
+const storage = (globalThis?.browser ?? globalThis?.chrome)?.storage?.local
 
 // Based on https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/storage/StorageArea
 export default class ExtLocalMap<V extends Serializable> {

a835a544d277297c198ca457ecece2aa8b080b57
Emil Bay <github@tixz.dk>
2023-03-23 12:47:53 +0000

    Implement smart node selection (closes #81)

--- web-extension/common/backend/node-rpc.js
+++ web-extension/common/backend/node-rpc.js
@@ -25,16 +25,103 @@ export default class NodeRPC {
    *
    * @param {URL[]} nodeUrls
    */
-  constructor (nodeUrls) {
-    if (typeof nodeUrls === 'string') nodeUrls = [nodeUrls]
+  constructor (nodeUrls, maxDrift = 2) {
+    if (!Array.isArray(nodeUrls)) nodeUrls = [nodeUrls]
 
     assert(nodeUrls.every(u => u instanceof URL), 'nodeUrls must be WHATWG URLs')
 
     this.urls = nodeUrls
+    this.maxDrift = maxDrift
   }
 
+  /**
+   * Find a healthy Vega node. Algorithm:
+   * 1. Request `/blockchain/height` from all nodes
+   * 2. Remove nodes that:
+   *  - Don't reply
+   *  - Return a HTTP status code outside 2xx
+   *  - Have a difference between their core block height and data node block height larger than `maxDrift`
+   * 3. Group nodes into buckets of 3 blocks within each other
+   * 4. Pick a random node from the largest bucket
+   *
+   * @returns URL
+   */
   async _findHealthyNode () {
-    return this.urls.at(0)
+    const nodesHeights = await promiseAllResolved(this.urls.map(async u => {
+      const res = await fetch(new URL('/blockchain/height', u), {
+        headers: {
+          Accept: 'application/json'
+        }
+      })
+
+      if (res.ok === false) throw new Error('Failed request')
+
+      const { height } = await res.json()
+
+      const coreHeight = BigInt(height)
+      // The header is not set for talking to core nodes
+      const nodeHeight = BigInt(res.headers.get('x-block-height') ?? coreHeight)
+
+      const drift = coreHeight - nodeHeight
+      // eslint-disable-next-line yoda
+      if (-this.maxDrift > drift || drift > this.maxDrift) throw new Error('Block drift too high')
+
+      return [u, nodeHeight]
+    }))
+
+    const maxHeight = nodesHeights.reduce((m, [_, height]) => bigintMax(m, height), 0n)
+
+    const groups = group(nodesHeights, ([node, height]) => {
+      const key = (maxHeight - height) / 3n // Group into buckets of 3 blocks
+      return [key, node]
+    })
+
+    const largestGroup = findLargest(groups)
+
+    if (largestGroup.length === 0) throw new Error('No healthy node found')
+
+    return pickRandom(largestGroup)
+
+    // Math.max does not work with bigint
+    /**
+     *
+     * @param {bigint} a
+     * @param {bigint} b
+     * @returns bigint
+     */
+    function bigintMax (a, b) {
+      return a > b ? a : b
+    }
+
+    function promiseAllResolved (promises) {
+      return Promise.allSettled(promises).then(results => {
+        return results
+          .filter(({ status }) => status === 'fulfilled')
+          .map(({ value }) => value)
+      })
+    }
+
+    function group (values, fn) {
+      const groups = values.reduce((map, val) => {
+        const [key, value] = fn(val)
+
+        const list = map.get(key) ?? []
+        list.push(value)
+        map.set(key, list)
+
+        return map
+      }, new Map())
+
+      return Array.from(groups.values())
+    }
+
+    function findLargest (arr) {
+      return arr.reduce((largest, group) => group.length > largest.length ? group : largest, [])
+    }
+
+    function pickRandom (arr) {
+      return arr[arr.length * Math.random() | 0]
+    }
   }
 
   async blockchainHeight () {

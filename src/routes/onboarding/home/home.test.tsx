7d8429e1dd97786bd258d5cc934d8c274ba1cb58
Dexter <dexter.edwards93@gmail.com>
2023-04-21 14:44:28 +0100

    test: remove broken test files

--- src/routes/onboarding/home/home.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react";
-import { OnboardingHome } from ".";
-
-describe("Onboarding home", () => {
-  it("should render", () => {
-    render(<OnboardingHome />);
-  });
-});

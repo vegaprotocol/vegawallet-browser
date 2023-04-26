f320f4b0dc557a3834b3c9fd556c3d0a7e125ed3
Dexter <dexter.edwards93@gmail.com>
2023-04-26 14:15:48 +0100

    test: polyfil resize observer

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+
+class ResizeObserver {
+  observe() {}
+  unobserve() {}
+}
+// @ts-ignore
+window.ResizeObserver = ResizeObserver

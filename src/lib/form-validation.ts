09c78a773cc5394600d660e7b995a0c62dcbd7b9
Dexter <dexter.edwards93@gmail.com>
2023-04-26 13:45:34 +0100

    feat: build create password page

---
+++ src/lib/form-validation.ts
@@ -0,0 +1,19 @@
+// export const Validation = {
+//   REQUIRED: 'Required',
+
+//   // Patterns
+//   URL: {
+//     value:
+//       // Complains about unnecessary escape character but I don't want to mess with it as
+//       // its copied from Stack Overflow
+//       // eslint-disable-next-line
+//       /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/i,
+//     message: 'Invalid URL'
+//   },
+
+//   // Custom logic
+//   match: (value: string | number | boolean) => ({
+//     message: 'Password does not match',
+//     value: new RegExp(`^${value}$`)
+//   })
+// }
---
+++ src/routes/onboarding/create-password/index.tsx
@@ -0,0 +1,102 @@
+import { Button, FormGroup, Input, InputError } from '@vegaprotocol/ui-toolkit'
+import { Page } from '../../../components/page'
+import { useForm, useWatch } from 'react-hook-form'
+import { Validation } from '../../../lib/form-validation'
+import { useCallback, useEffect } from 'react'
+import { Checkbox } from '../../../components/checkbox'
+import { useNavigate } from 'react-router-dom'
+import { FULL_ROUTES } from '../..'
+
+interface FormFields {
+  password: string
+  confirmPassword: string
+  acceptedTerms: boolean
+}
+
+export const CreatePassword = () => {
+  const {
+    control,
+    register,
+    handleSubmit,
+    setFocus,
+    formState: { errors }
+  } = useForm<FormFields>()
+  const navigate = useNavigate()
+  const password = useWatch({ control, name: 'password' })
+  const acceptedTerms = useWatch({ control, name: 'acceptedTerms' })
+  const submit = useCallback(
+    (fields: { confirmPassword: string; password: string }) => {
+      console.log(fields)
+      navigate(FULL_ROUTES.createWallet)
+    },
+    [navigate]
+  )
+
+  useEffect(() => {
+    setFocus('password')
+  }, [setFocus])
+
+  return (
+    <Page name="Create Password" backLocation={FULL_ROUTES.getStarted}>
+      <>
+        <p className="mb-6">
+          Set a password to protect and unlock your Vega Wallet. Your password
+          can't be recovered or used to recover a wallet.
+        </p>
+        <form onSubmit={handleSubmit(submit)}>
+          <FormGroup label="Password" labelFor="password" className="mb-6">
+            <Input
+              hasError={!!errors.password?.message}
+              placeholder="Enter a password"
+              data-testid="create-wallet-form-password"
+              type="password"
+              {...register('password', { required: Validation.REQUIRED })}
+            />
+            {errors.password?.message && (
+              <InputError forInput="password">
+                {errors.password.message}
+              </InputError>
+            )}
+          </FormGroup>
+          <FormGroup
+            label="Confirm password"
+            labelFor="confirmPassword"
+            className="mb-6"
+          >
+            <Input
+              hasError={!!errors.confirmPassword?.message}
+              placeholder="Enter password again"
+              data-testid="create-wallet-form-password-confirm"
+              type="password"
+              {...register('confirmPassword', {
+                required: Validation.REQUIRED,
+                pattern: Validation.match(password)
+              })}
+            />
+            {errors.confirmPassword?.message && (
+              <InputError forInput="confirmPassword">
+                {errors.confirmPassword.message}
+              </InputError>
+            )}
+          </FormGroup>
+          <Checkbox
+            name="acceptedTerms"
+            label="I understand that Vega Wallet cannot recover this password if I lose it"
+            control={control}
+          />
+          <Button
+            fill={true}
+            className="mt-8"
+            variant="primary"
+            type="submit"
+            disabled={
+              Boolean(errors.confirmPassword?.message) || !acceptedTerms
+            }
+          >
+            Submit
+          </Button>
+        </form>
+      </>
+    </Page>
+  )
+}

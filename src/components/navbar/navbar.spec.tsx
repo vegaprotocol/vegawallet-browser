6e74c5a69a9b4eaf27a66824b0d4da3989e16d07
Dexter <dexter.edwards93@gmail.com>
2023-04-27 13:50:02 +0100

    feat: build nav bar

---
+++ src/components/navbar/navbar.spec.tsx
@@ -0,0 +1,95 @@
+import { render, screen } from '@testing-library/react'
+import type { NavButtonProps } from '.'
+import { NavButton, NavBar } from '.'
+import { MemoryRouter } from 'react-router-dom'
+import type { HTMLAttributes } from 'react'
+
+jest.mock('@vegaprotocol/ui-toolkit', () => ({
+  Button: (props: HTMLAttributes<HTMLButtonElement>) => <button {...props} />
+}))
+
+const renderNavButton = (
+  props: NavButtonProps,
+  initialEntries: string[] = []
+) =>
+  render(
+    <MemoryRouter initialEntries={initialEntries}>
+      <NavButton {...props} />
+    </MemoryRouter>
+  )
+
+const renderNav = ({ isFairground }: { isFairground: boolean }) =>
+  render(
+    <MemoryRouter>
+      <NavBar isFairground={isFairground} />
+    </MemoryRouter>
+  )
+
+// eslint-disable-next-line jest/no-disabled-tests
+describe('NavButton', () => {
+  it('renders with text and icon', () => {
+    const icon = <svg data-testid="test-icon" />
+    renderNavButton(
+      {
+        icon: icon,
+        text: 'Test Button',
+        to: '/',
+        end: false,
+        isFairground: false
+      },
+      ['/foo']
+    )
+
+    expect(screen.getByTestId('nav-button')).toBeInTheDocument()
+    expect(screen.getByTestId('test-icon')).toBeInTheDocument()
+    expect(screen.getByText('Test Button')).toBeInTheDocument()
+    expect(screen.getByTestId('link-active')).not.toHaveClass('bg-vega-yellow')
+  })
+
+  it('renders with active link styles when active', () => {
+    const icon = <svg data-testid="test-icon" />
+    renderNavButton(
+      {
+        icon: icon,
+        text: 'Test Button',
+        to: '/settings',
+        end: false,
+        isFairground: false
+      },
+      ['/settings']
+    )
+
+    expect(screen.getByTestId('link-active')).toHaveClass('bg-vega-yellow')
+  })
+
+  it('changes style when in fairground mode', () => {
+    const icon = <svg data-testid="test-icon" />
+    renderNavButton(
+      {
+        icon: icon,
+        text: 'Test Button',
+        to: '/settings',
+        end: false,
+        isFairground: true
+      },
+      ['/settings']
+    )
+
+    expect(screen.getByTestId('link-active')).toHaveClass('bg-black')
+  })
+})
+
+// eslint-disable-next-line jest/no-disabled-tests
+describe('NavBar', () => {
+  it('renders with all three NavButtons', () => {
+    renderNav({ isFairground: false })
+    expect(screen.getByTestId('nav-bar')).toBeInTheDocument()
+    expect(screen.getByTestId('nav-bar')).toHaveClass('bg-black')
+    expect(screen.getAllByTestId('nav-button')).toHaveLength(4)
+  })
+
+  it('changes color if in fairground mode', () => {
+    renderNav({ isFairground: true })
+    expect(screen.getByTestId('nav-bar')).toHaveClass('bg-vega-yellow-500')
+  })
+})
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,115 @@
+import type { ReactNode } from 'react'
+import { Settings } from '../icons/settings'
+import { Wallet } from '../icons/wallet'
+import type { To } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import classnames from 'classnames'
+import { LeftRightArrows } from '../icons/left-right-arrows'
+import { FULL_ROUTES } from '../../routes'
+import { UpDownArrows } from '../icons/up-down-arrows'
+import locators from '../locators'
+
+export interface NavButtonProps {
+  end?: boolean
+  icon: ReactNode
+  text: string
+  to: To
+  isFairground: boolean
+}
+
+const TEXT_COLOR = {
+  fairground: {
+    active: 'text-black',
+    inactive: 'text-neutral-600'
+  },
+  default: {
+    active: 'text-white',
+    inactive: 'text-neutral-600'
+  }
+}
+
+export const NavButton = ({
+  icon,
+  text,
+  to,
+  end,
+  isFairground
+}: NavButtonProps) => {
+  const textColors = isFairground ? TEXT_COLOR.fairground : TEXT_COLOR.default
+  return (
+    <NavLink
+      end={end}
+      data-testid={locators.navButton}
+      to={to}
+      className="text-center"
+    >
+      {({ isActive }) => {
+        const textColor = isActive ? textColors.active : textColors.inactive
+
+        return (
+          <div className="h-full grid gap-0 grid-rows-[1fr_auto_auto]">
+            <div className={classnames('grid items-center mt-5', textColor)}>
+              {icon}
+            </div>
+            <span className={classnames('mt-2 text-xs', textColor)}>
+              {text}
+            </span>
+            <div
+              data-testid={locators.linkActive}
+              className={classnames('h-1 w-full mt-2', {
+                'bg-vega-yellow': isActive && !isFairground,
+                'bg-black': isActive && isFairground
+              })}
+            />
+          </div>
+        )
+      }}
+    </NavLink>
+  )
+}
+
+export const NavBar = ({ isFairground }: { isFairground: boolean }) => {
+  return (
+    <nav
+      data-testid={locators.navBar}
+      className={classnames(
+        'z-10 w-full h-20 grid gap-0 grid-cols-[1fr_1fr_1fr_1fr] border-t border-vega-dark-200',
+        {
+          'bg-black': !isFairground,
+          'bg-vega-yellow-500': isFairground
+        }
+      )}
+    >
+      <NavButton
+        isFairground={isFairground}
+        end={true}
+        icon={
+          <Wallet
+            className="m-auto"
+            squareFill={isFairground ? '#D7FB50' : 'black'}
+          />
+        }
+        to={{ pathname: FULL_ROUTES.wallets }}
+        text="Wallets"
+      />
+      <NavButton
+        isFairground={isFairground}
+        icon={<LeftRightArrows className="m-auto" />}
+        to={{ pathname: FULL_ROUTES.transactions }}
+        text="Transactions"
+      />
+      <NavButton
+        isFairground={isFairground}
+        icon={<UpDownArrows className="m-auto" />}
+        to={{ pathname: FULL_ROUTES.connections }}
+        text="Connections"
+      />
+      <NavButton
+        isFairground={isFairground}
+        icon={<Settings className="m-auto" />}
+        to={{ pathname: FULL_ROUTES.settings }}
+        text="Settings"
+      />
+    </nav>
+  )
+}

4c45a884efc94430071328c6f85230d775efc7d1
Emil Bay <github@tixz.dk>
2023-05-03 10:01:10 +0200

    fix: nested concurrent storage gets wrong context

--- web-extension/common/test/concurrent-storage.js
+++ web-extension/common/test/concurrent-storage.js
@@ -1,15 +1,39 @@
 import { test } from 'brittle'
-import LockedStorage from '../lib/concurrent-storage.js'
+import ConcurrentStorage from '../lib/concurrent-storage.js'
 
-test('locked-storage', async (assert) => {
+test('concurrent storage', async (assert) => {
   const storage = new Map()
-  const lockedStorage = new LockedStorage(storage)
+  const concurrentStorage = new ConcurrentStorage(storage)
 
-  await lockedStorage.transaction(async (store) => {
-    assert.ok((await store.get('foo')) == null)
+  await concurrentStorage.transaction(async (store) => {
+    assert.ok((await store.get('foo')) == null, 'get() returns undefined for missing keys')
 
-    assert.exception(async () => {
-      await lockedStorage.get('foo')
-    })
+    await assert.exception(async () => {
+      await concurrentStorage.get('foo')
+    }, 'Deadlok detected on nested access')
+
+    await store.set('foo', 'bar')
+
+    assert.is(await store.get('foo'), 'bar', 'get() returns the value for existing keys')
   })
+
+  assert.is(await concurrentStorage.get('foo'), 'bar', 'get() returns the value for existing keys')
+})
+
+test('nested concurrent storage', async (assert) => {
+  const storage = new Map()
+  const c1 = new ConcurrentStorage(storage)
+  const c2 = new ConcurrentStorage(c1)
+
+  await c2.transaction(async (store) => {
+    assert.ok((await store.get('foo')) == null, 'get() returns undefined for missing keys')
+
+    await assert.exception(async () => {
+      await c2.get('foo')
+    }, 'Deadlok detected on nested access')
+
+    await store.set('foo', 'bar')
+  })
+
+  assert.is(await c2.get('foo'), await c1.get('foo'), 'get() returns the value for existing keys on nested storage')
 })

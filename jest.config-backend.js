1a2a2bfd20c67b2eb589cb6805b01ef302297620
Dexter <dexter@vega.xyz>
2023-05-04 12:50:57 +0100

    ci: add test reports

--- jest.config-backend.js
+++ jest.config-backend.js
@@ -1,4 +1,14 @@
 module.exports = {
   testEnvironment: 'jest-environment-node',
-  testTimeout: 30000
+  testTimeout: 30000,
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: './test-reports',
+        outputName: 'backend-test-results.xml'
+      }
+    ]
+  ]
 }

ae7d93dfc76258ad2872cf4513e1ce8e9c6b201a
Emil Bay <github@tixz.dk>
2023-05-04 08:46:12 +0200

    feat: Implement remaining admin apis for this sprint

--- web-extension/common/backend/wallets.js
+++ web-extension/common/backend/wallets.js
@@ -1,35 +1,33 @@
 import { VegaWallet, HARDENED } from '@vegaprotocol/crypto'
 import { generate as generateMnemonic } from '@vegaprotocol/crypto/bip-0039/mnemonic'
-import mutexify from 'mutexify/promise'
+import ConcurrentStorage from '../lib/concurrent-storage.js'
 
 export class WalletCollection {
-  constructor(store) {
-    this.store = store
-    this.lock = mutexify()
+  constructor({ walletsStore, publicKeyIndexStore }) {
+    this.store = new ConcurrentStorage(walletsStore)
+    this.index = new ConcurrentStorage(publicKeyIndexStore)
   }
 
-  async get({ wallet }) {
-    return this.store.get(wallet)
+  async get({ name }) {
+    return this.store.get(name)
   }
 
   async getKeyByPublicKey({ publicKey }) {
-    const pkIndex = (await this.store.get('_index')) ?? {}
-    if (pkIndex == null) return
+    return this.store.transaction(async (store) => {
+      const walletName = this.index.get(publicKey)
+      if (walletName == null) return
 
-    const walletName = pkIndex[publicKey]
-    if (walletName == null) return
+      const walletConfig = await store.get(walletName)
+      if (walletConfig == null) return
 
-    const walletConfig = await this.get({ wallet: walletName })
+      const keyConfig = walletConfig.keys.find((k) => k.publicKey === publicKey)
+      if (keyConfig == null) return
 
-    if (walletConfig == null) return
+      const wallet = await VegaWallet.fromSeed(new Uint8Array(walletConfig.seed))
+      const keyPair = await wallet.keyPair(keyConfig.index)
 
-    const keyConfig = walletConfig.keys.find((k) => k.publicKey === publicKey)
-    if (keyConfig == null) return
-
-    const wallet = await VegaWallet.fromSeed(new Uint8Array(walletConfig.seed))
-    const keyPair = await wallet.keyPair(keyConfig.index)
-
-    return keyPair
+      return keyPair
+    })
   }
 
   async list() {
@@ -37,7 +35,7 @@ export class WalletCollection {
   }
 
   async listKeys({ wallet }) {
-    const walletConfig = await this.get({ wallet })
+    const walletConfig = await this.get({ name: wallet })
 
     if (walletConfig == null) {
       throw new Error(`Cannot find wallet with name "${wallet}".`)
@@ -46,88 +44,62 @@ export class WalletCollection {
     return walletConfig.keys
   }
 
-  async create({ name }) {
-    const release = await this.lock()
-
-    try {
-      // TODO: Consider the passphrase when we have encrypted storage
-      if (await this.store.has(name))
-        throw new Error(`Wallet with name "${name}" already exists.`)
-
-      const bitStrength = 256 // 24 words
-      const mnemonic = (await generateMnemonic(bitStrength)).join(' ')
-
-      const seed = await VegaWallet.deriveSeed(mnemonic)
-
-      await this.store.set(name, {
-        // TODO: When we make encrypted storage we should be able to persist
-        // this Uint8Array in a better way
-        seed: Array.from(seed),
-        keys: []
-      })
-
-      return {
-        recoveryPhrase: mnemonic
-      }
-    } finally {
-      release()
-      await this.generateKey({
-        wallet: name,
-        metadata: [{ key: 'name', value: 'Key 1' }]
-      })
-    }
+  async generateRecoveryPhrase() {
+    const bitStrength = 256 // 24 words
+    return (await generateMnemonic(bitStrength)).join(' ')
   }
 
-  async generateKey({ wallet: walletName, metadata: extraMetadata }) {
-    const release = await this.lock()
-    try {
-      // TODO: Consider the passphrase when we have encrypted storage
-      const walletConfig = await this.get({ wallet: walletName })
+  async import({ name, recoveryPhrase }) {
+    return await this.store.transaction(async (store) => {
+      if (await store.has(name)) throw new Error(`Wallet with name "${name}" already exists.`)
+
+      const seed = await VegaWallet.deriveSeed(recoveryPhrase)
+      const firstKey = await this._generateKey({
+        walletInstance: await VegaWallet.fromSeed(new Uint8Array(seed)),
+        // TODO Verify this is actually generating the correct keys
+        index: HARDENED + 1 + 1
+      })
+      await store.set(name, {
+        seed: Array.from(seed),
+        keys: [firstKey]
+      })
+
+      await this.index.set(firstKey.publicKey, name)
+
+      return null
+    })
+  }
+
+  async _generateKey({ walletInstance, index, name, metadata, options }) {
+    const keyPair = await walletInstance.keyPair(index)
+    const publicKey = keyPair.publicKey.toString()
+
+    if (name == null) name = `Key ${keyPair.index - HARDENED}`
+
+    return { name, publicKey, index: keyPair.index, metadata, options }
+  }
+
+  async generateKey({ wallet: walletName, name, metadata, options }) {
+    return await this.store.transaction(async (store) => {
+      const walletConfig = await store.get(walletName)
 
       if (walletConfig == null) {
         throw new Error(`Cannot find wallet with name "${walletName}".`)
       }
 
-      const wallet = await VegaWallet.fromSeed(
-        new Uint8Array(walletConfig.seed)
-      )
+      const wallet = await VegaWallet.fromSeed(new Uint8Array(walletConfig.seed))
 
-      const lastKey = walletConfig.keys.at(-1)
+      const lastKey = walletConfig.keys.at(-1) ?? {}
+      const lastKeyIndex = (lastKey.index ?? HARDENED + 1) + 1
 
-      const lastKeyIndex = lastKey?.index ?? HARDENED + 1
+      const key = await this._generateKey({ walletInstance: wallet, name, metadata, options })
 
-      const keyPair = await wallet.keyPair(lastKeyIndex + 1)
-      const publicKey = keyPair.publicKey.toString()
+      walletConfig.keys.push(key)
+      await store.set(walletName, walletConfig)
 
-      let keyName = extraMetadata.find((m) => m.key === 'name')?.value
-      if (keyName == null) keyName = `Key ${keyPair.index - HARDENED}`
+      await this.index.set(key.publicKey, walletName)
 
-      const metadata = [
-        {
-          key: 'name',
-          value: keyName
-        },
-        // Add renaming metadata sans the key name
-        ...extraMetadata.filter((m) => m.key === 'name')
-      ]
-
-      // TODO: Ideally this should be in a mutex on the storage key
-      walletConfig.keys.push({
-        index: keyPair.index,
-        publicKey,
-        name: keyName,
-        metadata
-      })
-
-      await this.store.set(walletName, walletConfig)
-
-      const pkIndex = (await this.store.get('_index')) ?? {}
-      pkIndex[publicKey] = walletName
-      await this.store.set('_index', pkIndex)
-
-      return keyPair
-    } finally {
-      release()
-    }
+      return key
+    })
   }
 }

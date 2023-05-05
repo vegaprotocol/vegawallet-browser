3b4056596556dd2796aacb59a4676b5ad214a3d0
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-05-05 17:43:56 +0100

    explicitely set chrome binary

--- test/e2e/selenium-util.ts
+++ test/e2e/selenium-util.ts
@@ -6,13 +6,7 @@ const extensionPath = './build'
 
 export async function initDriver() {
   let driver: WebDriver | null = null
-  let chromeOptions = new chrome.Options().addArguments(`--load-extension=${extensionPath + '/chrome'}`)
-  chromeOptions.addArguments('--disable-gpu');
-  chromeOptions.addArguments('--no-sandbox');
-  chromeOptions.addArguments('--disable-dev-shm-usage');
-  if (process.env.HEADLESS) {
-    chromeOptions = chromeOptions.headless()
-  }
+  let chromeOptions = new chrome.Options().addArguments(`--load-extension=${extensionPath + '/chrome'}`, '--disable-gpu', '--no-sandbox', '--disable-dev-shm-usage').setChromeBinaryPath('/usr/bin/google-chrome')
   driver = new Builder().withCapabilities(Capabilities.chrome()).setChromeOptions(chromeOptions).build()
 
   if (!driver) {

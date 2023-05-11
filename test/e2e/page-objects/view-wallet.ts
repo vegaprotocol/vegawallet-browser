2a96456a853ee622a265f66593ac9f5c9bffdbf1
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-05-11 10:36:20 +0100

    add test checking fairground default

--- test/e2e/page-objects/view-wallet.ts
+++ test/e2e/page-objects/view-wallet.ts
@@ -8,12 +8,14 @@ import {
 } from '../selenium-util'
 import * as locators from '../../../src/locator-ids'
 import 'jest-expect-message'
+import { networkIndicator } from '../../../src/locator-ids'
 
 export class ViewWallet {
   private readonly viewWalletsHeader: By = getByDataTestID(locators.viewWalletsHeader)
   private readonly walletName: By = getByDataTestID(locators.walletsWalletName)
   private readonly createNewKeyPairButton: By = getByDataTestID(locators.walletsCreateKey)
   private readonly walletKeys: By = getByDataTestID('list')
+  private readonly networkIndicator: By = getByDataTestID(locators.networkIndicator)
 
   constructor(private readonly driver: WebDriver) {}
 
@@ -37,5 +39,9 @@ export class ViewWallet {
 
   async createNewKeyPair() {
     await clickElement(this.driver, this.createNewKeyPairButton)
+  }
+
+  async getNetworkConnectedTo() {
+    return await getElementText(this.driver, this.networkIndicator)
   }
 }

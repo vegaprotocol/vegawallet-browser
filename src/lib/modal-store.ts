160b91ab257bb2329638b0d69ad8fed1fd0add29
Dexter <dexter@vega.xyz>
2023-05-11 14:33:50 +0100

    feat: connection modal

---
+++ src/lib/modal-store.ts
@@ -0,0 +1,23 @@
+import { create } from 'zustand'
+
+export type ModalStore = {
+  connectionModalOpen: boolean
+  setConnectionModalOpen: (open: boolean) => void
+  transactionModalOpen: boolean
+  setTransactionModalOpen: (open: boolean) => void
+}
+
+export const useModalStore = create<ModalStore>()((set, get) => ({
+  connectionModalOpen: true,
+  transactionModalOpen: false,
+  setConnectionModalOpen: (open: boolean) => {
+    set({
+      connectionModalOpen: open
+    })
+  },
+  setTransactionModalOpen: (open: boolean) => {
+    set({
+      transactionModalOpen: open
+    })
+  }
+}))
--- src/routes/auth/index.tsx
+++ src/routes/auth/index.tsx
@@ -3,9 +3,12 @@ import { NavBar } from '../../components/navbar'
 import { VegaIcon } from '../../components/icons/vega-icon'
 import { networkIndicator } from '../../locator-ids'
 
+import { ConnectionModal } from '../../components/connection-modal'
+
 export const Auth = () => {
   return (
     <div className="h-full w-full grid grid-rows-[1fr_min-content] overflow-y-scroll">
+      <ConnectionModal />
       <section className="w-full h-full overflow-y-scroll pt-3 px-5 bg-vega-dark-100">
         <div className="flex justify-between items-center mb-10">
           <VegaIcon size={48} backgroundColor="none" />

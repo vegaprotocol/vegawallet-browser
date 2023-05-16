5fe0f741811389caead60c16bdfc91051a250c1c
Dexter <dexter@vega.xyz>
2023-05-16 12:37:23 +0100

    test: get tests passing because of conflicts

--- src/components/collapsible-panel/collapsible-panel.spec.tsx
+++ src/components/collapsible-panel/collapsible-panel.spec.tsx
@@ -1,5 +1,6 @@
 import { fireEvent, render, screen } from '@testing-library/react'
 import { CollapsiblePanel } from './collapsible-panel'
+import locators from '../locators'
 
 const renderComponent = ({ initiallyOpen = false }: { initiallyOpen?: boolean }) => {
   return render(
@@ -10,20 +11,20 @@ const renderComponent = ({ initiallyOpen = false }: { initiallyOpen?: boolean })
 describe('Collapsible panel', () => {
   it('renders title, arrow and does not render content', () => {
     renderComponent({})
-    expect(screen.getByTestId('collapsible-panel-button')).toBeInTheDocument()
-    expect(screen.getByTestId('collapsible-panel-title')).toHaveTextContent('Title')
-    expect(screen.getByTestId('collapsible-panel-content')).toHaveClass('hidden')
-    expect(screen.getByTestId('dropdown-arrow')).toBeInTheDocument()
+    expect(screen.getByTestId(locators.collapsiblePanelButton)).toBeInTheDocument()
+    expect(screen.getByTestId(locators.collapsiblePanelTitle)).toHaveTextContent('Title')
+    expect(screen.getByTestId(locators.collapsiblePanelContent)).toHaveClass('hidden')
+    expect(screen.getByTestId(locators.dropdownArrow)).toBeInTheDocument()
   })
   it('opens panel on click', () => {
     renderComponent({})
-    fireEvent.click(screen.getByTestId('collapsible-panel-button'))
-    expect(screen.getByTestId('collapsible-panel-content')).not.toHaveClass('hidden')
-    expect(screen.getByTestId('dropdown-arrow')).toHaveClass('rotate-180')
+    fireEvent.click(screen.getByTestId(locators.collapsiblePanelButton))
+    expect(screen.getByTestId(locators.collapsiblePanelContent)).not.toHaveClass('hidden')
+    expect(screen.getByTestId(locators.dropdownArrow)).toHaveClass('rotate-180')
   })
   it('renders panel as open when initially open is true', () => {
     renderComponent({ initiallyOpen: true })
-    expect(screen.getByTestId('collapsible-panel-content')).not.toHaveClass('hidden')
-    expect(screen.getByTestId('dropdown-arrow')).toHaveClass('rotate-180')
+    expect(screen.getByTestId(locators.collapsiblePanelContent)).not.toHaveClass('hidden')
+    expect(screen.getByTestId(locators.dropdownArrow)).toHaveClass('rotate-180')
   })
 })

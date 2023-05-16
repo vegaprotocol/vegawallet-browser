1e2339d567e2742cbe1d8d6037bd10cc5003df38
Dexter <dexter@vega.xyz>
2023-05-16 12:37:39 +0100

    chore: add transaction modal

--- src/routes/auth/index.tsx
+++ src/routes/auth/index.tsx
@@ -2,11 +2,13 @@ import { Outlet } from 'react-router-dom'
 import { NavBar } from '../../components/navbar'
 
 import { ConnectionModal } from '../../components/connection-modal'
+import { TransactionModal } from '../../components/transaction-modal'
 
 export const Auth = () => {
   return (
     <div className="h-full w-full grid grid-rows-[1fr_min-content] overflow-y-scroll">
       <ConnectionModal />
+      <TransactionModal />
       <section className="w-full h-full overflow-y-scroll bg-vega-dark-100">
         <Outlet />
       </section>

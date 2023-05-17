0c06b7b697940496439fde45633227562c4ea2b6
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-05-17 10:58:11 +0100

    remove changes, want to be unintrusive to the tests at this point

--- test/e2e/view-wallet.test.ts
+++ test/e2e/view-wallet.test.ts
@@ -6,7 +6,6 @@ import { SecureYourWallet } from './page-objects/secure-your-wallet'
 import { CreateAWallet } from './page-objects/create-a-wallet'
 import { ViewWallet } from './page-objects/view-wallet'
 import { navigateToLandingPage } from './wallet-helpers/common'
-import { APIHelper } from './wallet-helpers/api-helpers'
 
 describe('View wallet page', () => {
   let driver: WebDriver
@@ -15,7 +14,6 @@ describe('View wallet page', () => {
   let secureYourWallet: SecureYourWallet
   let createAWallet: CreateAWallet
   let viewWallet: ViewWallet
-  let apiHelper: APIHelper
   const testPassword = 'password1'
 
   beforeAll(async () => {
@@ -27,7 +25,6 @@ describe('View wallet page', () => {
     createAWallet = new CreateAWallet(driver)
     viewWallet = new ViewWallet(driver)
     await navigateToLandingPage(driver)
-    apiHelper = new APIHelper(driver)
     await getStarted.getStarted()
     await password.createPassword(testPassword)
     await createAWallet.createNewWallet()
@@ -44,8 +41,10 @@ describe('View wallet page', () => {
     // 1101-BWAL-030 New key pairs are listed in order they were created - oldest first
     await viewWallet.createNewKeyPair()
     expect(await viewWallet.getWalletKeys()).toMatchObject(['Key 1', 'Key 2'])
+
     await viewWallet.createNewKeyPair()
     expect(await viewWallet.getWalletKeys()).toMatchObject(['Key 1', 'Key 2', 'Key 3'])
+
     await navigateToLandingPage(driver)
     expect(await viewWallet.getWalletKeys()).toMatchObject(['Key 1', 'Key 2', 'Key 3'])
   })

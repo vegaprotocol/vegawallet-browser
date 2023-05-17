60c991f3d4f769e4ccd1329b81f845f25c8f7aed
Dexter <dexter@vega.xyz>
2023-05-17 13:28:12 +0100

    chore: add todo

--- src/test-helpers/mock-client.ts
+++ src/test-helpers/mock-client.ts
@@ -132,6 +132,7 @@ export const mockClient = (
               listeners.map((fn) =>
                 fn({
                   jsonrpc: '2.0',
+                  // TODO ensure this error is correct
                   error: { code: -32600, message: 'Incorrect Passphrase' },
                   id: message.id
                 })

3d86d63851f40d8560a0ebb6e12079a6866ecb1e
Dexter <dexter@vega.xyz>
2023-05-22 14:41:21 +0100

    chore: add tests for transaction modal

--- src/components/transaction-modal/transaction-modal.spec.tsx
+++ src/components/transaction-modal/transaction-modal.spec.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { useModalStore } from '../../lib/modal-store'
 import locators from '../locators'
 import { TransactionModal } from '.'
@@ -10,7 +10,10 @@ jest.mock('../../lib/modal-store', () => ({
 describe('ConnectionModal', () => {
   beforeEach(() => {
     jest.clearAllMocks()
+    jest.useFakeTimers()
+    jest.setSystemTime(new Date('2021-01-01T00:00:00.000Z'))
   })
+
   it('renders nothing when isOpen is false', () => {
     ;(useModalStore as unknown as jest.Mock).mockImplementationOnce((fn) => {
       const res = { isOpen: false }
@@ -20,6 +23,7 @@ describe('ConnectionModal', () => {
     const { container } = render(<TransactionModal />)
     expect(container).toBeEmptyDOMElement()
   })
+
   it('renders page header, transaction type, hostname and key', () => {
     ;(useModalStore as unknown as jest.Mock).mockImplementationOnce((fn) => {
       const res = { isOpen: true }
@@ -29,6 +33,35 @@ describe('ConnectionModal', () => {
     render(<TransactionModal />)
     expect(screen.getByTestId(locators.transactionWrapper)).toBeInTheDocument()
     expect(screen.getByTestId(locators.pageHeader)).toBeInTheDocument()
-    expect(false).toBe(true)
+    expect(screen.getByTestId(locators.transactionType)).toHaveTextContent('Order submission')
+    expect(screen.getByTestId(locators.transactionRequest)).toHaveTextContent('Request from https://www.google.com')
+    expect(screen.getByTestId(locators.transactionKey)).toHaveTextContent('Signing with')
+    expect(screen.getByTestId(locators.transactionTimeAgo)).toHaveTextContent('Received just now')
+    expect(screen.getByTestId(locators.transactionModalApproveButton)).toBeInTheDocument()
+    expect(screen.getByTestId(locators.transactionModalDenyButton)).toBeInTheDocument()
+  })
+
+  it('renders nothing after denying', async () => {
+    let open = true
+    ;(useModalStore as unknown as jest.Mock).mockImplementation((fn) => {
+      const res = { isOpen: open, setIsOpen: (v: boolean) => (open = v) }
+      fn(res)
+      return res
+    })
+    const { container } = render(<TransactionModal />)
+    fireEvent.click(screen.getByTestId(locators.transactionModalDenyButton))
+    await waitFor(() => expect(container).toBeEmptyDOMElement())
+  })
+
+  it('renders nothing after approving', async () => {
+    let open = true
+    ;(useModalStore as unknown as jest.Mock).mockImplementation((fn) => {
+      const res = { isOpen: open, setIsOpen: (v: boolean) => (open = v) }
+      fn(res)
+      return res
+    })
+    const { container } = render(<TransactionModal />)
+    fireEvent.click(screen.getByTestId(locators.transactionModalApproveButton))
+    await waitFor(() => expect(container).toBeEmptyDOMElement())
   })
 })

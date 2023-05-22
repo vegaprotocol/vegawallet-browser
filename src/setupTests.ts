8aa72fd782ef71dc52f10efbda5970c3283ccd9c
Dexter <dexter@vega.xyz>
2023-05-22 13:51:19 +0100

    test: fix transaction modal test

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,7 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
 
+TimeAgo.addDefaultLocale(en)
 class ResizeObserver {
   observe() {}
   unobserve() {}

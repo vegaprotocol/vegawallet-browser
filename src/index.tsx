a3a5582e051469bc0d98b4c50954b8ccc83d5817
Dexter <dexter@vega.xyz>
2023-05-22 13:43:48 +0100

    feat: create time ago

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
+
+TimeAgo.addDefaultLocale(en)
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(

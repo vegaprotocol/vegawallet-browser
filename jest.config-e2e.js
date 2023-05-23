c16334784df59e858ab086673447cc14d316739f
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-05-23 13:19:22 +0100

    amend test

--- jest.config-e2e.js
+++ jest.config-e2e.js
@@ -2,7 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testTimeout: 60000,
+  testTimeout: 120000,
   setupFilesAfterEnv: ["jest-expect-message"],
   reporters: [
     'default',

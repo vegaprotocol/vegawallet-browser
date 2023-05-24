b7bbf8c391b9e733407599d2affe2d6f12251c11
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-05-24 21:03:47 +0100

    get both sets of tests passing (hopefully)

--- test/e2e/login.test.ts
+++ test/e2e/login.test.ts
@@ -1,33 +1,35 @@
 import { WebDriver } from 'selenium-webdriver'
-import { captureScreenshot, copyProfile, initDriver } from './driver'
+import { captureScreenshot, copyProfile, deleteAutomationFirefoxProfile, initDriver, initFirefoxDriver } from './driver'
 import { navigateToLandingPage } from './wallet-helpers/common'
 import { APIHelper } from './wallet-helpers/api-helpers'
 import { Login } from './page-objects/login'
 import { CreateAWallet } from './page-objects/create-a-wallet'
+import { openNewWindowAndSwitchToIt } from './selenium-util'
 
 describe('Login', () => {
   let driver: WebDriver
   let apiHelper: APIHelper
   const testPassword = 'password1'
+  const isFirefox = process.env.BROWSER?.toLowerCase() === 'firefox'
 
   beforeEach(async () => {
-    driver = await initDriver(true)
+    driver = isFirefox ? await initFirefoxDriver(true) : await initDriver()
     apiHelper = new APIHelper(driver)
     await navigateToLandingPage(driver)
     await apiHelper.createPassphrase(testPassword)
-    await copyProfile(driver)
-    await driver.quit()
-    driver = await initDriver(true, false)
-    await navigateToLandingPage(driver)
   })
 
   afterEach(async () => {
     await captureScreenshot(driver, expect.getState().currentTestName as string)
+    if (isFirefox) {
+      deleteAutomationFirefoxProfile()
+    }
     await driver.quit()
   })
 
-  it('Check can log in via the login page', async () => {
-    //1101-BWAL-058 When I have quit my browser, and then reopened, I am asked to enter my browser extension password'
+  it('Check can log in via the login page after quitting the browser(firefox) or loading in a new tab(chrome)', async () => {
+    // 1101-BWAL-058 When I have quit my browser, and then reopened, I am asked to enter my browser extension password'
+    await openNewInstanceOfVegaWallet()
     const login = new Login(driver)
     await login.checkOnLoginPage()
     await login.login(testPassword)
@@ -36,9 +38,22 @@ describe('Login', () => {
   })
 
   it('1101-BWAL-059 I am informed if I enter my password incorrectly', async () => {
+    await apiHelper.lockWallet()
+    await navigateToLandingPage(driver)
     const login = new Login(driver)
     await login.checkOnLoginPage()
     await login.login(testPassword + '1')
-    // Need to  assert an error here.
+    // Need to assert an error here.
   })
+
+  async function openNewInstanceOfVegaWallet() {
+    if (isFirefox) {
+      await copyProfile(driver)
+      await driver.quit()
+      driver = await initFirefoxDriver(true, false)
+    } else {
+      await openNewWindowAndSwitchToIt(driver)
+    }
+    await navigateToLandingPage(driver)
+  }
 })

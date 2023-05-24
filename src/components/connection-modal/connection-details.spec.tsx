ad36dc7918635e25e6763339786c23d17266215c
Dexter <dexter@vega.xyz>
2023-05-24 14:22:41 +0100

    test: fix connection details tests

--- src/components/connection-modal/connection-details.spec.tsx
+++ src/components/connection-modal/connection-details.spec.tsx
@@ -1,33 +1,28 @@
-// import { render, screen } from '@testing-library/react'
-// import { ConnectionDetails } from './connection-details'
-// import locators from '../locators'
+import { render, screen } from '@testing-library/react'
+import { ConnectionDetails } from './connection-details'
+import locators from '../locators'
 
-// describe('ConnectionDetails', () => {
-//   it('should render header, hostname, keys and permissions', () => {
-//     render(<ConnectionDetails handleDecision={() => {}} isLoading={false} hostname="https://www.google.com" />)
-//     expect(screen.getByTestId(locators.modalHeaderTitle)).toHaveTextContent('Connected to dApp')
-//     expect(screen.getByTestId(locators.dAppHostname)).toHaveTextContent('https://www.google.com')
-//     expect(screen.getByTestId(locators.connectionModalAccessListTitle)).toHaveTextContent('Allow this site to:')
-//     const permissions = screen.getAllByTestId(locators.connectionModalAccessListAccess)
-//     expect(permissions).toHaveLength(2)
-//     expect(permissions[0]).toHaveTextContent('See all of your wallet’s public keys')
-//     expect(permissions[1]).toHaveTextContent('Send transaction requests for you to sign')
-//   })
-//   it('calls handleDecision handler with true when approve button is clicked', () => {
-//     const handleDecision = jest.fn()
-//     render(<ConnectionDetails handleDecision={handleDecision} isLoading={false} hostname="https://www.google.com" />)
-//     screen.getByTestId(locators.connectionModalApproveButton).click()
-//     expect(handleDecision).toHaveBeenCalledWith(true)
-//   })
-//   it('calls handleDecision handler with false when deny button is clicked', () => {
-//     const handleDecision = jest.fn()
-//     render(<ConnectionDetails handleDecision={handleDecision} isLoading={false} hostname="https://www.google.com" />)
-//     screen.getByTestId(locators.connectionModalDenyButton).click()
-//     expect(handleDecision).toHaveBeenCalledWith(false)
-//   })
-//   it('disables buttons when loading', () => {
-//     render(<ConnectionDetails handleDecision={() => {}} isLoading={true} hostname="https://www.google.com" />)
-//     expect(screen.getByTestId(locators.connectionModalApproveButton)).toBeDisabled()
-//     expect(screen.getByTestId(locators.connectionModalDenyButton)).toBeDisabled()
-//   })
-// })
+describe('ConnectionDetails', () => {
+  it('should render header, hostname, keys and permissions', () => {
+    render(<ConnectionDetails handleDecision={() => {}} hostname="https://www.google.com" />)
+    expect(screen.getByTestId(locators.modalHeaderTitle)).toHaveTextContent('Connected to dApp')
+    expect(screen.getByTestId(locators.dAppHostname)).toHaveTextContent('https://www.google.com')
+    expect(screen.getByTestId(locators.connectionModalAccessListTitle)).toHaveTextContent('Allow this site to:')
+    const permissions = screen.getAllByTestId(locators.connectionModalAccessListAccess)
+    expect(permissions).toHaveLength(2)
+    expect(permissions[0]).toHaveTextContent('See all of your wallet’s public keys')
+    expect(permissions[1]).toHaveTextContent('Send transaction requests for you to sign')
+  })
+  it('calls handleDecision handler with true when approve button is clicked', () => {
+    const handleDecision = jest.fn()
+    render(<ConnectionDetails handleDecision={handleDecision} hostname="https://www.google.com" />)
+    screen.getByTestId(locators.connectionModalApproveButton).click()
+    expect(handleDecision).toHaveBeenCalledWith(true)
+  })
+  it('calls handleDecision handler with false when deny button is clicked', () => {
+    const handleDecision = jest.fn()
+    render(<ConnectionDetails handleDecision={handleDecision} hostname="https://www.google.com" />)
+    screen.getByTestId(locators.connectionModalDenyButton).click()
+    expect(handleDecision).toHaveBeenCalledWith(false)
+  })
+})

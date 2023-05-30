76c43bbfd466093af8cc9e48f491ce020f5025c1
Dexter <dexter@vega.xyz>
2023-05-30 10:24:23 +0100

    test: correct impoty

--- web-extension/common/test/client-ns.spec.js
+++ web-extension/common/test/client-ns.spec.js
@@ -1,14 +1,14 @@
 import initClientServer from '../backend/client-ns.js'
 import { WalletCollection } from '../backend/wallets.js'
 import { NetworkCollection } from '../backend/network.js'
-import { ConnectionCollection } from '../backend/connections.js'
+import { ConnectionsCollection } from '../backend/connections.js'
 import ConcurrentStorage from '../lib/concurrent-storage.js'
 import EncryptedStorage from '../lib/encrypted-storage.js'
 
 describe('client-ns', () => {
   it('should connect, then disconnect', async () => {
     const publicKeyIndexStore = new ConcurrentStorage(new Map())
-    const connections = new ConnectionCollection({
+    const connections = new ConnectionsCollection({
       connectionsStore: new ConcurrentStorage(new Map()),
       publicKeyIndexStore
     })
@@ -40,28 +40,32 @@ describe('client-ns', () => {
       origin: 'https://example.com'
     }
 
-    const res = await server.onrequest({
-      jsonrpc: '2.0',
-      id: 1,
-      method: 'client.connect_wallet',
-      params: null
-    }, context)
+    const res = await server.onrequest(
+      {
+        jsonrpc: '2.0',
+        id: 1,
+        method: 'client.connect_wallet',
+        params: null
+      },
+      context
+    )
 
     expect(res).toBe(null)
     expect(context.isConnected).toBe(true)
     expect(await connections.has(context.origin)).toBe(true)
 
-    const res2 = await server.onrequest({
-      jsonrpc: '2.0',
-      id: 2,
-      method: 'client.disconnect_wallet',
-      params: null
-    }, context)
+    const res2 = await server.onrequest(
+      {
+        jsonrpc: '2.0',
+        id: 2,
+        method: 'client.disconnect_wallet',
+        params: null
+      },
+      context
+    )
 
     expect(res2).toBe(null)
     expect(context.isConnected).toBe(false)
     expect(await connections.has(context.origin)).toBe(true)
   })
 })
-
-

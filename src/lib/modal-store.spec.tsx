9e225843055ad8b603a1fd82635e1665aee8536d
Dexter <dexter@vega.xyz>
2023-05-30 09:12:31 +0100

    test: add tests for the modal store

--- src/lib/modal-store.spec.tsx
+++ src/lib/modal-store.spec.tsx
@@ -1,23 +1,53 @@
-// import { useModalStore } from './modal-store'
+import { useModalStore } from './modal-store'
 
-// const initialState = useModalStore.getState()
+const initialState = useModalStore.getState()
 
-// describe('Store', () => {
-//   beforeEach(() => {
-//     useModalStore.setState(initialState)
-//   })
-//   it('sets connection modal open to appropriate value', () => {
-//     expect(useModalStore.getState().connectionModalOpen).toBe(false)
-//     useModalStore.getState().setConnectionModalOpen(true)
-//     expect(useModalStore.getState().connectionModalOpen).toBe(true)
-//     useModalStore.getState().setConnectionModalOpen(false)
-//     expect(useModalStore.getState().connectionModalOpen).toBe(false)
-//   })
-//   it('sets transaction modal open to appropriate value', () => {
-//     expect(useModalStore.getState().transactionModalOpen).toBe(false)
-//     useModalStore.getState().setTransactionModalOpen(true)
-//     expect(useModalStore.getState().transactionModalOpen).toBe(true)
-//     useModalStore.getState().setTransactionModalOpen(false)
-//     expect(useModalStore.getState().transactionModalOpen).toBe(false)
-//   })
-// })
+describe('ModalStore', () => {
+  beforeEach(() => {
+    useModalStore.setState(initialState)
+  })
+  it('transaction modal sets modal as open and resolves promise with value', () => {
+    expect(useModalStore.getState().transactionModalOpen).toBe(false)
+    expect(useModalStore.getState().transactionPromise).toBe(null)
+    const promise = useModalStore.getState().handleTransaction({})
+    expect(useModalStore.getState().transactionModalOpen).toBe(true)
+    expect(useModalStore.getState().transactionPromise).not.toBe(null)
+    useModalStore.getState().handleTransactionDecision(true)
+    expect(useModalStore.getState().transactionModalOpen).toBe(false)
+    expect(useModalStore.getState().transactionPromise).toBe(null)
+    return expect(promise).resolves.toBe(true)
+  })
+  it('transaction resolves with false if not approved', () => {
+    expect(useModalStore.getState().transactionModalOpen).toBe(false)
+    expect(useModalStore.getState().transactionPromise).toBe(null)
+    const promise = useModalStore.getState().handleTransaction({})
+    expect(useModalStore.getState().transactionModalOpen).toBe(true)
+    expect(useModalStore.getState().transactionPromise).not.toBe(null)
+    useModalStore.getState().handleTransactionDecision(false)
+    expect(useModalStore.getState().transactionModalOpen).toBe(false)
+    expect(useModalStore.getState().transactionPromise).toBe(null)
+    return expect(promise).resolves.toBe(false)
+  })
+  it('connection modal sets modal as open and resolves promise with value', () => {
+    expect(useModalStore.getState().connectionModalOpen).toBe(false)
+    expect(useModalStore.getState().connectionPromise).toBe(null)
+    const promise = useModalStore.getState().handleConnection({})
+    expect(useModalStore.getState().connectionModalOpen).toBe(true)
+    expect(useModalStore.getState().connectionPromise).not.toBe(null)
+    useModalStore.getState().handleConnectionDecision(true)
+    expect(useModalStore.getState().connectionModalOpen).toBe(false)
+    expect(useModalStore.getState().connectionPromise).toBe(null)
+    return expect(promise).resolves.toBe(true)
+  })
+  it('connection resolves with false if not approved', () => {
+    expect(useModalStore.getState().connectionModalOpen).toBe(false)
+    expect(useModalStore.getState().connectionPromise).toBe(null)
+    const promise = useModalStore.getState().handleConnection({})
+    expect(useModalStore.getState().connectionModalOpen).toBe(true)
+    expect(useModalStore.getState().connectionPromise).not.toBe(null)
+    useModalStore.getState().handleConnectionDecision(false)
+    expect(useModalStore.getState().connectionModalOpen).toBe(false)
+    expect(useModalStore.getState().connectionPromise).toBe(null)
+    return expect(promise).resolves.toBe(false)
+  })
+})

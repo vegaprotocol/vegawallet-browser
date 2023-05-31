004d37568452b7755154488347b1953429e50402
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-05-31 15:43:52 +0100

    additional test

--- test/e2e/transaction.test.ts
+++ test/e2e/transaction.test.ts
@@ -6,7 +6,6 @@ import { APIHelper } from './wallet-helpers/api-helpers'
 import { VegaAPI } from './wallet-helpers/vega-api'
 import { ConnectWallet } from './page-objects/connect-wallet'
 import { TransferRequest } from '@vegaprotocol/protos/dist/vega/TransferRequest'
-import { TransferType } from '@vegaprotocol/protos/dist/vega/TransferType'
 import { Transaction } from './page-objects/transaction'
 import { openNewWindowAndSwitchToIt } from './selenium-util'
 
@@ -50,7 +49,7 @@ describe('transactions', () => {
   it('can confirm a transaction', async () => {
     // 1101-BWAL-044 When I view a transaction request I can choose to approve it
     const keys = await vegaAPI.listKeys()
-    await vegaAPI.sendTransaction(keys[0].publicKey, { transfer: transferReq })
+    await vegaAPI.sendTransactionAndCheckOutcome(keys[0].publicKey, { transfer: transferReq })
     await transaction.checkOnTransactionPage()
     await transaction.confirmTransaction()
     //check for confirmation (does not exist yet!)
@@ -61,15 +60,15 @@ describe('transactions', () => {
     // 1101-BWAL-047 When I approve a transaction after I have approved it we revert to the next transaction if there's a queue OR we revert to the key view (the front / homepage)
     // 1101-BWAL-051 When I reject a transaction after I have rejected it we revert to the next transaction if there's a queue OR we revert to the key view (start / home page)
     const keys = await vegaAPI.listKeys()
-    await vegaAPI.sendTransaction(keys[0].publicKey, { transfer: transferReq })
-    await vegaAPI.sendTransaction(keys[0].publicKey, { transfer: transferReq })
+    await vegaAPI.sendTransactionAndCheckOutcome(keys[0].publicKey, { transfer: transferReq })
+    await vegaAPI.sendTransactionAndCheckOutcome(keys[0].publicKey, { transfer: transferReq })
     await transaction.checkOnTransactionPage()
     await transaction.confirmTransaction()
     await transaction.checkOnTransactionPage()
     await transaction.confirmTransaction()
     await viewWallet.checkOnViewWalletPage()
-    await vegaAPI.sendTransaction(keys[0].publicKey, { transfer: transferReq })
-    await vegaAPI.sendTransaction(keys[0].publicKey, { transfer: transferReq })
+    await vegaAPI.sendTransactionAndCheckOutcome(keys[0].publicKey, { transfer: transferReq })
+    await vegaAPI.sendTransactionAndCheckOutcome(keys[0].publicKey, { transfer: transferReq })
     await transaction.checkOnTransactionPage()
     await transaction.rejectTransaction()
     //check for confirmation (does not exist yet!)
@@ -81,31 +80,39 @@ describe('transactions', () => {
   it('can reject a transaction', async () => {
     // 1101-BWAL-048 When I view a transaction request I can choose to reject it
     const keys = await vegaAPI.listKeys()
-    await vegaAPI.sendTransaction(keys[0].publicKey, { transfer: transferReq })
+    await vegaAPI.sendTransactionAndCheckOutcome(keys[0].publicKey, { transfer: transferReq })
     await transaction.checkOnTransactionPage()
     await transaction.rejectTransaction()
     //check for confirmation (does not exist yet!)
     await viewWallet.checkOnViewWalletPage()
   })
 
+  it("1101-BWAL-052 When the dapp requests a transaction with a key we don't know about, we don't see a request in the wallet but instead send an error back to the dapp", async () => {
+    const keyThatDoesNotExistInWallet = await vegaAPI.generateEncodedHexPublicKey()
+    await vegaAPI.sendTransactionAndCheckOutcome(
+      keyThatDoesNotExistInWallet,
+      { transfer: transferReq },
+      false,
+      'Unknown public key'
+    )
+    await viewWallet.checkOnViewWalletPage()
+  })
+
   it('the transaction persists when the extension is opened', async () => {
     // 1101-BWAL-054 When the user opens the extension (or it has automatically opened) they can immediately see a transaction request
     const keys = await vegaAPI.listKeys()
-    await vegaAPI.sendTransaction(keys[0].publicKey, { transfer: transferReq })
+    await vegaAPI.sendTransactionAndCheckOutcome(keys[0].publicKey, { transfer: transferReq })
     await transaction.checkOnTransactionPage()
     await openNewWindowAndSwitchToIt(driver)
+    await navigateToLandingPage(driver)
     await apiHelper.lockWallet()
     await apiHelper.login('password1')
+    await navigateToLandingPage(driver)
     await transaction.checkOnTransactionPage()
   })
 
   async function setUpWalletAndKey() {
     await apiHelper.setUpWalletAndKey()
     await navigateToLandingPage(driver)
-  }
-
-  async function assertNumberOfKeys(expectedNumber: number) {
-    const keys = await vegaAPI.listKeys()
-    expect(keys.length, `expected to be able to view 2 wallet keys, instead found ${keys.length}`).toBe(expectedNumber)
   }
 })

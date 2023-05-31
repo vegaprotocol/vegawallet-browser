94c5f43fdee8307cf870f1963ee2a22c0ab35d42
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-05-31 17:22:16 +0100

    add missing assertion into test and expand the test

--- test/e2e/login.test.ts
+++ test/e2e/login.test.ts
@@ -5,18 +5,23 @@ import { APIHelper } from './wallet-helpers/api-helpers'
 import { Login } from './page-objects/login'
 import { CreateAWallet } from './page-objects/create-a-wallet'
 import { openNewWindowAndSwitchToIt } from './selenium-util'
+import { ViewWallet } from './page-objects/view-wallet'
 
 describe('Login', () => {
   let driver: WebDriver
   let apiHelper: APIHelper
+  let viewWallet: ViewWallet
+  let login: Login
   const testPassword = 'password1'
   const isFirefox = process.env.BROWSER?.toLowerCase() === 'firefox'
 
   beforeEach(async () => {
     driver = isFirefox ? await initFirefoxDriver(true) : await initDriver()
     apiHelper = new APIHelper(driver)
+    viewWallet = new ViewWallet(driver)
+    login = new Login(driver)
     await navigateToLandingPage(driver)
-    await apiHelper.createPassphrase(testPassword)
+    await apiHelper.setUpWalletAndKey()
   })
 
   afterEach(async () => {
@@ -29,21 +34,27 @@ describe('Login', () => {
 
   it('Check can log in via the login page after quitting the browser(firefox) or loading in a new tab(chrome)', async () => {
     // 1101-BWAL-058 When I have quit my browser, and then reopened, I am asked to enter my browser extension password'
+    // 1101-BWAL-062 When entering a correct password decrypts my wallets
     await openNewInstanceOfVegaWallet()
-    const login = new Login(driver)
     await login.checkOnLoginPage()
     await login.login(testPassword)
-    const createWallet = new CreateAWallet(driver)
-    await createWallet.checkOnCreateWalletPage()
+    await viewWallet.checkOnViewWalletPage()
+    const keys = await viewWallet.getWalletKeys()
+    expect(keys.length, `expected to be able to view 1 wallet key, instead found ${keys.length}`).toBe(1)
+    const name = await viewWallet.getWalletName()
+    expect(name).toBe('Wallet 1')
   })
 
-  it('1101-BWAL-059 I am informed if I enter my password incorrectly', async () => {
+  it('shows error if password entered incorrectly, I can log in after fixing the error', async () => {
+    // 1101-BWAL-059 I am informed if I enter my password incorrectly
     await apiHelper.lockWallet()
     await navigateToLandingPage(driver)
-    const login = new Login(driver)
     await login.checkOnLoginPage()
     await login.login(testPassword + '1')
-    // Need to assert an error here.
+    const errorText = await login.getErrorText()
+    expect(errorText).toBe('Incorrect passphrase')
+    await login.login(testPassword)
+    await viewWallet.checkOnViewWalletPage()
   })
 
   async function openNewInstanceOfVegaWallet() {
--- test/e2e/page-objects/login.ts
+++ test/e2e/page-objects/login.ts
@@ -1,11 +1,20 @@
 import { By, WebDriver } from 'selenium-webdriver'
-import { clickElement, getByDataTestID, isElementDisplayed, sendKeysToElement } from '../selenium-util'
+import {
+  clearTextField,
+  clickElement,
+  getByDataTestID,
+  getElementText,
+  isElementDisplayed,
+  sendKeysToElement
+} from '../selenium-util'
 import 'jest-expect-message'
-import { loginButton, loginPassphrase } from '../../../src/locator-ids'
+import { errorMessage, loginButton, loginPassphrase } from '../../../src/locator-ids'
+import { clear } from 'console'
 
 export class Login {
   private readonly loginButton: By = getByDataTestID(loginButton)
   private readonly passwordField: By = getByDataTestID(loginPassphrase)
+  private readonly error: By = getByDataTestID(errorMessage)
 
   constructor(private readonly driver: WebDriver) {}
 
@@ -15,12 +24,17 @@ export class Login {
     }).toBe(true)
   }
 
+  async getErrorText() {
+    return await getElementText(this.driver, this.error)
+  }
+
   async isLoginPage() {
     return await isElementDisplayed(this.driver, this.loginButton)
   }
 
   async login(password: string) {
     await this.checkOnLoginPage()
+    await clearTextField(this.driver, this.passwordField)
     await sendKeysToElement(this.driver, this.passwordField, password)
     await clickElement(this.driver, this.loginButton)
   }

64f1aa8b56223f40cf46559b81c64367c33ea184
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-05-31 14:14:47 +0100

    push tests, these will fail but most should pass when outstanding backend bug fixed

---
+++ test/e2e/page-objects/transaction.ts
@@ -0,0 +1,27 @@
+import { By, WebDriver } from 'selenium-webdriver'
+import { clickElement, getByDataTestID, isElementDisplayed } from '../selenium-util'
+import 'jest-expect-message'
+import locators from '../../../src/components/locators'
+
+export class Transaction {
+  private readonly confirm: By = getByDataTestID(locators.transactionModalApproveButton)
+  private readonly reject: By = getByDataTestID(locators.transactionModalDenyButton)
+
+  constructor(private readonly driver: WebDriver) {}
+
+  async confirmTransaction() {
+    await clickElement(this.driver, this.confirm)
+  }
+
+  async rejectTransaction() {
+    await clickElement(this.driver, this.reject)
+  }
+
+  async checkOnTransactionPage() {
+    expect(
+      await isElementDisplayed(this.driver, this.confirm),
+      "expected to be on the confirm/reject transaction view but couldn't find the confirm button",
+      { showPrefix: false }
+    ).toBe(true)
+  }
+}

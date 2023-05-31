c3009fe3fa63eb5fad7fc1cb327e2d9bb463798a
Dexter <dexter@vega.xyz>
2023-05-31 18:41:59 +0100

    test: add defensive check to pow-worker

--- web-extension/common/pow-worker.js
+++ web-extension/common/pow-worker.js
@@ -3,6 +3,11 @@ import { PoW } from '@vegaprotocol/crypto'
 // Independent PoW solver as a web worker to unblock the background thread
 // the requests are JSON-RPC to ease the client implementation
 globalThis.onmessage = async function (ev) {
+  // FIXME: this is a hack to make the tests work because the pow-worker.js file is being incorrectly bundled
+  // into the application when it should be requested on demand instead.
+
+  // Question, why is this only happening on connect?
+  if (!ev.data || !ev.data.method || ev.data.method !== 'solve') return
   const id = ev.data.id
   const { difficulty, blockHash, tid, startNonce, endNonce } = ev.data.params
 

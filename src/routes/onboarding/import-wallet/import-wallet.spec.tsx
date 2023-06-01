c9ef7a05eb9cec91013116444eb57d1fd3ae36e7
Dexter <dexter@vega.xyz>
2023-06-01 11:42:22 +0100

    chore: add tests for basic import wallet functionality

--- src/routes/onboarding/import-wallet/import-wallet.spec.tsx
+++ src/routes/onboarding/import-wallet/import-wallet.spec.tsx
@@ -1,7 +1,10 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 import { ImportWallet } from '.'
 import { importMnemonic, importMnemonicDescription, importMnemonicSubmit } from '../../../locator-ids'
+import { JsonRPCProvider } from '../../../contexts/json-rpc/json-rpc-provider'
+import { mockClient } from '../../../test-helpers/mock-client'
+import { FULL_ROUTES } from '../../route-names'
 
 const mockedUsedNavigate = jest.fn()
 
@@ -10,13 +13,20 @@ jest.mock('react-router-dom', () => ({
   useNavigate: () => mockedUsedNavigate
 }))
 
-describe('ImportWallet', () => {
-  it('renders description, input and submit button', () => {
-    render(
+const renderComponent = () => {
+  render(
+    <JsonRPCProvider>
       <MemoryRouter>
         <ImportWallet />
       </MemoryRouter>
-    )
+    </JsonRPCProvider>
+  )
+}
+
+describe('ImportWallet', () => {
+  it('renders description, input and submit button', () => {
+    mockClient()
+    renderComponent()
     expect(screen.getByTestId(importMnemonicDescription)).toHaveTextContent(
       "Enter or paste in your Vega wallet's recovery phrase."
     )
@@ -24,16 +34,60 @@ describe('ImportWallet', () => {
     expect(screen.getByTestId(importMnemonicSubmit)).toHaveTextContent('Import wallet')
   })
 
-  it('after successfully importing a wallet it redirects to wallets page', () => {
-    render(
-      <MemoryRouter>
-        <ImportWallet />
-      </MemoryRouter>
-    )
+  it('after successfully importing a wallet it redirects to wallets page', async () => {
+    mockClient()
+    renderComponent()
+
     expect(screen.getByTestId(importMnemonicDescription)).toHaveTextContent(
       "Enter or paste in your Vega wallet's recovery phrase."
     )
-    expect(screen.getByTestId(importMnemonic)).toBeInTheDocument()
-    expect(screen.getByTestId(importMnemonicSubmit)).toHaveTextContent('Import wallet')
+    fireEvent.change(screen.getByTestId(importMnemonic), {
+      target: {
+        value:
+          'solid length discover gun swear nose artwork unfair vacuum canvas push hybrid owner wasp arrest mixed oak miss cage scatter tree harsh critic believe'
+      }
+    })
+    fireEvent.click(screen.getByTestId(importMnemonicSubmit))
+
+    await waitFor(() => expect(mockedUsedNavigate).toBeCalledWith(FULL_ROUTES.wallets))
+  })
+
+  it('renders error message if recovery phrase is invalid', async () => {
+    const listeners: Function[] = []
+    // @ts-ignore
+    global.browser = {
+      runtime: {
+        connect: () => ({
+          postMessage: (message: any) => {
+            listeners.map((fn) =>
+              fn({
+                jsonrpc: '2.0',
+                error: {
+                  message: 'Invalid recovery phrase',
+                  // TODO correct error code
+                  code: -32602
+                },
+                id: message.id
+              })
+            )
+          },
+          onmessage: (...args: any[]) => {
+            console.log('om', args)
+          },
+          onMessage: {
+            addListener: (fn: any) => {
+              listeners.push(fn)
+            }
+          }
+        })
+      }
+    }
+    renderComponent()
+    fireEvent.change(screen.getByTestId(importMnemonic), {
+      target: { value: 'bad mnemonic' }
+    })
+    fireEvent.click(screen.getByTestId(importMnemonicSubmit))
+    await screen.findByText('Invalid recovery phrase')
+    expect(screen.getByTestId(importMnemonicSubmit)).toBeDisabled()
   })
 })

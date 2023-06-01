cf9bafaef2c3150187a706279aa947766328e65e
Emil Bay <github@tixz.dk>
2023-06-01 12:38:06 +0200

    chore: minor nit

--- web-extension/common/pow-worker.js
+++ web-extension/common/pow-worker.js
@@ -2,10 +2,10 @@ import { PoW } from '@vegaprotocol/crypto'
 
 // Independent PoW solver as a web worker to unblock the background thread
 // the requests are JSON-RPC to ease the client implementation
-globalThis.onmessage = async function (ev) {
+globalThis.onmessage = async function(ev) {
   // FIXME: this is a hack to make the tests work because the pow-worker.js file is being incorrectly bundled
   // into the application when it should be requested on demand instead.
-  if (!ev.data || !ev.data.method || ev.data.method !== 'solve') return
+  if (ev.data?.method !== 'solve') return
   const id = ev.data.id
   const { difficulty, blockHash, tid, startNonce, endNonce } = ev.data.params
 

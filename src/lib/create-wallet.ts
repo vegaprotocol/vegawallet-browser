b2e7d9b620cbf917a6cf5e304a874699b98a249c
Dexter <dexter@vega.xyz>
2023-06-01 10:17:45 +0100

    chore: create create wallet helper

---
+++ src/lib/create-wallet.ts
@@ -0,0 +1,11 @@
+import JSONRPCClient from './json-rpc-client'
+import { RpcMethods } from './rpc-methods'
+
+export const createWallet = async (mnemonic: string, client: JSONRPCClient) => {
+  const walletName = 'Wallet 1'
+  await client.request(RpcMethods.ImportWallet, { recoveryPhrase: mnemonic, name: walletName })
+  await client.request(RpcMethods.GenerateKey, {
+    wallet: walletName,
+    name: `Key 1`
+  })
+}

c33d28dfb3744478d5634eb986e14d6be8615918
Dexter <dexter@vega.xyz>
2023-06-02 10:05:05 +0100

    chore: reabse issues

--- src/lib/create-wallet.ts
+++ src/lib/create-wallet.ts
@@ -1,5 +1,5 @@
+import { RpcMethods } from './client-rpc-methods'
 import JSONRPCClient from './json-rpc-client'
-import { RpcMethods } from './rpc-methods'
 
 export const createWallet = async (mnemonic: string, client: JSONRPCClient) => {
   const walletName = 'Wallet 1'

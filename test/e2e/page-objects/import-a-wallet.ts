3922ed20793ded9da033f9c2cc868aad125a2c32
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-06-05 14:53:15 +0100

    add some initial test coverage

---
+++ test/e2e/page-objects/import-a-wallet.ts
@@ -0,0 +1,48 @@
+import { By, WebDriver } from 'selenium-webdriver'
+import {
+  clearTextField,
+  clickElement,
+  getByDataTestID,
+  getElementText,
+  isElementDisplayed,
+  sendKeysToElement
+} from '../selenium-util'
+import 'jest-expect-message'
+import { errorMessage, importMnemonic, importMnemonicSubmit } from '../../../src/locator-ids'
+
+export class ImportAWallet {
+  private readonly recoveryPhraseField: By = getByDataTestID(importMnemonic)
+  private readonly errorText: By = getByDataTestID(errorMessage)
+  private readonly recoveryPhraseSubmitButton: By = getByDataTestID(importMnemonicSubmit)
+
+  constructor(private readonly driver: WebDriver) {}
+
+  async checkOnImportAWalletPage() {
+    expect(
+      await this.isImportAWalletPage(),
+      "expected to be on the 'Import a wallet' page but could not locate the recovery phrase field",
+      {
+        showPrefix: false
+      }
+    ).toBe(true)
+  }
+
+  async fillInRecoveryPhraseAndSubmit(recoveryPhrase: string) {
+    await clearTextField(this.driver, this.recoveryPhraseField)
+    await this.fillInRecoveryPhrase(recoveryPhrase)
+    await clickElement(this.driver, this.recoveryPhraseSubmitButton)
+  }
+
+  async fillInRecoveryPhrase(recoveryPhrase: string) {
+    await this.checkOnImportAWalletPage()
+    await sendKeysToElement(this.driver, this.recoveryPhraseField, recoveryPhrase)
+  }
+
+  async getErrorMessageText() {
+    return await getElementText(this.driver, this.errorText)
+  }
+
+  async isImportAWalletPage() {
+    return await isElementDisplayed(this.driver, this.recoveryPhraseField)
+  }
+}

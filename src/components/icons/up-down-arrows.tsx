a14f49ad949f810d542af54f0ad39bc91d4a2dc9
Dexter <dexter@vega.xyz>
2023-06-05 14:37:19 +0100

    chore: add icons

---
+++ src/components/icons/up-down-arrows.tsx
@@ -0,0 +1,15 @@
+export const UpDownArrows = ({ className }: { className: string }) => {
+  return (
+    <svg
+      className={className}
+      width="22"
+      height="22"
+      viewBox="0 0 22 22"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M15.5 11.5L15.5 1M15.5 1L21 6M15.5 1L10 6" stroke="currentColor" />
+      <path d="M6.5 10.5L6.5 21M6.5 21L1 16M6.5 21L12 16" stroke="currentColor" />
+    </svg>
+  )
+}

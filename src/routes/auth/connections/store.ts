0d3b09ab8819970004b24e422ef63a92a06ef53f
Dexter <dexter@vega.xyz>
2023-06-05 14:38:10 +0100

    feat: build connections page

---
+++ src/routes/auth/connections/store.ts
@@ -0,0 +1,39 @@
+import { create } from 'zustand'
+import JSONRPCClient from '../../../lib/json-rpc-client'
+import { RpcMethods } from '../../../lib/client-rpc-methods'
+
+export interface Connection {
+  allowList: AllowList
+  origin: string
+}
+
+export interface AllowList {
+  publicKeys: any[]
+  wallets: string[]
+}
+
+export type ConnectionsStore = {
+  connections: Connection[]
+  loading: boolean
+  error: string | null
+  loadConnections: (client: JSONRPCClient) => void
+  removeConnection: (client: JSONRPCClient, connection: Connection) => void
+}
+
+export const useConnectionStore = create<ConnectionsStore>()((set, get) => ({
+  connections: [],
+  loading: true,
+  error: null,
+  loadConnections: async (client: JSONRPCClient) => {
+    try {
+      set({ loading: true, error: null })
+      const { connections } = await client.request(RpcMethods.ListConnections, null)
+      set({ connections })
+    } catch (e) {
+      set({ error: e?.toString() || 'Something went wrong' })
+    } finally {
+      set({ loading: false })
+    }
+  },
+  removeConnection: async (client: JSONRPCClient, connection: Connection) => {}
+}))

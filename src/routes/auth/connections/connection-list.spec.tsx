70aa616f11786bc8ef097f0a9b181e21bb894af7
Dexter <dexter@vega.xyz>
2023-06-05 15:00:30 +0100

    test: add test for connection list

---
+++ src/routes/auth/connections/connection-list.spec.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react'
+import { ConnectionsList } from './connection-list'
+import locators from '../../../components/locators'
+
+describe('ConnectionList', () => {
+  it('renders list of connections passed in with image', () => {
+    render(
+      <ConnectionsList
+        connections={[
+          {
+            allowList: {
+              publicKeys: [],
+              wallets: ['Wallet 1']
+            },
+            origin: 'https://vega.xyz'
+          },
+          {
+            allowList: {
+              publicKeys: [],
+              wallets: ['Wallet 1']
+            },
+            origin: 'foo.com'
+          }
+        ]}
+      />
+    )
+    const connections = screen.getAllByTestId('connection')
+    expect(connections).toHaveLength(2)
+    const [vega, foo] = connections
+    expect(vega).toHaveTextContent('https://vega.xyz')
+    expect(foo).toHaveTextContent('foo.com')
+    const images = screen.getAllByTestId(locators.hostImage)
+    expect(images).toHaveLength(2)
+  })
+})

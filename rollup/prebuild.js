ad56139184202171bde2022c5dd487412b43d37f
Dexter <dexter@vega.xyz>
2023-06-07 12:50:54 +0100

    chore: create builds for chrome & firefox

--- rollup/prebuild.js
+++ rollup/prebuild.js
@@ -1,6 +1,10 @@
 import ajvPlugin from './helpers/ajv/ajv-plugin.js'
 import { folderInput } from 'rollup-plugin-folder-input'
 
+/**
+ * Generates the validation files for the client and admin schemas
+ * @returns {object[]} - The rollup configs for client and admin
+ */
 export default () => [
   {
     input: 'web-extension/common/schemas/client/*.js',

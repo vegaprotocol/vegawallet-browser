20dc54ba4383541fff4dcc5f8b6faefc4d9cc65d
Dexter <dexter@vega.xyz>
2023-06-09 10:58:57 +0100

    chore: make test scripts consistent, coverage for backend

--- test/jest-config/jest.config-backend.js
+++ test/jest-config/jest.config-backend.js
@@ -13,5 +13,8 @@ export default {
       }
     ]
   ],
+  collectCoverageFrom: ['**/*.js', '!**/node_modules/**'],
+  coverageDirectory: 'coverage/backend',
+  coverageReporters: ['html'],
   transformIgnorePatterns: ['/node_modules/(?!(@vegaprotocol)/protos)/']
 }

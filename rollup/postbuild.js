01b0f248b8e60d47d07a878a4e33b4958707d297
Dexter <dexter@vega.xyz>
2023-06-09 10:25:50 +0100

    fix: only put test keys into manifest if this is a test build

--- rollup/postbuild.js
+++ rollup/postbuild.js
@@ -11,7 +11,12 @@ const fileName = 'I_SHOULD_NOT_EXIST.js'
  * @param {string} commonFolder - The folder containing the common files
  * @param {string} build - The browser specific build folder
  */
-export default (browser, commonFolder, build) => {
+export default (browser, commonFolder, build, isTestBuild) => {
+  const testReplacements = isTestBuild
+    ? {
+        key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA08UzOSHIQYHM54WUFpkwdli9r64CjLvR0zQywfNvJEW808vRJF86esnXtgFn+XaPc/rKL1SguiGrhi0DJH6uzNEBs37q7kEsEzK+yxWu8OPGp8Bf6p1MnvT5m/44tXqcbdLll3K8rBrNq8PAGIbw7AI/dkLnK1UosVDkkO7CCqLYLDp0ccJTLs1ALS78o6Es9tg91DuTRJyNc1HP8rZn0FL+rjOkqAX+26rhy+UOwWvqe7FZbMU18ZsQ5Z/rFWAYnRG6+lWMMWYBsU2irwRLVPd4RydEr2JKeaNi9V42a7kAtDlYW9607LCOtXfAJTIA3g2zrtxvPBSLMO84abvGzwIDAQAB'
+      }
+    : {}
   return [
     {
       // Not actually used, but required by rollup. We're just using thr copy plugin.
@@ -34,7 +39,8 @@ export default (browser, commonFolder, build) => {
         mainfest({
           manifests: [`web-extension/common/manifest.json`, `web-extension/${browser}/manifest.json`],
           overrides: {
-            version: pkg.version
+            version: pkg.version,
+            ...testReplacements
           }
         }),
         copy({

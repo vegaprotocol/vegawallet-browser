e73ff15115c01937f8f5cb3510b6ea527d5582c2
Dexter <dexter@vega.xyz>
2023-06-13 10:43:14 +0100

    chore: uniq connections

--- frontend/stores/connections.ts
+++ frontend/stores/connections.ts
@@ -1,6 +1,8 @@
 import { create } from 'zustand'
 import JSONRPCClient from '../../lib/json-rpc-client'
 import { RpcMethods } from '../lib/client-rpc-methods'
+import uniq from 'lodash/uniq'
+import sortBy from 'lodash/sortBy'
 
 export interface Connection {
   allowList: AllowList
@@ -18,20 +20,25 @@ export type ConnectionsStore = {
   error: string | null
   loadConnections: (client: JSONRPCClient) => void
   addConnection: (connection: Connection) => void
+  setConnections: (connections: Connection[]) => void
 }
 
 export const useConnectionStore = create<ConnectionsStore>()((set, get) => ({
   connections: [],
   loading: true,
   error: null,
+  setConnections: (connections: Connection[]) => {
+    set({ connections: sortBy(uniq(connections), 'origin') })
+  },
   addConnection: (connection: Connection) => {
-    set({ connections: [...get().connections, connection] })
+    const newConnections = [...get().connections, connection]
+    get().setConnections(newConnections)
   },
   loadConnections: async (client: JSONRPCClient) => {
     try {
       set({ loading: true, error: null })
       const { connections } = await client.request(RpcMethods.ListConnections, null)
-      set({ connections })
+      get().setConnections(connections)
     } catch (e) {
       set({ error: e?.toString() || 'Something went wrong' })
     } finally {

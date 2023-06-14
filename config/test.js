814a615f9645cfbe19e77e25f31e4aa9a5c42240
Dexter <dexter@vega.xyz>
2023-06-14 15:58:40 +0100

    chore: change encryption to be in settings

--- config/test.js
+++ config/test.js
@@ -5,7 +5,10 @@ const test = {
   EXPLORER_URL: 'https://explorer.fairground.wtf', // TODO @Dale: update this
   FEEDBACK_LINK: 'https://github.com/vegaprotocol/feedback/discussions', // TODO @Dale: update this
   NODES: ['https://api.n06.testnet.vega.xyz', 'https://api.n07.testnet.vega.xyz'], // TODO @Dale: update this
-  LIGHT_ENCRYPTION: true
+  ENCRYPTION_SETTINGS: {
+    memory: 10,
+    iterations: 1
+  }
 }
 
 export default test

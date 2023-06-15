a164c90785707042c947d39773f42e7b8fb509dc
Dexter <dexter@vega.xyz>
2023-06-15 14:15:00 +0100

    feat: prevent window being resized

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,8 +2,22 @@ import { HashRouter as Router } from 'react-router-dom'
 import { Routing } from './routes'
 import { JsonRPCProvider } from './contexts/json-rpc/json-rpc-provider'
 import GlobalErrorBoundary from './components/global-error-boundary'
+import { useLayoutEffect } from 'react'
+
+function usePreventWindowResize() {
+  useLayoutEffect(() => {
+    function resetSize() {
+      const frameHeight = window.outerHeight - window.innerHeight
+      window.resizeTo(360, 600 + frameHeight)
+    }
+    window.addEventListener('resize', resetSize)
+    resetSize()
+    return () => window.removeEventListener('resize', resetSize)
+  }, [])
+}
 
 function App() {
+  usePreventWindowResize()
   return (
     <Router>
       <GlobalErrorBoundary>

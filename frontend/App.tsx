c4b25864d97da6bd520a7d4be5afa897821729d3
Dexter <dexter@vega.xyz>
2023-06-19 17:45:39 +0100

    chore: adjust height

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,18 +2,18 @@ import { HashRouter as Router } from 'react-router-dom'
 import { Routing } from './routes'
 import { JsonRPCProvider } from './contexts/json-rpc/json-rpc-provider'
 import GlobalErrorBoundary from './components/global-error-boundary'
-import { useLayoutEffect } from 'react'
+import { useLayoutEffect, useMemo } from 'react'
 
 function usePreventWindowResize() {
+  const frameHeight = useMemo(() => window.outerHeight - window.innerHeight, [])
   useLayoutEffect(() => {
     function resetSize() {
-      const frameHeight = window.outerHeight - window.innerHeight
       window.resizeTo(360, 600 + frameHeight)
     }
     window.addEventListener('resize', resetSize)
     resetSize()
     return () => window.removeEventListener('resize', resetSize)
-  }, [])
+  }, [frameHeight])
 }
 
 function App() {

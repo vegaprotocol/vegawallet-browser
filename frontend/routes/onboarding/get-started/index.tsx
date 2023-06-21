ea83ef117b7fe0f4aeb67a8af00d25ad11eee3f1
Dexter <dexter@vega.xyz>
2023-06-21 10:52:41 +0100

    perf: hyper minor perf increase

--- frontend/routes/onboarding/get-started/index.tsx
+++ frontend/routes/onboarding/get-started/index.tsx
@@ -3,20 +3,20 @@ import { useNavigate } from 'react-router-dom'
 import { FULL_ROUTES } from '../../route-names'
 import { Frame } from '../../../components/frame'
 import { Tick } from '../../../components/icons/tick'
-import { useMemo } from 'react'
 import { StarsWrapper } from '../../../components/stars-wrapper'
 import { getStartedButton } from '../../../locator-ids'
 import { VegaHeader } from '../../../components/vega-header'
 
+const ITEMS = ['Securely connect to Vega dapps', 'Instantly approve and reject transactions']
+
 export const GetStarted = () => {
-  const items = useMemo(() => ['Securely connect to Vega dapps', 'Instantly approve and reject transactions'], [])
   const navigate = useNavigate()
   return (
     <StarsWrapper>
       <VegaHeader />
       <Frame>
         <ul className="list-none text-left">
-          {items.map((i) => (
+          {ITEMS.map((i) => (
             <li key={i} className="flex">
               <div>
                 <Tick className="w-3 mr-2 text-vega-green-550" />

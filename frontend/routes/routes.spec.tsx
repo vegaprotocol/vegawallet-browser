fe87957a0d859568ffefbccd5b521e4ee91095d8
Dexter <dexter@vega.xyz>
2023-06-21 11:03:37 +0100

    feat: create telemetery page

--- frontend/routes/routes.spec.tsx
+++ frontend/routes/routes.spec.tsx
@@ -13,6 +13,7 @@ jest.mock('./onboarding/create-password')
 jest.mock('./onboarding/import-wallet')
 jest.mock('./onboarding/create-wallet')
 jest.mock('./onboarding/save-mnemonic')
+jest.mock('./onboarding/telemetry')
 
 jest.mock('./auth')
 jest.mock('./auth/wallets')

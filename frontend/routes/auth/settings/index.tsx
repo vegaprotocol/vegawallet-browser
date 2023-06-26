be44464b7459323ce313d21f839f0c56e6b4b890
Dexter <dexter@vega.xyz>
2023-06-26 14:11:13 +0100

    feat: implement settings page design

--- frontend/routes/auth/settings/index.tsx
+++ frontend/routes/auth/settings/index.tsx
@@ -1,6 +1,6 @@
-import { Button, ExternalLink } from '@vegaprotocol/ui-toolkit'
+import { Button, ExternalLink, Radio, RadioGroup } from '@vegaprotocol/ui-toolkit'
 import { useForm } from 'react-hook-form'
-import { useCallback } from 'react'
+import { ReactNode, useCallback, useState } from 'react'
 import packageJson from '../../../../package.json'
 import { useJsonRpcClient } from '../../../contexts/json-rpc/json-rpc-context'
 import { useNavigate } from 'react-router-dom'
@@ -18,7 +18,18 @@ export const locators = {
   settingsOpenPopoutButton: 'settings-open-popout-button'
 }
 
+const SettingsSection = ({ children }: { children: ReactNode }) => (
+  <div className="pt-6 border-b border-1 border-vega-dark-150 pb-6">{children}</div>
+)
+
+const SettingsHeader = ({ text }: { text: string }) => (
+  <div className="text-vega-dark-300 text-sm uppercase" data-testid={locators.settingsVersionTitle}>
+    {text}
+  </div>
+)
+
 export const Settings = () => {
+  const [telemetryEnabled, setTelemetryEnabled] = useState(false)
   const { client } = useJsonRpcClient()
   const navigate = useNavigate()
   const { handleSubmit: handleLock } = useForm()
@@ -37,21 +48,36 @@ export const Settings = () => {
   return (
     <section data-testid={locators.settingsPage}>
       <h1 className="flex justify-center flex-col text-2xl text-white">Settings</h1>
-      <div className="pt-6 border-b border-1 border-vega-dark-150 pb-6">
-        <div className="text-dark-300 text-sm uppercase" data-testid={locators.settingsVersionTitle}>
-          Vega wallet version
-        </div>
+      <SettingsSection>
+        <SettingsHeader text="Vega wallet version" />
         <div className="text-white text-lg mt-1" data-testid={locators.settingsVersionNumber}>
           {packageJson.version}
         </div>
-      </div>
-      <form className="pt-6" onSubmit={handleLock(lock)}>
-        <Button data-testid={locators.settingsLockButton} fill={true} variant="secondary" type="submit">
-          Lock
-        </Button>
-      </form>
+      </SettingsSection>
 
-      <div className="pt-6 border-b border-1 border-vega-dark-150 pb-6">
+      <SettingsSection>
+        <SettingsHeader text="Vega wallet version" />
+        <p className="my-4">Improve Vega Wallet by automatically reporting bugs and crashes.</p>
+        <RadioGroup
+          onChange={(value) => {
+            setTelemetryEnabled(value === 'true')
+          }}
+          value={telemetryEnabled.toString()}
+        >
+          <Radio id="associate-radio-contract" label="Yes" value="true" />
+          <Radio id="associate-radio-wallet" label="No" value="false" />
+        </RadioGroup>
+        <ExternalLink className="text-white" href={config.userDataPolicy}>
+          Read Vega Wallet's user data policy
+        </ExternalLink>
+      </SettingsSection>
+
+      <SettingsSection>
+        <form className="pb-6" onSubmit={handleLock(lock)}>
+          <Button data-testid={locators.settingsLockButton} fill={true} variant="secondary" type="submit">
+            Lock
+          </Button>
+        </form>
         <div data-testid={locators.settingsFeedbackDescription}>Spotted any issues or bugs?</div>
         <ExternalLink
           data-testid={locators.settingsFeedbackLink}
@@ -60,11 +86,11 @@ export const Settings = () => {
         >
           Provide feedback
         </ExternalLink>
-      </div>
+      </SettingsSection>
       <ExternalLink
         onClick={handlePopout(popout)}
         data-testid={locators.settingsOpenPopoutButton}
-        className="text-white mt-6"
+        className="text-white my-6"
         type="submit"
       >
         Open wallet in a new window

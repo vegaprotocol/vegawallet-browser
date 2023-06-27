710596f1df50759fb137ad436844ba1ad7d13aae
Dexter <dexter@vega.xyz>
2023-06-27 14:07:34 +0100

    test: write jest tests for new auth behaviour

--- frontend/routes/auth/auth.spec.tsx
+++ frontend/routes/auth/auth.spec.tsx
@@ -2,12 +2,20 @@ import { render, screen } from '@testing-library/react'
 import { Auth } from '.'
 import { MemoryRouter } from 'react-router-dom'
 import locators from '../../components/locators'
-import { JsonRPCProvider } from '../../contexts/json-rpc/json-rpc-provider'
-import { mockClient } from '../../test-helpers/mock-client'
+
+import { useWalletStore } from '../../stores/wallets'
 
 jest.mock('react-router-dom', () => ({
   ...(jest.requireActual('react-router-dom') as any),
   Outlet: () => <div data-testid="outlet" />
+}))
+
+jest.mock('../../contexts/json-rpc/json-rpc-context', () => ({
+  useJsonRpcClient: () => ({ client: jest.fn() })
+}))
+
+jest.mock('../../stores/wallets', () => ({
+  useWalletStore: jest.fn()
 }))
 
 jest.mock('../../components/connection-modal', () => ({
@@ -18,22 +26,29 @@ jest.mock('../../components/transaction-modal', () => ({
   TransactionModal: () => <div data-testid="transaction-modal" />
 }))
 
+const renderComponent = () => {
+  return render(
+    <MemoryRouter>
+      <Auth />
+    </MemoryRouter>
+  )
+}
+
 describe('Auth', () => {
   it('renders outlet, header and navbar', () => {
-    mockClient()
-    render(
-      <JsonRPCProvider>
-        <MemoryRouter>
-          <Auth />
-        </MemoryRouter>
-      </JsonRPCProvider>
-    )
+    ;(useWalletStore as unknown as jest.Mock).mockReturnValue({ loadWallets: jest.fn() })
+    renderComponent()
+
     expect(screen.getByTestId(locators.navBar)).toBeInTheDocument()
     expect(screen.getByTestId('outlet')).toBeInTheDocument()
     expect(screen.getByTestId('connection-modal')).toBeInTheDocument()
     expect(screen.getByTestId('transaction-modal')).toBeInTheDocument()
   })
   it('loads the users wallets', () => {
-    expect(false).toBe(true)
+    const mockLoad = jest.fn()
+    ;(useWalletStore as unknown as jest.Mock).mockReturnValue({ loadWallets: mockLoad })
+    renderComponent()
+
+    expect(mockLoad).toBeCalledTimes(1)
   })
 })

e374091200b379ba59f3435794b8f74cccb32afd
Dexter <dexter@vega.xyz>
2023-06-27 13:15:02 +0100

    feat: replace vega keys with sanitised strings

--- frontend/routes/auth/index.tsx
+++ frontend/routes/auth/index.tsx
@@ -4,8 +4,18 @@ import { NavBar } from '../../components/navbar'
 import { ConnectionModal } from '../../components/connection-modal'
 import { TransactionModal } from '../../components/transaction-modal'
 import { PageHeader } from '../../components/page-header'
+import { useWalletStore } from './wallets/store'
+import { useEffect } from 'react'
+import { useJsonRpcClient } from '../../contexts/json-rpc/json-rpc-context'
 
 export const Auth = () => {
+  const { client } = useJsonRpcClient()
+  const { loadWallets } = useWalletStore((state) => ({
+    loadWallets: state.loadWallets
+  }))
+  useEffect(() => {
+    loadWallets(client)
+  }, [client, loadWallets])
   return (
     <div className="h-full w-full grid grid-rows-[1fr_min-content] overflow-y-auto">
       <ConnectionModal />

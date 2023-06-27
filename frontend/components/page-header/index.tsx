f1e48dc7c43135bfb13695a869c6589c04dc1ea3
Dexter <dexter@vega.xyz>
2023-06-27 16:46:30 +0100

    feat: open in new window icon

--- frontend/components/page-header/index.tsx
+++ frontend/components/page-header/index.tsx
@@ -3,27 +3,40 @@ import { VegaIcon } from '../icons/vega-icon'
 import locators from '../locators'
 import config from '../../lib/config'
 import { ExpandIcon } from '../icons/expand'
+import { useJsonRpcClient } from '../../contexts/json-rpc/json-rpc-context'
+import { useCallback } from 'react'
+import { RpcMethods } from '../../lib/client-rpc-methods'
+
+const useOpenInNewWindow = () => {
+  const { client } = useJsonRpcClient()
+
+  return useCallback(async () => {
+    await client.request(RpcMethods.OpenPopout, null)
+    if (config.closeWindowOnPopupOpen) {
+      window.close()
+    }
+  }, [client])
+}
 
 export const PageHeader = () => {
+  const open = useOpenInNewWindow()
   return (
-    <>
-      <div
-        data-testid={locators.pageHeader}
-        className="p-3 flex justify-between items-center border-b border-1 border-vega-dark-150"
-      >
-        <VegaIcon size={40} backgroundColor="none" />
-        <div className="flex justify-between items center">
-          <div
-            data-testid={networkIndicator}
-            className="flex flex-col justify-center border rounded-md border-vega-dark-300 text-sm px-2 h-6"
-          >
-            {config.network.name}
-          </div>
-          <button className="border rounded-md border-vega-dark-300 text-sm h-6 ml-3 px-1">
-            <ExpandIcon />
-          </button>
+    <div
+      data-testid={locators.pageHeader}
+      className="p-3 flex justify-between items-center border-b border-1 border-vega-dark-150"
+    >
+      <VegaIcon size={40} backgroundColor="none" />
+      <div className="flex justify-between items center">
+        <div
+          data-testid={networkIndicator}
+          className="flex flex-col justify-center border rounded-md border-vega-dark-300 text-sm px-2 h-6"
+        >
+          {config.network.name}
         </div>
+        <button onClick={open} className="border rounded-md border-vega-dark-300 text-sm h-6 ml-3 px-1">
+          <ExpandIcon />
+        </button>
       </div>
-    </>
+    </div>
   )
 }

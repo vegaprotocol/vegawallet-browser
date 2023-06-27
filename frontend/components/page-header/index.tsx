2f6633a085731d484486c92b97fcc33240f35d40
Dexter <dexter@vega.xyz>
2023-06-27 23:39:29 +0100

    chore: add unit tests

--- frontend/components/page-header/index.tsx
+++ frontend/components/page-header/index.tsx
@@ -1,11 +1,15 @@
-import { networkIndicator } from '../../locator-ids'
 import { VegaIcon } from '../icons/vega-icon'
-import locators from '../locators'
 import config from '../../lib/config'
 import { ExpandIcon } from '../icons/expand'
 import { useJsonRpcClient } from '../../contexts/json-rpc/json-rpc-context'
 import { useCallback } from 'react'
 import { RpcMethods } from '../../lib/client-rpc-methods'
+
+export const locators = {
+  pageHeader: 'page-header',
+  networkIndicator: 'network-indicator',
+  openPopoutButton: 'open-popout-button'
+}
 
 const useOpenInNewWindow = () => {
   const { client } = useJsonRpcClient()
@@ -28,12 +32,16 @@ export const PageHeader = () => {
       <VegaIcon size={40} backgroundColor="none" />
       <div className="flex justify-between items center">
         <div
-          data-testid={networkIndicator}
+          data-testid={locators.networkIndicator}
           className="flex flex-col justify-center border rounded-md border-vega-dark-300 text-sm px-2 h-6"
         >
           {config.network.name}
         </div>
-        <button onClick={open} className="border rounded-md border-vega-dark-300 text-sm h-6 ml-3 px-1">
+        <button
+          data-testid={locators.openPopoutButton}
+          onClick={open}
+          className="border rounded-md border-vega-dark-300 text-sm h-6 ml-3 px-1"
+        >
           <ExpandIcon />
         </button>
       </div>

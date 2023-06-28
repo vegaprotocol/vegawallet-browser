300ac1cacc050cb3fe19ce3d9b3ea24e409899bd
Dexter <dexter@vega.xyz>
2023-06-28 02:21:56 +0100

    test: new tests for host image

--- frontend/components/host-image/host-image.spec.tsx
+++ frontend/components/host-image/host-image.spec.tsx
@@ -1,18 +1,31 @@
 import { fireEvent, render, screen } from '@testing-library/react'
-import { HostImage } from '.'
-import locators from '../locators'
+import { HostImage, locators } from '.'
 
 describe('HostImage', () => {
   it('render host image at size specified', () => {
-    render(<HostImage hostname="https://www.google.com" size={1} />)
+    render(<HostImage hostname="https://www.google.com" size={4} />)
     const img = screen.getByTestId(locators.hostImage)
     expect(img).toHaveStyle('width: 4px; height: 4px;')
   })
 
-  it('falls back to a question mark if URL does not exist', () => {
+  it('falls back square with domain letter if no image is available', () => {
+    render(<HostImage hostname="https://www.google.com" />)
+    const img = screen.getByTestId(locators.hostImage)
+    fireEvent.error(img)
+    expect(screen.getByTestId(locators.hostImageFallback)).toHaveTextContent('g')
+  })
+
+  it('falls back to a question mark if URL cannot be parsed', () => {
     render(<HostImage hostname="f" />)
     const img = screen.getByTestId(locators.hostImage)
     fireEvent.error(img)
-    expect(img).toHaveAttribute('src', './question-mark.png')
+    expect(screen.getByTestId(locators.hostImageFallback)).toHaveTextContent('?')
+  })
+
+  it('falls back to a question mark if URL is not listed', () => {
+    render(<HostImage hostname="https://www.google.comkjasbdfjhasfsd" />)
+    const img = screen.getByTestId(locators.hostImage)
+    fireEvent.error(img)
+    expect(screen.getByTestId(locators.hostImageFallback)).toHaveTextContent('?')
   })
 })

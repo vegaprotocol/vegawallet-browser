cb04e0772e61daac6cf114b70254509b8491831c
Dexter <dexter@vega.xyz>
2023-06-28 10:45:01 +0100

    chore: add test for store

--- frontend/stores/connections.spec.tsx
+++ frontend/stores/connections.spec.tsx
@@ -1,17 +1,22 @@
-import { silenceErrors } from '../test-helpers/silence-errors'
+import { RpcMethods } from '../lib/client-rpc-methods'
 import { useConnectionStore } from './connections'
 
-const request = () => {
-  return {
-    connections: [
-      {
-        allowList: {
-          publicKeys: [],
-          wallets: ['Wallet 1']
-        },
-        origin: 'https://vega.xyz'
+const client = {
+  request(method: string) {
+    if (method === RpcMethods.ListConnections) {
+      return {
+        connections: [
+          {
+            allowList: {
+              publicKeys: [],
+              wallets: ['Wallet 1']
+            },
+            origin: 'https://vega.xyz'
+          }
+        ]
       }
-    ]
+    }
+    return null
   }
 }
 
@@ -71,5 +76,93 @@ describe('Store', () => {
         origin: 'https://vega.xyz'
       }
     ])
+  })
+  it('renders error if error is present', async () => {
+    jest.spyOn(console, 'error').mockImplementation(() => {})
+    await useConnectionStore.getState().loadConnections({
+      request() {
+        throw new Error('Something sideways')
+      }
+    } as unknown as any)
+    expect(useConnectionStore.getState().error).toStrictEqual('Error: Something sideways')
+    expect(useConnectionStore.getState().loading).toBe(false)
+    expect(useConnectionStore.getState().connections).toStrictEqual([])
+  })
+  it('renders generic error if error message is not present', async () => {
+    jest.spyOn(console, 'error').mockImplementation(() => {})
+    await useConnectionStore.getState().loadConnections({
+      request() {
+        // eslint-disable-next-line no-throw-literal
+        throw null
+      }
+    } as unknown as any)
+    expect(useConnectionStore.getState().error).toStrictEqual('Something went wrong')
+    expect(useConnectionStore.getState().loading).toBe(false)
+    expect(useConnectionStore.getState().connections).toStrictEqual([])
+  })
+  it('removes connections from backend', async () => {
+    const mockConnection = {
+      allowList: {
+        publicKeys: [],
+        wallets: ['Wallet 1']
+      },
+      origin: 'https://vega.xyz'
+    }
+    useConnectionStore.setState({
+      connections: [mockConnection]
+    })
+
+    await useConnectionStore.getState().removeConnection(client as unknown as any, mockConnection)
+    expect(useConnectionStore.getState().connections).toStrictEqual([])
+  })
+  it('renders error if connections could not be removed', async () => {
+    jest.spyOn(console, 'error').mockImplementation(() => {})
+    const mockConnection = {
+      allowList: {
+        publicKeys: [],
+        wallets: ['Wallet 1']
+      },
+      origin: 'https://vega.xyz'
+    }
+    useConnectionStore.setState({
+      connections: [mockConnection]
+    })
+
+    await useConnectionStore.getState().removeConnection(
+      {
+        request() {
+          throw new Error('Something sideways')
+        }
+      } as unknown as any,
+      mockConnection
+    )
+    expect(useConnectionStore.getState().error).toStrictEqual('Error: Something sideways')
+    expect(useConnectionStore.getState().connections).toStrictEqual([mockConnection])
+  })
+
+  it('renders generic error if error message is not present when removing connection', async () => {
+    jest.spyOn(console, 'error').mockImplementation(() => {})
+    const mockConnection = {
+      allowList: {
+        publicKeys: [],
+        wallets: ['Wallet 1']
+      },
+      origin: 'https://vega.xyz'
+    }
+    useConnectionStore.setState({
+      connections: [mockConnection]
+    })
+
+    await useConnectionStore.getState().removeConnection(
+      {
+        request() {
+          // eslint-disable-next-line no-throw-literal
+          throw null
+        }
+      } as unknown as any,
+      mockConnection
+    )
+    expect(useConnectionStore.getState().error).toStrictEqual('Something went wrong')
+    expect(useConnectionStore.getState().connections).toStrictEqual([mockConnection])
   })
 })

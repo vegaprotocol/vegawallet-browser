726abe93eebaab90e1d8527f465b701cbb4745e4
Dexter <dexter@vega.xyz>
2023-06-28 13:54:38 +0100

    test: make e2e test make further assertions

--- test/e2e/list-connections.spec.ts
+++ test/e2e/list-connections.spec.ts
@@ -1,21 +1,29 @@
 import { WebDriver } from 'selenium-webdriver'
 import { captureScreenshot, initDriver } from './driver'
-import { ViewWallet } from './page-objects/view-wallet'
 import { navigateToLandingPage } from './wallet-helpers/common'
 import { APIHelper } from './wallet-helpers/api-helpers'
 import { NavPanel } from './page-objects/navpanel'
 import { VegaAPI } from './wallet-helpers/vega-api'
 import { ConnectWallet } from './page-objects/connect-wallet'
 
+const transferReq = {
+  fromAccountType: 4,
+  toAccountType: 4,
+  amount: '1',
+  asset: 'fc7fd956078fb1fc9db5c19b88f0874c4299b2a7639ad05a47a28c0aef291b55',
+  to: 'fc7fd956078fb1fc9db5c19b88f0874c4299b2a7639ad05a47a28c0aef291b55',
+  kind: {
+    oneOff: {}
+  }
+}
+
 describe('list connections tests', () => {
   let driver: WebDriver
-  let viewWallet: ViewWallet
   let navPanel: NavPanel
   let connectWallet: ConnectWallet
 
   beforeEach(async () => {
     driver = await initDriver()
-    viewWallet = new ViewWallet(driver)
     await navigateToLandingPage(driver)
     navPanel = new NavPanel(driver)
     connectWallet = new ConnectWallet(driver)
@@ -59,18 +67,43 @@ describe('list connections tests', () => {
   })
 
   it('allows disconnecting of a dapp', async () => {
+    // TODO this opens 4 windows rather than reusing the same ones for dApp 1 and 2
+    // this should be fixed
     // 1109-VCON-006 I can choose to disconnect a dapp connection (and it's pre-approved status i.e. the next time I want to connect the dapp I am asked to approve the connection)
     const connections = await navPanel.goToListConnections()
     await connections.checkNoConnectionsExist()
-    const windowHandle = await driver.getWindowHandle()
-    const firstDapp = new VegaAPI(driver, windowHandle, 'https://vegaprotocol.github.io/vegawallet-browser/')
+
+    const firstWindowHandle = await driver.getWindowHandle()
+    const firstDapp = new VegaAPI(driver, firstWindowHandle, 'https://vegaprotocol.github.io/vegawallet-browser/')
     await firstDapp.connectWallet()
     const connectWalletModal = new ConnectWallet(driver)
     await connectWalletModal.approveConnectionAndCheckSuccess()
-    await connections.checkNumConnections(1)
+
+    const secondWindowHandle = await driver.getWindowHandle()
+    const secondDapp = new VegaAPI(driver, secondWindowHandle, 'https://vega.xyz')
+    await secondDapp.connectWallet()
+    await connectWalletModal.approveConnectionAndCheckSuccess()
+    await connections.checkNumConnections(2)
+
+    const keys = await firstDapp.listKeys()
+
     await connections.disconnectConnection('https://vegaprotocol.github.io')
-    await connections.checkNoConnectionsExist()
+    await connections.checkNumConnections(1)
+    let connectionNames = await connections.getConnectionNames()
+    expect(connectionNames[0]).toContain('https://vega.xyz')
+
+    // Check that the disconnected dapp cannot send a transaction
+    await firstDapp.sendTransaction(keys[0].publicKey, { transfer: transferReq })
+    const res = await firstDapp.getTransactionResult()
+    expect(res).toBe('Not connected')
+
+    // Check the first dApp needs to reconnect
     await firstDapp.connectWallet()
     await connectWallet.checkOnConnectWallet()
+    await connectWallet.denyConnection()
+
+    // Check the second dApp does not need to reconnect
+    await secondDapp.connectWallet()
+    await connections.checkOnListConnectionsPage()
   })
 })

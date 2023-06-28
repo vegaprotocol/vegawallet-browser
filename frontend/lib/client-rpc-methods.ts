4ab71459a2442b4f7b77792382e09d5c3f55ba7f
Dexter <dexter@vega.xyz>
2023-06-28 10:29:30 +0100

    chore: add new RPC method, tidy RPC method map

--- frontend/lib/client-rpc-methods.ts
+++ frontend/lib/client-rpc-methods.ts
@@ -1,14 +1,24 @@
 export const RpcMethods = {
+  // Wallets
   ListWallets: 'admin.list_wallets',
   ListKeys: 'admin.list_keys',
-  AppGlobals: 'admin.app_globals',
+  GenerateKey: 'admin.generate_key',
+
+  // Onboarding
   CreatePassphrase: 'admin.create_passphrase',
   GenerateRecoveryPhrase: 'admin.generate_recovery_phrase',
   ImportWallet: 'admin.import_wallet',
-  GenerateKey: 'admin.generate_key',
+  AppGlobals: 'admin.app_globals',
+
+  // Login
   Lock: 'admin.lock',
+  Unlock: 'admin.unlock',
+  OpenPopout: 'admin.open_popout',
+
+  // Connections
   ListConnections: 'admin.list_connections',
   ConnectionsChange: 'admin.connections_change',
   OpenPopout: 'admin.open_popout',
-  UpdateSettings: 'admin.update_app_settings'
+  UpdateSettings: 'admin.update_app_settings',
+  RemoveConnection: 'admin.remove_connection'
 }

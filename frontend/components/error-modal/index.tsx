e6767554f010c1f0b2fd8a11e0a84d85e98bb2d5
Dexter <dexter@vega.xyz>
2023-06-29 15:24:13 +0100

    chore: make global error boundary sensitive to async errors as well

--- frontend/components/error-modal/index.tsx
+++ frontend/components/error-modal/index.tsx
@@ -4,14 +4,18 @@ import { Warning } from '../icons/warning'
 import { Splash } from '../splash'
 import locators from '../locators'
 
-export const ErrorModal = ({ error, onClose }: { error: Error; onClose: () => void }) => {
+export const ErrorModal = ({ error, onClose }: { error: Error | null; onClose: () => void }) => {
   return (
     <Splash data-testid={locators.errorModal}>
       <section className="text-center">
         <h1 className="text-3xl mb-4">Something's gone wrong 🙃</h1>
         <Warning className="w-12 text-vega-pink" />
         <div className="my-5">
-          <CodeWindow content={error.message} text={error.message} />
+          {error ? (
+            <CodeWindow content={error.message} text={error.message} />
+          ) : (
+            <CodeWindow content={'An unknown error occurred'} text={'An unknown error occurred'} />
+          )}
         </div>
         <Button data-testid={locators.errorModalClose} onClick={onClose}>
           Close
--- frontend/components/global-error-boundary/index.tsx
+++ frontend/components/global-error-boundary/index.tsx
@@ -2,13 +2,19 @@ import { Component, ComponentType, ErrorInfo, ReactNode } from 'react'
 import { ErrorModal } from '../error-modal'
 import { NavigateFunction, useNavigate } from 'react-router-dom'
 import { FULL_ROUTES } from '../../routes/route-names'
+import { useErrorStore } from '../../stores/error'
 
-interface ExtendedProps {
+interface RouterProps {
   navigate: NavigateFunction
 }
 
-function withRouter<P extends ExtendedProps>(Component: ComponentType<P>) {
-  const Wrapper = (props: Omit<P, keyof ExtendedProps>) => {
+interface ErrorProps {
+  error: Error | null
+  setError: (error: Error | null) => void
+}
+
+function withRouter<P extends RouterProps>(Component: ComponentType<P>) {
+  const Wrapper = (props: Omit<P, keyof RouterProps>) => {
     const navigate = useNavigate()
     const routerProps = { navigate }
 
@@ -18,7 +24,20 @@ function withRouter<P extends ExtendedProps>(Component: ComponentType<P>) {
   return Wrapper
 }
 
-interface Props extends ExtendedProps {
+function withErrorStore<P extends ErrorProps>(Component: ComponentType<P>) {
+  const Wrapper = (props: Omit<P, keyof ErrorProps>) => {
+    const setError = useErrorStore((state) => ({
+      setError: state.setError
+    }))
+    const errorProps = { setError }
+
+    return <Component {...errorProps} {...(props as P)} />
+  }
+
+  return Wrapper
+}
+
+interface Props extends RouterProps, ErrorProps {
   children?: ReactNode
 }
 
@@ -42,14 +61,22 @@ class GlobalErrorBoundary extends Component<Props, State> {
   }
 
   public render() {
+    // This error is caught by the boundary
     const { error } = this.state
-    if (error) {
-      // You can render any custom fallback UI
+    // This error is reported from backend interactions, which are async
+    // and as such not caught by the global error boundary
+    const { error: storeError } = this.props
+    if (error || storeError) {
+      const err = error || storeError
+      // If this is a reported error then ensure this component is also aware of it
+      this.setState({ error: err })
+      this.props.setError(err)
       return (
         <ErrorModal
-          error={error}
+          error={err}
           onClose={() => {
             this.setState({ error: null })
+            this.props.setError(null)
             this.props.navigate(FULL_ROUTES.home)
           }}
         />
@@ -60,4 +87,4 @@ class GlobalErrorBoundary extends Component<Props, State> {
   }
 }
 
-export default withRouter(GlobalErrorBoundary)
+export default withErrorStore(withRouter(GlobalErrorBoundary))

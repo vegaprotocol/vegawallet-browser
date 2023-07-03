7d1ccfd022ec2597700fe7cd1ec9d8eb25e7eacc
Dexter <dexter@vega.xyz>
2023-07-03 11:42:07 +0100

    chore: move prevent resize hook into its' own component

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,19 +2,7 @@ import { HashRouter as Router } from 'react-router-dom'
 import { Routing } from './routes'
 import { JsonRPCProvider } from './contexts/json-rpc/json-rpc-provider'
 import GlobalErrorBoundary from './components/global-error-boundary'
-import { useLayoutEffect, useMemo } from 'react'
-
-function usePreventWindowResize() {
-  const frameHeight = useMemo(() => window.outerHeight - window.innerHeight, [])
-  useLayoutEffect(() => {
-    function resetSize() {
-      window.resizeTo(360, Math.max(window.outerHeight, 600 + frameHeight))
-    }
-    window.addEventListener('resize', resetSize)
-    resetSize()
-    return () => window.removeEventListener('resize', resetSize)
-  }, [frameHeight])
-}
+import { usePreventWindowResize } from './hooks/prevent-window-reszie'
 
 function App() {
   usePreventWindowResize()

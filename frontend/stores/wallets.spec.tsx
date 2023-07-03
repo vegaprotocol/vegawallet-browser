c859b991d4816f28b2e786315a52bb18bcc2f9cb
Dexter <dexter@vega.xyz>
2023-07-03 11:15:50 +0100

    test: fix wallet tests

--- frontend/stores/wallets.spec.tsx
+++ frontend/stores/wallets.spec.tsx
@@ -15,19 +15,17 @@ const keys = [
   }
 ]
 
-const client = {
-  request(method: string) {
-    if (method === RpcMethods.ListWallets) {
-      return { wallets: ['Wallet 1'] }
-    } else if (method === RpcMethods.ListKeys) {
-      return {
-        keys
-      }
-    } else if (method === RpcMethods.GenerateKey) {
-      return {
-        publicKey: '17248acbd899061ba9c5f3ab47791df2045c8e249f1805a04c2a943160533673',
-        name: 'Key 2'
-      }
+const request = (method: string) => {
+  if (method === RpcMethods.ListWallets) {
+    return { wallets: ['Wallet 1'] }
+  } else if (method === RpcMethods.ListKeys) {
+    return {
+      keys
+    }
+  } else if (method === RpcMethods.GenerateKey) {
+    return {
+      publicKey: '17248acbd899061ba9c5f3ab47791df2045c8e249f1805a04c2a943160533673',
+      name: 'Key 2'
     }
   }
 }
@@ -41,7 +39,7 @@ describe('WalletsStore', () => {
   it('loads wallets from backend', async () => {
     expect(useWalletStore.getState().loading).toBe(true)
     expect(useWalletStore.getState().wallets).toStrictEqual([])
-    await useWalletStore.getState().loadWallets(client as unknown as any)
+    await useWalletStore.getState().loadWallets(request as unknown as any)
     expect(useWalletStore.getState().loading).toBe(false)
     expect(useWalletStore.getState().wallets).toStrictEqual([
       {
@@ -52,23 +50,19 @@ describe('WalletsStore', () => {
   })
   it('throws error if the wallet we are trying to create a key for could not be found', async () => {
     jest.spyOn(console, 'error').mockImplementation(() => {})
-    expect(
+    const fakeRequest = () => {
+      // eslint-disable-next-line no-throw-literal
+      throw null
+    }
+    await expect(
       async () =>
-        await useWalletStore.getState().createKey(
-          {
-            request(method: string) {
-              // eslint-disable-next-line no-throw-literal
-              throw null
-            }
-          } as unknown as any,
-          'this wallet name does not exist'
-        )
-    ).toThrowError('Could not find wallet to create key for')
+        await useWalletStore.getState().createKey(fakeRequest as unknown as any, 'this wallet name does not exist')
+    ).rejects.toThrow('Could not find wallet to create key for')
   })
   it('adds a new key onto wallet when creating a new key for that wallet', async () => {
     jest.spyOn(console, 'error').mockImplementation(() => {})
-    await useWalletStore.getState().loadWallets(client as unknown as any)
-    await useWalletStore.getState().createKey(client as unknown as any, 'Wallet 1')
+    await useWalletStore.getState().loadWallets(request as unknown as any)
+    await useWalletStore.getState().createKey(request as unknown as any, 'Wallet 1')
     expect(useWalletStore.getState().wallets[0].keys).toHaveLength(2)
     const [key1, key2] = useWalletStore.getState().wallets[0].keys
     expect(key1.name).toBe('Key 1')

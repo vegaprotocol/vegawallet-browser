0cb67c6e779307f7ccbb8355235c4778f3e86565
Dexter <dexter@vega.xyz>
2023-07-04 12:54:44 +0100

    test: begin adding tests for window store

---
+++ frontend/stores/window-store.spec.tsx
@@ -0,0 +1,96 @@
+import { createStore } from './window-store'
+
+const mockBrowser = () => {
+  // @ts-ignore
+  globalThis.browser = {
+    windows: {
+      update: jest.fn(),
+      onCreated: {
+        addListener: jest.fn(),
+        removeListener: jest.fn()
+      },
+      onRemoved: {
+        addListener: jest.fn(),
+        removeListener: jest.fn()
+      },
+      getAll: jest.fn().mockReturnValue([])
+    }
+  }
+}
+
+const mockChrome = () => {
+  globalThis.chrome = {
+    windows: {
+      update: jest.fn(),
+      // @ts-ignore
+      onCreated: {
+        addListener: jest.fn(),
+        removeListener: jest.fn()
+      },
+      // @ts-ignore
+      onRemoved: {
+        addListener: jest.fn(),
+        removeListener: jest.fn()
+      },
+      getAll: jest.fn().mockReturnValue([])
+    }
+  }
+}
+
+const initialState = createStore().getState()
+
+describe('useWindowStore', () => {
+  beforeEach(() => {
+    createStore().setState(initialState)
+  })
+  afterEach(() => {
+    // @ts-ignore
+    delete globalThis.browser
+    // @ts-ignore
+    delete globalThis.chrome
+  })
+  it('uses browser if chrome is not defined', () => {
+    mockBrowser()
+    const windowStore = createStore()
+    windowStore.getState().setup()
+    expect(windowStore.getState().popupOpen).toBe(false)
+    expect(windowStore.getState().popupId).toBe(null)
+  })
+  it('uses chrome if browser is not defined', () => {
+    mockChrome()
+    const windowStore = createStore()
+    windowStore.getState().setup()
+    expect(windowStore.getState().popupOpen).toBe(false)
+    expect(windowStore.getState().popupId).toBe(null)
+  })
+  it('initially sets popupId and popupOpen are falsey', () => {
+    mockChrome()
+    const windowStore = createStore()
+    expect(windowStore.getState().popupId).toBe(null)
+    expect(windowStore.getState().popupOpen).toBe(false)
+  })
+
+  it('setup sets up listeners', async () => {
+    mockChrome()
+    const windowStore = createStore()
+    await windowStore.getState().setup()
+    const { windows } = globalThis.chrome
+    expect(windows.onRemoved.addListener).toHaveBeenCalledWith(windowStore.getState().onRemoved, {
+      windowTypes: ['popup']
+    })
+    expect(windows.onRemoved.addListener).toBeCalledTimes(1)
+    expect(windows.onCreated.addListener).toHaveBeenCalledWith(windowStore.getState().onCreated, {
+      windowTypes: ['popup']
+    })
+    expect(windows.onCreated.addListener).toBeCalledTimes(1)
+  })
+  it('setup throws error if there a multiple popups', () => {})
+  it('setup sets popup open if there is a popup', () => {})
+  it('setup leaves popup as false if there is not a popup', () => {})
+  it('teardown removes listeners and resets state', () => {})
+  it('onCreate sets popupOpen to true and popupId to windowId', () => {})
+  it('onRemove sets popupOpen to false and popupId to null', () => {})
+  it('onRemove sets nothing if id does not match popupId', () => {})
+  it('focusPopup focuses the popup', () => {})
+  it('focusPopup throws error if popupId is null', () => {})
+})
--- frontend/stores/window-store.ts
+++ frontend/stores/window-store.ts
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 
 // @ts-ignore
-const windows = globalThis.browser?.windows ?? globalThis.chrome?.windows
+const getWindows = () => globalThis.browser?.windows ?? globalThis.chrome?.windows
 
 export type WindowStore = {
   popupOpen: boolean
@@ -16,49 +16,55 @@ export type WindowStore = {
   teardown: () => void
 }
 
-export const useWindowStore = create<WindowStore>()((set, get) => ({
-  popupOpen: false,
-  popupId: null,
-  focusPopup: () => {
-    const { popupId } = get()
-    if (popupId) {
-      windows.update(popupId, { focused: true, drawAttention: true })
-    } else {
-      throw new Error('Tried to focus popup but none existed')
-    }
-  },
-  onCreated: (window) => {
-    if (!get().popupOpen) {
-      set({ popupOpen: true, popupId: window.id })
-    }
-  },
-  onRemoved: (windowId) => {
-    if (get().popupId === windowId) {
-      set({ popupOpen: false, popupId: null })
-    }
-  },
-  setup: async () => {
-    windows.onCreated.addListener(get().onCreated, {
-      windowTypes: ['popup']
-    })
-    windows.onRemoved.addListener(get().onRemoved, {
-      windowTypes: ['popup']
-    })
-    const wins = await windows.getAll({
-      windowTypes: ['popup']
-    })
-    if (wins.length === 1) {
-      set({ popupOpen: true, popupId: wins[0].id })
-    } else if (wins.length > 1) {
-      throw new Error('Multiple popups open, this should not happen')
-    }
-  },
-  teardown: () => {
-    windows.onCreated.removeListener(get().onCreated)
-    windows.onRemoved.removeListener(get().onRemoved)
-    set({
+// This is needed because in order to mock the windows functions in tests
+// if the file is immediately evaluated the mock is not present
+export const createStore = () =>
+  create<WindowStore>()((set, get) => {
+    const windows = getWindows()
+    return {
       popupOpen: false,
-      popupId: null
-    })
-  }
-}))
+      popupId: null,
+      focusPopup: () => {
+        const { popupId } = get()
+        if (popupId) {
+          windows.update(popupId, { focused: true, drawAttention: true })
+        } else {
+          throw new Error('Tried to focus popup but none existed')
+        }
+      },
+      onCreated: (window) => {
+        set({ popupOpen: true, popupId: window.id })
+      },
+      onRemoved: (windowId) => {
+        if (get().popupId === windowId) {
+          set({ popupOpen: false, popupId: null })
+        }
+      },
+      setup: async () => {
+        windows.onCreated.addListener(get().onCreated, {
+          windowTypes: ['popup']
+        })
+        windows.onRemoved.addListener(get().onRemoved, {
+          windowTypes: ['popup']
+        })
+        const wins = await windows.getAll({
+          windowTypes: ['popup']
+        })
+        if (wins.length === 1) {
+          set({ popupOpen: true, popupId: wins[0].id })
+        } else if (wins.length > 1) {
+          throw new Error('Multiple popups open, this should not happen')
+        }
+      },
+      teardown: () => {
+        windows.onCreated.removeListener(get().onCreated)
+        windows.onRemoved.removeListener(get().onRemoved)
+        set({
+          popupOpen: false,
+          popupId: null
+        })
+      }
+    }
+  })
+
+export const useWindowStore = createStore()

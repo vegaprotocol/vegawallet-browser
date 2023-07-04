bac6a91bca02e3b6687c056b011d69119ea18b74
Dexter <dexter@vega.xyz>
2023-07-04 14:03:12 +0100

    chore: implement the modal wrapper design

---
+++ frontend/components/popover-open-modal/index.tsx
@@ -0,0 +1,33 @@
+import { Button } from '@vegaprotocol/ui-toolkit'
+import { usePopoverStore } from '../../stores/window-store'
+import { Frame } from '../frame'
+import { ExpandIcon } from '../icons/expand'
+import { Splash } from '../splash'
+
+export const locators = {
+  popoverModal: 'popover-modal'
+}
+
+export const PopoverOpenModal = () => {
+  const { focusPopover, popoverOpen } = usePopoverStore((state) => ({
+    popoverOpen: state.popoverOpen,
+    focusPopover: state.focusPopover
+  }))
+
+  if (!popoverOpen) return null
+  return (
+    <Splash data-testid={locators.popoverModal} centered={true}>
+      <Frame>
+        <div className="text-center">
+          <div className="flex justify-center">
+            <ExpandIcon size={24} />
+          </div>
+          <p className="my-4 text-lg">You’re viewing your wallet in another window</p>
+          <Button fill={true} onClick={focusPopover} variant="primary">
+            Continue here
+          </Button>
+        </div>
+      </Frame>
+    </Splash>
+  )
+}

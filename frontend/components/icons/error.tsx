96c7a2f25d5d17026a00e2a0b47d5dbddfb4704a
Dexter <dexter@vega.xyz>
2023-07-07 11:49:51 +0200

    Revert "test: fix test errors"
    This reverts commit 958a0a33ed3f8351df0cc490411cec25e16f7e94.

--- frontend/components/icons/error.tsx
+++ frontend/components/icons/error.tsx
@@ -1,10 +1,10 @@
 export const ErrorIcon = ({ color = '#FF077F' }: { color?: string }) => {
   return (
     <svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <g clipPath="url(#clip0_3642_6558)">
+      <g clip-path="url(#clip0_3642_6558)">
         <rect x="27" y="24" width="2" height="12" fill={color} />
         <rect x="27" y="39" width="2" height="2" fill={color} />
-        <path d="M28 9.01556L50.2768 48H5.72318L28 9.01556Z" stroke={color} strokeWidth="2" />
+        <path d="M28 9.01556L50.2768 48H5.72318L28 9.01556Z" stroke={color} stroke-width="2" />
       </g>
       <defs>
         <clipPath id="clip0_3642_6558">

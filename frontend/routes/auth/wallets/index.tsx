8b653729ea977642f245054f36e14b57d6df3d6b
Dexter <dexter@vega.xyz>
2023-07-07 12:11:20 +0200

    feat: add link to block explorer from transactions list

--- frontend/routes/auth/wallets/index.tsx
+++ frontend/routes/auth/wallets/index.tsx
@@ -4,17 +4,19 @@ import { useCallback, useState } from 'react'
 import { useJsonRpcClient } from '../../../contexts/json-rpc/json-rpc-context'
 import { Frame } from '../../../components/frame'
 import { List } from '../../../components/list'
-import {
-  walletsAssetHeader,
-  walletsCreateKey,
-  walletsDepositLink,
-  walletsKeyName,
-  walletsPage,
-  walletsWalletName
-} from '../../../locator-ids'
 import { KeyIcon } from '../../../components/key-icon'
-import config from '../../../lib/config'
 import { useWalletStore, Key } from '../../../stores/wallets'
+import config from '@/config'
+
+export const locators = {
+  walletsAssetHeader: 'wallets-asset-header',
+  walletsCreateKey: 'wallets-create-key',
+  walletsDepositLink: 'wallets-deposit-link',
+  walletsKeyName: 'wallets-key-name',
+  walletsPage: 'wallets-page',
+  walletsWalletName: 'wallets-wallet-name',
+  walletsExplorerLink: 'wallets-explorer-link'
+}
 
 export const Wallets = () => {
   const { request } = useJsonRpcClient()
@@ -35,8 +37,8 @@ export const Wallets = () => {
   if (loading) return null
 
   return (
-    <section data-testid={walletsPage}>
-      <h1 data-testid={walletsWalletName} className="flex justify-center flex-col text-2xl text-white">
+    <section data-testid={locators.walletsPage}>
+      <h1 data-testid={locators.walletsWalletName} className="flex justify-center flex-col text-2xl text-white">
         {wallet.name}
       </h1>
       <h2 className="uppercase text-sm mt-6 mb-2 text-vega-dark-300">Keys</h2>
@@ -47,29 +49,33 @@ export const Wallets = () => {
           <div className="flex items-center">
             <KeyIcon publicKey={k.publicKey} />
             <div className="ml-4">
-              <div data-testid={walletsKeyName} className="text-lg text-white">
+              <div data-testid={locators.walletsKeyName} className="text-lg text-white">
                 {k.name}
               </div>
-              <CopyWithCheckmark text={k.publicKey}>
-                <span className="underline">{truncateMiddle(k.publicKey)}</span>
-              </CopyWithCheckmark>
+              <ExternalLink
+                data-testid={locators.walletsExplorerLink}
+                href={`${config.network.explorer}/parties/${k.publicKey}`}
+              >
+                {truncateMiddle(k.publicKey)}
+              </ExternalLink>
+              <CopyWithCheckmark text={k.publicKey} />
             </div>
           </div>
         )}
       />
       <div className="mt-3 text-white">
-        <ButtonLink disabled={creatingKey} onClick={createKey} data-testid={walletsCreateKey}>
+        <ButtonLink disabled={creatingKey} onClick={createKey} data-testid={locators.walletsCreateKey}>
           Create new key/pair
         </ButtonLink>
       </div>
       <section className="mt-10">
-        <h1 data-testid={walletsAssetHeader} className="mb-3 text-vega-dark-300 uppercase text-sm">
+        <h1 data-testid={locators.walletsAssetHeader} className="mb-3 text-vega-dark-300 uppercase text-sm">
           Assets
         </h1>
         <Frame>
           Deposit and manage your assets directly in the&nbsp;
           <ExternalLink
-            data-testid={walletsDepositLink}
+            data-testid={locators.walletsDepositLink}
             className="break-word text-white"
             href={config.network.console}
           >

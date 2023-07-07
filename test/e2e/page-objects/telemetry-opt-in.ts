e4c38f9642b8c9a58b62d5554e5984b494353d24
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-07-07 09:38:09 +0100

    test: add initial telemtry page object

---
+++ test/e2e/page-objects/telemetry-opt-in.ts
@@ -0,0 +1,28 @@
+import { By, WebDriver } from 'selenium-webdriver'
+import { clickElement, getByDataTestID, isElementDisplayed } from '../selenium-util'
+import { locators } from '../../../frontend/routes/onboarding/telemetry/index'
+
+export class Telemetry {
+  private readonly noThanksButton: By = getByDataTestID(locators.noThanks)
+  private readonly reportBugsAndCrashesButton: By = getByDataTestID(locators.reportBugsAndCrashes)
+
+  constructor(private readonly driver: WebDriver) {}
+
+  async optIn() {
+    await this.checkOnTelemetryPage()
+    await clickElement(this.driver, this.reportBugsAndCrashesButton)
+  }
+
+  async optOut() {
+    await this.checkOnTelemetryPage()
+    await clickElement(this.driver, this.noThanksButton)
+  }
+
+  async checkOnTelemetryPage() {
+    expect(
+      await isElementDisplayed(this.driver, this.noThanksButton),
+      'expected to be on the Telemetry page but could not locate the password input element',
+      { showPrefix: false }
+    ).toBe(true)
+  }
+}

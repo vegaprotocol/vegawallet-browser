037826871c39c05e922f5adb98e886accdb3d82e
Dexter <dexter@vega.xyz>
2023-07-11 13:00:02 +0100

    chore: create base config for frontend

---
+++ test/jest-config/frontend-base.js
@@ -0,0 +1,51 @@
+const base = {
+  rootDir: '../..',
+  roots: ['<rootDir>/frontend'],
+
+  // Jest transformations -- this adds support for TypeScript
+  // using ts-jest
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'ts-jest'
+  },
+
+  testEnvironment: 'jsdom',
+
+  // Runs special logic, such as cleaning up components
+  // when using React Testing Library and adds special
+  // extended assertions to Jest
+  // setupFilesAfterEnv: ['@testing-library/react/cleanup-after-each', '@testing-library/jest-dom/extend-expect'],
+
+  setupFilesAfterEnv: ['<rootDir>/frontend/setupTests.ts'],
+
+  collectCoverageFrom: ['**/*.{ts,tsx}', '!**/node_modules/**', '!test/**'],
+  coverageDirectory: 'coverage/frontend',
+  coverageReporters: ['html'],
+  coverageThreshold: {
+    global: {
+      branches: 96,
+      functions: 99.5,
+      lines: 100,
+      statements: 100
+    }
+  },
+
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: './test/test-reports',
+        outputName: 'unit-test-results.xml'
+      }
+    ]
+  ],
+
+  // Module file extensions for importing
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '@/config': '<rootDir>/config/test.js'
+  }
+}
+
+export default base

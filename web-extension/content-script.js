41ad024769dccd2419db5268f2e10804e8b0e9ee
Emil Bay <github@tixz.dk>
2023-07-13 19:08:28 +0200

    feat: add manifest v3 keep alive
    This should prevent the extension from locking after even a short while.
    It's strongly inspired by Metamask' approach.
    By sending a JSON-RPC notification to the background script we can prevent
    the extension from going to sleep. After 45 min of inactivity the keep alive
    will stop and the extension go to sleep.

--- web-extension/content-script.js
+++ web-extension/content-script.js
@@ -16,6 +16,44 @@ let backgroundPort
 
 window.addEventListener('message', onwindowmessage, false)
 
+const isManifestV3 = runtime.getManifest().manifest_version === 3
+
+// This timeout stops the keepAliveInterval. This will effectively
+// put the extension to sleep after 45 minutes of inactivity.
+let keepAliveTimeout
+
+// This interval sends a ping to the background every second to
+// prevent the extension from going to sleep.
+let keepAliveInterval
+
+// Start the loop. This code is strongly inspired by Metamask's
+// implementation: https://github.com/MetaMask/metamask-extension/blob/develop/app/scripts/contentscript.js#L133
+keepAlive()
+function keepAlive() {
+  // Only applicable to manifest v3 (ie chromium based browsers)
+  if (isManifestV3) return
+
+  // Refresh the keepalive timeout
+  clearTimeout(keepAliveTimeout)
+  keepAliveTimeout = setTimeout(() => {
+    clearInterval(keepAliveInterval)
+    keepAliveInterval = null
+    keepAliveTimeout = null
+  }, 1000 * 60 * 45)
+
+  if (keepAliveInterval != null) return
+  keepAliveInterval = setInterval(() => {
+    if (backgroundPort == null) connect()
+    backgroundPort.postMessage({ jsonrpc: '2.0', method: 'ping', params: null })
+  }, 1000)
+}
+
+function connect() {
+  backgroundPort = runtime.connect({ name: 'content-script' })
+  backgroundPort.onMessage.addListener(onbackgroundmessage)
+  backgroundPort.onDisconnect.addListener(onbackgrounddisconnect)
+}
+
 // Relay requests from page to background
 function onwindowmessage(event) {
   if (event.source !== window) return
@@ -24,12 +62,9 @@ function onwindowmessage(event) {
   // Only react to requests and notifications
   if (!isNotification(data) && !isRequest(data)) return
 
-  if (backgroundPort == null) {
-    backgroundPort = runtime.connect({ name: 'content-script' })
-    backgroundPort.onMessage.addListener(onbackgroundmessage)
-    backgroundPort.onDisconnect.addListener(onbackgrounddisconnect)
-  }
+  if (backgroundPort == null) connect()
 
+  keepAlive() // refresh keepalive on each message
   backgroundPort.postMessage(data)
 }
 

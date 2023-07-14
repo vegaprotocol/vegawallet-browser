062c47059cb9d83291f1f6818310c4076aebc7e6
Dexter <dexter@vega.xyz>
2023-07-14 20:21:02 +0100

    feat: build out transaction view

--- frontend/components/receipts/transfer/index.tsx
+++ frontend/components/receipts/transfer/index.tsx
@@ -1,7 +1,31 @@
+import { truncateMiddle } from '@vegaprotocol/ui-toolkit'
+import { CopyWithCheckmark } from '../../copy-with-check'
+import { KeyIcon } from '../../key-icon'
+
 export const Transfer = ({ transaction }: { transaction: any }) => {
+  // Not supporting recurring transfers yet
+  if (transaction.transfer.recurring) return null
   return (
-    <div>
-      <h1>Transfer</h1>
-    </div>
+    <section>
+      <h1 className="text-vega-dark-300">Transfer</h1>
+      <div className="flex text-lg mb-4">
+        <div className="mr-2">0.0182199822</div>
+        <div className="text-vega-dark-300">tDAI</div>
+      </div>
+      <h1 className="text-vega-dark-300">To</h1>
+      <div className="flex items-center mb-4">
+        <KeyIcon publicKey={transaction.transfer.to} />
+        <div className="ml-4">
+          <div>Receiving Key</div>
+          <p>
+            <CopyWithCheckmark text={transaction.transfer.to}>
+              <span className="underline text-vega-dark-400">{truncateMiddle(transaction.transfer.to)}</span>
+            </CopyWithCheckmark>
+          </p>
+        </div>
+      </div>
+      <h1 className="text-vega-dark-300">When</h1>
+      <p>Now</p>
+    </section>
   )
 }

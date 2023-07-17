823c86f69a851260e3982bbdbc4702788a33abe1
Dexter <dexter@vega.xyz>
2023-07-17 12:15:46 +0100

    fix: always show popover as priority

--- frontend/components/modals/modal-wrapper/index.tsx
+++ frontend/components/modals/modal-wrapper/index.tsx
@@ -5,9 +5,10 @@ import { TransactionModal } from '../transaction-modal'
 export const ModalWrapper = () => {
   return (
     <>
-      <PopoverOpenModal />
       <ConnectionModal />
       <TransactionModal />
+      {/* Must be at the bottom to ensure that if we have a connection or transaction pending the popover modal is always shown */}
+      <PopoverOpenModal />
     </>
   )
 }

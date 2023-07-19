7657b96c5f8af3cfa00582009c0fb73ecdff5dce
Dexter <dexter@vega.xyz>
2023-07-19 16:20:44 +0100

    chore: add locators

--- frontend/components/receipts/transfer/index.tsx
+++ frontend/components/receipts/transfer/index.tsx
@@ -16,31 +16,49 @@ const getTime = (transaction: Transaction) => {
   return null
 }
 
+export const locators = {
+  transferSection: 'transfer-section',
+  transferTitle: 'transfer-title',
+  assetSymbol: 'asset-symbol',
+  receivingKeySection: 'receiving-key-section',
+  publicKey: 'public-key',
+  whenSection: 'when-section',
+  whenElement: 'when-element'
+}
+
 export const Transfer = ({ transaction }: ReceiptComponentProps) => {
   // Not supporting recurring transfers yet
   if (transaction.transfer.recurring) return null
   const time = getTime(transaction)
   return (
-    <section>
-      <h1 className="text-vega-dark-300">Transfer</h1>
-      <div className="flex text-lg mb-4">
+    <section data-testid={locators.transferSection}>
+      <h1 className="text-vega-dark-300" data-testid={locators.transferTitle}>
+        Transfer
+      </h1>
+      <div className="flex text-lg mb-4" data-testid={locators.assetSymbol}>
         <div className="mr-2">0.0182199822</div>
         <div className="text-vega-dark-300">tDAI</div>
       </div>
       <h1 className="text-vega-dark-300">To</h1>
-      <div className="flex items-center mb-4">
+      <div className="flex items-center mb-4" data-testid={locators.receivingKeySection}>
         <KeyIcon publicKey={transaction.transfer.to} />
         <div className="ml-4">
           <div>Receiving Key</div>
           <p>
             <CopyWithCheckmark text={transaction.transfer.to}>
-              <span className="underline text-vega-dark-400">{truncateMiddle(transaction.transfer.to)}</span>
+              <span className="underline text-vega-dark-400" data-testid={locators.publicKey}>
+                {truncateMiddle(transaction.transfer.to)}
+              </span>
             </CopyWithCheckmark>
           </p>
         </div>
       </div>
-      <h1 className="text-vega-dark-300">When</h1>
-      <p>{time ? <ReactTimeAgo timeStyle="round" date={time} locale="en-US" /> : 'Now'}</p>
+      <h1 className="text-vega-dark-300" data-testid={locators.whenSection}>
+        When
+      </h1>
+      <p data-testid={locators.whenElement}>
+        {time ? <ReactTimeAgo timeStyle="round" date={time} locale="en-US" /> : 'Now'}
+      </p>
     </section>
   )
 }

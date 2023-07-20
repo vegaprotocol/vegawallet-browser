45debe90637849f248245fc647588e3b0ec4813b
Dexter <dexter@vega.xyz>
2023-07-20 13:25:50 +0100

    test: add tests for order badges

---
+++ frontend/components/receipts/utils/order-badges.spec.tsx
@@ -0,0 +1,72 @@
+import { render, screen } from '@testing-library/react'
+import { OrderBadges } from './order-badges'
+import { OrderTimeInForce } from '@vegaprotocol/types'
+import { silenceErrors } from '../../../test-helpers/silence-errors'
+
+describe('OrderBadges component', () => {
+  test('renders Good til date badge when timeInForce is GTT', () => {
+    const mockExpiresAt = '2022-01-01T00:00:00.000Z'
+    render(
+      <OrderBadges
+        postOnly={false}
+        reduceOnly={false}
+        timeInForce={OrderTimeInForce.TIME_IN_FORCE_GTT}
+        expiresAt={mockExpiresAt}
+      />
+    )
+
+    expect(screen.getByText('Good til 1/1/2022, 12:00:00 AM')).toBeInTheDocument()
+  })
+
+  test('renders TIF badge when timeInForce is not GTT', () => {
+    render(
+      <OrderBadges
+        postOnly={false}
+        reduceOnly={false}
+        timeInForce={OrderTimeInForce.TIME_IN_FORCE_GTC}
+        expiresAt={null}
+      />
+    )
+
+    expect(screen.getByText('GTC')).toBeVisible()
+  })
+
+  test('renders post only badge when postOnly is true', () => {
+    render(
+      <OrderBadges
+        postOnly={true}
+        reduceOnly={false}
+        timeInForce={OrderTimeInForce.TIME_IN_FORCE_GTC}
+        expiresAt={null}
+      />
+    )
+    expect(screen.getByText('Post only')).toBeVisible()
+  })
+
+  test('renders reduce only badge when reduceOnly is true', () => {
+    render(
+      <OrderBadges
+        postOnly={false}
+        reduceOnly={true}
+        timeInForce={OrderTimeInForce.TIME_IN_FORCE_GTC}
+        expiresAt={null}
+      />
+    )
+
+    expect(screen.getByText('Reduce only')).toBeVisible()
+  })
+
+  test('throws error if GTT order does not have expiresAt', () => {
+    silenceErrors()
+    expect(() =>
+      render(
+        <OrderBadges
+          postOnly={false}
+          reduceOnly={false}
+          timeInForce={OrderTimeInForce.TIME_IN_FORCE_GTT}
+          expiresAt={null}
+        />
+      )
+    ).toThrowError('GTT order without expiresAt')
+  })
+})
--- frontend/components/receipts/utils/order-badges.tsx
+++ frontend/components/receipts/utils/order-badges.tsx
@@ -3,18 +3,27 @@ import { OrderTimeInForce } from '@vegaprotocol/types'
 import { ReactNode } from 'react'
 import { getDateTimeFormat } from '@vegaprotocol/utils'
 
+const TIF_MAP: { [key in OrderTimeInForce]: string } = {
+  [OrderTimeInForce.TIME_IN_FORCE_GTC]: 'GTC',
+  [OrderTimeInForce.TIME_IN_FORCE_GTT]: 'GTT',
+  [OrderTimeInForce.TIME_IN_FORCE_IOC]: 'IOC',
+  [OrderTimeInForce.TIME_IN_FORCE_FOK]: 'FOK',
+  [OrderTimeInForce.TIME_IN_FORCE_GFA]: 'GFA',
+  [OrderTimeInForce.TIME_IN_FORCE_GFN]: 'GFN'
+}
+
 const OrderBadge = ({ children }: { children: ReactNode }) => {
   return <Lozenge className="text-xs mr-0.5 text-vega-dark-400 whitespace-nowrap">{children}</Lozenge>
 }
 
-const TifBadge = ({ timeInForce, expiresAt }: { timeInForce: OrderTimeInForce | null; expiresAt: string | null }) => {
+const TifBadge = ({ timeInForce, expiresAt }: { timeInForce: OrderTimeInForce; expiresAt: string | null }) => {
   if (timeInForce === OrderTimeInForce.TIME_IN_FORCE_GTT) {
     if (!expiresAt) {
       throw new Error('GTT order without expiresAt')
     }
     return <OrderBadge>Good til {getDateTimeFormat().format(new Date(expiresAt))}</OrderBadge>
   }
-  return <OrderBadge>{timeInForce}</OrderBadge>
+  return <OrderBadge>{TIF_MAP[timeInForce]}</OrderBadge>
 }
 
 export const OrderBadges = ({
@@ -25,7 +34,7 @@ export const OrderBadges = ({
 }: {
   postOnly: boolean | null
   reduceOnly: boolean | null
-  timeInForce: OrderTimeInForce | null
+  timeInForce: OrderTimeInForce
   expiresAt: string | null
 }) => {
   const postBadge = postOnly ? <OrderBadge>Post only</OrderBadge> : null

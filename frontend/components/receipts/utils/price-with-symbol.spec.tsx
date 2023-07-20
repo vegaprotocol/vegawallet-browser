9130a391c25683d9d29fa8a39476bbc0ba924dec
Dexter <dexter@vega.xyz>
2023-07-20 11:58:30 +0100

    feat: add price with symbol

---
+++ frontend/components/receipts/utils/price-with-symbol.spec.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react'
+import { PriceWithSymbol, locators } from './price-with-symbol' // Assuming the component is in the same directory
+
+describe('PriceWithSymbol component', () => {
+  it('renders price and symbol correctly', () => {
+    const price = 9.99
+    const symbol = 'USD'
+
+    render(<PriceWithSymbol price={price} symbol={symbol} />)
+
+    const priceElement = screen.getByTestId(locators.price)
+    const symbolElement = screen.getByTestId(locators.symbol)
+    const priceWithSymbolElement = screen.getByTestId(locators.priceWithSymbol)
+
+    expect(priceElement).toHaveTextContent(price.toString())
+    expect(symbolElement).toHaveTextContent(symbol)
+    expect(priceWithSymbolElement).toContainElement(priceElement)
+    expect(priceWithSymbolElement).toContainElement(symbolElement)
+    expect(screen.getByTestId(locators.priceWithSymbol)).toHaveTextContent('9.99 USD')
+  })
+})
---
+++ frontend/components/receipts/utils/price-with-symbol.tsx
@@ -0,0 +1,17 @@
+export const locators = {
+  price: 'price',
+  symbol: 'symbol',
+  priceWithSymbol: 'price-with-symbol'
+}
+
+export const PriceWithSymbol = ({ price, symbol }: { price: string | number; symbol: string }) => {
+  return (
+    <div data-testid={locators.priceWithSymbol}>
+      <span data-testid={locators.price}>{price}</span>
+      &nbsp;
+      <span className="text-vega-dark-300" data-testid={locators.symbol}>
+        {symbol}
+      </span>
+    </div>
+  )
+}

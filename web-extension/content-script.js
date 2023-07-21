ae63cd08bb2c0d6dab1c995dedd6d58658f002e7
Emil Bay <github@tixz.dk>
2023-07-21 08:44:51 +0200

    feat: factor out mv3 keep-alive and unit test

--- web-extension/content-script.js
+++ web-extension/content-script.js
@@ -1,4 +1,5 @@
 import { isRequest, isNotification } from '../lib/json-rpc.js'
+import createKeepAlive from '../lib/mv3-keep-alive.js'
 
 const runtime = globalThis.browser?.runtime ?? globalThis.chrome?.runtime
 
@@ -16,37 +17,7 @@ let backgroundPort
 
 window.addEventListener('message', onwindowmessage, false)
 
-const isManifestV3 = runtime.getManifest().manifest_version === 3
-
-// This timeout stops the keepAliveInterval. This will effectively
-// put the extension to sleep after 45 minutes of inactivity.
-let keepAliveTimeout
-
-// This interval sends a ping to the background every second to
-// prevent the extension from going to sleep.
-let keepAliveInterval
-
-// Start the loop. This code is strongly inspired by Metamask's
-// implementation: https://github.com/MetaMask/metamask-extension/blob/develop/app/scripts/contentscript.js#L133
-keepAlive()
-function keepAlive() {
-  // Only applicable to manifest v3 (ie chromium based browsers)
-  if (isManifestV3) return
-
-  // Refresh the keepalive timeout
-  clearTimeout(keepAliveTimeout)
-  keepAliveTimeout = setTimeout(() => {
-    clearInterval(keepAliveInterval)
-    keepAliveInterval = null
-    keepAliveTimeout = null
-  }, 1000 * 60 * 45)
-
-  if (keepAliveInterval != null) return
-  keepAliveInterval = setInterval(() => {
-    if (backgroundPort == null) connect()
-    backgroundPort.postMessage({ jsonrpc: '2.0', method: 'ping', params: null })
-  }, 1000)
-}
+const keepAlive = createKeepAlive()
 
 function connect() {
   backgroundPort = runtime.connect({ name: 'content-script' })
@@ -64,7 +35,9 @@ function onwindowmessage(event) {
 
   if (backgroundPort == null) connect()
 
-  keepAlive() // refresh keepalive on each message
+  // TODO: We could filter out different types of messages here like metamask does,
+  // where only user "initiated" messages cause the keep-alive loop to refresh
+  keepAlive(backgroundPort) // refresh keepalive on each message
   backgroundPort.postMessage(data)
 }
 
@@ -77,4 +50,5 @@ function onbackgrounddisconnect() {
   backgroundPort.onMessage.removeListener(onbackgroundmessage)
   backgroundPort.onDisconnect.removeListener(onbackgrounddisconnect)
   backgroundPort = null
+  keepAlive(null) // stop keepalive
 }

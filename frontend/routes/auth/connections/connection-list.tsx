86c57c9867cc9890bdfef9bba62df48839e53a9a
Dexter <dexter@vega.xyz>
2023-07-24 14:28:38 +0100

    feat: add last accessed time to connection list

--- frontend/routes/auth/connections/connection-list.tsx
+++ frontend/routes/auth/connections/connection-list.tsx
@@ -2,6 +2,7 @@ import { List } from '../../../components/list'
 import { HostImage } from '../../../components/host-image'
 import { Connection } from '../../../stores/connections'
 import { Cross } from '../../../components/icons/cross'
+import { getTimeFormat, getDateFormat } from '@vegaprotocol/utils'
 
 export const locators = {
   connectionDetails: 'connection-details',
@@ -25,19 +26,24 @@ export const ConnectionsList = ({
         idProp="origin"
         items={connections}
         renderItem={(i) => (
-          <div className="flex justify-between">
-            <div className="flex flex-col justify-center">
-              <HostImage size={42} hostname={i.origin} />
+          <div>
+            <div className="flex justify-between">
+              <div className="flex flex-col justify-center">
+                <HostImage size={42} hostname={i.origin} />
+              </div>
+              <div
+                data-testid={locators.connectionOrigin}
+                className="ml-4 flex-1 flex flex-col justify-center overflow-hidden break-all"
+              >
+                {i.origin}
+              </div>
+              <button data-testid={locators.connectionRemoveConnection} onClick={() => removeConnection(i)}>
+                <Cross className="w-8 h-8" />
+              </button>
             </div>
-            <div
-              data-testid={locators.connectionOrigin}
-              className="ml-4 flex-1 flex flex-col justify-center overflow-hidden break-all"
-            >
-              {i.origin}
+            <div className="text-vega-dark-400">
+              Last connected: {getDateFormat().format(i.accessedAt)} · {getTimeFormat().format(i.accessedAt)}
             </div>
-            <button data-testid={locators.connectionRemoveConnection} onClick={() => removeConnection(i)}>
-              <Cross className="w-8 h-8" />
-            </button>
           </div>
         )}
       />

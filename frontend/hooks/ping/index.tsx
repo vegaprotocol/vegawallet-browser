d321c4ec21a98c0e88b288479a15fb7eebd9deb9
Dexter <dexter@vega.xyz>
2023-07-24 09:15:47 +0100

    style: lint

--- frontend/hooks/ping/index.tsx
+++ frontend/hooks/ping/index.tsx
@@ -3,8 +3,10 @@ import { useEffect, useMemo } from 'react'
 import { getExtensionApi } from '../../lib/extension-apis'
 
 export const usePing = () => {
-  const { runtime } = getExtensionApi()
-  const backgroundPort = useMemo(() => runtime.connect({ name: 'popup' }), [])
+  const backgroundPort = useMemo(() => {
+    const { runtime } = getExtensionApi()
+    return runtime.connect({ name: 'popup' })
+  }, [])
   const keepAlive = useMemo(() => initKeepAlive(), [])
   useEffect(() => {
     const { keepAliveTimeout, keepAliveInterval } = keepAlive(backgroundPort)

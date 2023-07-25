924522e93786963dddbf7671982e19952c787279
Dexter <dexter@vega.xyz>
2023-07-25 11:33:51 +0100

    test: add tests for dapp header

--- frontend/setupTests.ts
+++ frontend/setupTests.ts
@@ -6,11 +6,25 @@ import '@testing-library/jest-dom'
 import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en'
 import 'jest-canvas-mock'
+import ResizeObserver from 'resize-observer-polyfill'
 
 TimeAgo.addDefaultLocale(en)
-class ResizeObserver {
-  observe() {}
-  unobserve() {}
+
+// Required by radix-ui/react-tooltip
+global.ResizeObserver = ResizeObserver
+
+// Required by radix-ui/react-tooltip
+global.DOMRect = class DOMRect {
+  bottom = 0
+  left = 0
+  right = 0
+  top = 0
+
+  constructor(public x = 0, public y = 0, public width = 0, public height = 0) {}
+  static fromRect(other?: DOMRectInit): DOMRect {
+    return new DOMRect(other?.x, other?.y, other?.width, other?.height)
+  }
+  toJSON() {
+    return JSON.stringify(this)
+  }
 }
-// @ts-ignore
-window.ResizeObserver = ResizeObserver

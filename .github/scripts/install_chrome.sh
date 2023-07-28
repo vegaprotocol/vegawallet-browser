87b71247599cc0be95e307f14d7dc28565114fba
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-07-28 10:35:45 +0100

    test:debugging

--- .github/scripts/install_chrome.sh
+++ .github/scripts/install_chrome.sh
@@ -5,4 +5,6 @@ source get_latest_stable_chrome.sh
 chrome_filename="chrome-linux64"
 download_url="https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$latest_stable_chrome/linux64/$chrome_zip"
 
+echo "current directory: $(pwd)"
+echo "files in directory: $(ls)"
 source ./download_and_unzip.sh -$download_url -$chrome_filename
--- .github/scripts/install_chromedriver.sh
+++ .github/scripts/install_chromedriver.sh
@@ -6,4 +6,7 @@ chromedriver_filename="chromedriver-linux64"
 
 download_url="https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$latest_stable_chrome/linux64/$chromedriver_zip"
 
+echo "current directory: $(pwd)"
+echo "files in directory: $(ls)"
+
 source ./download_and_unzip.sh -$download_url -$chromedriver_filename

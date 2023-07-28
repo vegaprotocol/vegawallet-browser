b28bb012659e91d22656fc012157edc41f1cf9d6
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-07-28 13:24:34 +0100

    test: fix mistake made from rushing

--- .github/scripts/install_chromedriver.sh
+++ .github/scripts/install_chromedriver.sh
@@ -5,4 +5,4 @@ source .github/scripts/get_latest_stable_chrome.sh
 chromedriver_filename="chromedriver-linux64"
 download_url="https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$latest_stable_chrome/linux64/${chromedriver_filename}.zip"
 
-sudo $(pwd) ./.github/scripts/download_and_unzip.sh "$download_url" "$chromedriver_filename"
+sudo $(pwd)/.github/scripts/download_and_unzip.sh "$download_url" "$chromedriver_filename"

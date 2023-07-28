cb195be1b70ed75b54c9c3e5acac97992760f74e
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-07-28 10:43:56 +0100

    test: wget not working

--- .github/scripts/download_and_unzip.sh
+++ .github/scripts/download_and_unzip.sh
@@ -5,8 +5,8 @@ filename="$2"
 echo "Download URL: $download_url"
 echo "Filename: $filename"
 
-wget -q "$download_url"
-unzip -q "$filename" + ".zip"
+wget -q $download_url
+unzip -q $filename + ".zip"
 
 sudo mv "$filename" /usr/local/bin/
 

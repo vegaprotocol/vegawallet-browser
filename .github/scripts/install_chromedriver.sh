ec9f751e33e6d4abc997337bd67c4b49baf11d71
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-07-28 10:08:29 +0100

    test: clean up the script

--- .github/scripts/install_chromedriver.sh
+++ .github/scripts/install_chromedriver.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-response=$(curl -s https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions.json)
-
-stable_version=$(echo "$response" | grep -oP '"Stable":{"channel":"Stable","version":"\K[^"]+')
+stable_version=$(curl -s https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions.json | grep -oP '"Stable":{"channel":"Stable","version":"\K[^"]+')
 
 echo "Stable Version: $stable_version"
-chromedriver_filename="chromedriver-linux64.zip" 
 
-download_url="https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$stable_version/linux64/$chromedriver_filename"
-echo "Download URL: $download_url" 
+chromedriver_filename="chromedriver-linux64"
 
-wget $download_url
-unzip $chromedriver_filename
+chromedriver_zip="${chromedriver_filename}.zip"
 
-ls -la
-sudo mv chromedriver-linux64 /usr/local/bin/
-sudo chown root:root /usr/local/bin/chromedriver-linux64
-sudo chmod +x /usr/local/bin/chromedriver-linux64
+download_url="https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$stable_version/linux64/$chromedriver_zip"
+echo "Download URL: $download_url"
+
+wget -q "$download_url"
+unzip -q "$chromedriver_zip"
+
+sudo mv "$chromedriver_filename" /usr/local/bin/
+
+sudo chown root:root "/usr/local/bin/$chromedriver_filename"
+sudo chmod +x "/usr/local/bin/$chromedriver_filename"
+
+rm "$chromedriver_zip"

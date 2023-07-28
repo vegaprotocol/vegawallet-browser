6ab9bd226c4c5f29e7126c8b3b37a4c4c5cec14b
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-07-28 10:38:52 +0100

    test: add path to files

--- .github/scripts/install_chrome.sh
+++ .github/scripts/install_chrome.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-source get_latest_stable_chrome.sh
+source .github/scripts/get_latest_stable_chrome.sh
 
 chrome_filename="chrome-linux64"
 download_url="https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$latest_stable_chrome/linux64/$chrome_zip"
 
 echo "current directory: $(pwd)"
 echo "files in directory: $(ls)"
-source ./download_and_unzip.sh -$download_url -$chrome_filename
+source .github/scripts/download_and_unzip.sh -$download_url -$chrome_filename
--- .github/scripts/install_chromedriver.sh
+++ .github/scripts/install_chromedriver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source get_latest_stable_chrome.sh
+source .github/scripts/get_latest_stable_chrome.sh
 
 chromedriver_filename="chromedriver-linux64"
 
@@ -9,4 +9,4 @@ download_url="https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$lates
 echo "current directory: $(pwd)"
 echo "files in directory: $(ls)"
 
-source ./download_and_unzip.sh -$download_url -$chromedriver_filename
+source .github/scripts/download_and_unzip.sh -$download_url -$chromedriver_filename

ebc530dade8f268edb923441672c19d4264b0874
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-07-28 10:46:22 +0100

    fix: script fails due to referencing old var that does not exist

--- .github/scripts/install_chromedriver.sh
+++ .github/scripts/install_chromedriver.sh
@@ -4,7 +4,7 @@ source .github/scripts/get_latest_stable_chrome.sh
 
 chromedriver_filename="chromedriver-linux64"
 
-download_url="https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$latest_stable_chrome/linux64/$chromedriver_zip"
+download_url="https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$latest_stable_chrome/linux64/${chromedriver_filename}.zip"
 
 echo "current directory: $(pwd)"
 echo "files in directory: $(ls)"

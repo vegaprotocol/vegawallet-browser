c44cb20a955995f4a65cd55652621e5279e83a83
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-08-01 16:03:46 +0100

    chore: get scripts working locally

--- .github/scripts/get_latest_stable_chrome.sh
+++ .github/scripts/get_latest_stable_chrome.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sudo chmod +x -R .github/scripts
+current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+sudo chmod +x -R $current_dir
 response=$(curl -s https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions.json)
 latest_stable_chrome=$(echo "$response" | jq -r '.channels.Stable.version')

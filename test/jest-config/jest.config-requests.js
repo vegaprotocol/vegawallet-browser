2e0f79145bb6d680a6ed56a14db60c7ad8508d07
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-08-16 10:16:22 +0100

    test: add new jest config

---
+++ test/jest-config/jest.config-requests.js
@@ -0,0 +1,25 @@
+export default {
+    rootDir: '../..',
+    roots: ['<rootDir>/web-extension'],
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest',
+      '^.+\\.jsx?$': 'ts-jest'
+    },
+    testEnvironment: 'jest-environment-node',
+    testTimeout: 30000,
+    reporters: [
+      'default',
+      [
+        'jest-junit',
+        {
+          outputDirectory: './test/test-reports',
+          outputName: 'request-test-results.xml'
+        }
+      ]
+    ],
+    transformIgnorePatterns: ['/node_modules/(?!(@vegaprotocol)/protos)/'],
+    moduleNameMapper: {
+      '!/config': '<rootDir>/config/test.js'
+    }
+  }
+  

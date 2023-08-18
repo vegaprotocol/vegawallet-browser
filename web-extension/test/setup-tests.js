a5aee78bd66cd9a3dd2d79ddbe622a8432024103
Dexter <dexter@vega.xyz>
2023-08-18 10:14:12 +0100

    chore: better mock

--- web-extension/test/setup-tests.js
+++ web-extension/test/setup-tests.js
@@ -1,11 +1,37 @@
 import 'jest-webextension-mock'
 
+const window = {
+  alwaysOnTop: false,
+  focused: false,
+  height: 800,
+  id: Math.floor(Math.random() * 10000),
+  incognito: false,
+  left: 258,
+  state: 'normal',
+  top: 25,
+  type: 'normal',
+  width: 742
+}
+
+const popout = {
+  alwaysOnTop: false,
+  focused: false,
+  height: 800,
+  id: Math.floor(Math.random() * 10000),
+  incognito: false,
+  left: 258,
+  state: 'normal',
+  top: 25,
+  type: 'popout',
+  width: 742
+}
+
 const windows = {
-  create: jest.fn(),
+  create: jest.fn().mockResolvedValue(() => popout),
   get: jest.fn(),
   getAll: jest.fn(),
-  getCurrent: jest.fn(),
-  getLastFocused: jest.fn(),
+  getCurrent: jest.fn().mockResolvedValue(() => window),
+  getLastFocused: jest.fn(() => window),
   remove: jest.fn(),
   update: jest.fn(),
   onBoundsChanged: {

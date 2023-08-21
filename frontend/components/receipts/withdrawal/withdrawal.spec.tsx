68dafe0ac3de1de1afb938a340fc35a7ef5ef47a
Dexter <dexter@vega.xyz>
2023-08-21 17:10:06 +0100

    feat: withdrawals receipt

---
+++ frontend/components/receipts/withdrawal/withdrawal.spec.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react'
+import { Withdraw } from '.'
+
+jest.mock('../../keys/ethereum-key', () => ({
+  EthereumKey: () => <div data-testid="ethereum-key" />
+}))
+
+describe('Withdrawal', () => {
+  it('renders nothing if withdrawal is not of type erc20', () => {
+    const { container } = render(
+      <Withdraw
+        transaction={{
+          withdrawSubmission: {}
+        }}
+      />
+    )
+    expect(container).toBeEmptyDOMElement()
+  })
+  it('renders ethereum address', () => {
+    render(
+      <Withdraw
+        transaction={{
+          withdrawSubmission: {
+            ext: {
+              erc20: {
+                receiverAddress: '0x1111111111111111111111111111111111111111'
+              }
+            }
+          }
+        }}
+      />
+    )
+    expect(screen.getByTestId('ethereum-key')).toBeVisible()
+  })
+})

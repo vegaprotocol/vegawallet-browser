8dd99f8d046f271e2ba4089354d0dc5194bcc811
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-08-22 11:26:46 +0100

    test: tidy

--- test/e2e/wallet-helpers/vega-api.ts
+++ test/e2e/wallet-helpers/vega-api.ts
@@ -32,12 +32,6 @@ export class VegaAPI {
     return this.vegaExtensionWindowHandle
   }
 
-  async getCurrentVegaDappWindowHandle(withNewTab = true) {
-    return await this.controlTabs(withNewTab, false, async () => {
-      return await this.driver.getWindowHandle()
-    })
-  }
-
   async getVegaDappWindowHandle() {
     return this.vegaDappWindowHandle
   }

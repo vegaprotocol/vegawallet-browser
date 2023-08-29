e9374d62be13ee563b24cdedb576672fb2d562bb
Dexter <dexter@vega.xyz>
2023-08-29 16:48:23 +0100

    fix: sonar nit

--- frontend/components/data-table/data-table.tsx
+++ frontend/components/data-table/data-table.tsx
@@ -16,7 +16,7 @@ export const DataTable = ({ items }: DataTableProps) => {
       {items.map(([key, value], i) => (
         <dl
           data-testid={locators.dataRow}
-          key={`data-row-${i}`}
+          key={`data-row-${key}`}
           className="flex gap-1 flex-wrap justify-between py-1 text-sm flex-row items-center"
         >
           <dt data-testid={locators.dataKey} className="text-vega-dark-300 break-words">

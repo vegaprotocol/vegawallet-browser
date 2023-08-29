4445c56622de0fe79f12617e95f66b31e4358596
Dexter <dexter@vega.xyz>
2023-08-29 14:31:54 +0100

    chore: add futher information into order table

--- frontend/components/receipts/utils/order-table.spec.tsx
+++ frontend/components/receipts/utils/order-table.spec.tsx
@@ -14,6 +14,18 @@ describe('OrderTable', () => {
         price={'123'}
       />
     )
+    const [marketRow, orderRow, directionRow, sizeRow, priceRow] = screen.getAllByTestId(locators.dataRow)
+    expect(marketRow).toHaveTextContent('Market')
+    expect(marketRow).toHaveTextContent('111111…1111')
+    expect(orderRow).toHaveTextContent('Order')
+    expect(orderRow).toHaveTextContent('222222…2222')
+    expect(directionRow).toHaveTextContent('Direction')
+    expect(directionRow).toHaveTextContent('Long')
+    expect(sizeRow).toHaveTextContent('Size')
+    expect(sizeRow).toHaveTextContent('12')
+    expect(priceRow).toHaveTextContent('Price')
+    expect(priceRow).toHaveTextContent('123')
+    expect(screen.getAllByTestId(locators.dataRow)).toHaveLength(5)
   })
 
   it('does not render row if the property is undefined', () => {
--- frontend/components/receipts/utils/order-table.tsx
+++ frontend/components/receipts/utils/order-table.tsx
@@ -1,5 +1,24 @@
 import { ReactNode } from 'react'
 import { DataTable } from '../../data-table/data-table'
+import { ExternalLink, truncateMiddle } from '@vegaprotocol/ui-toolkit'
+import config from '!/config'
+import { Side } from '@vegaprotocol/types'
+
+const MarketLink = ({ marketId, name }: { marketId: string; name?: string }) => (
+  <ExternalLink className="text-vega-dark-400" href={`${config.network.explorer}/markets/${marketId}`}>
+    {name ? name : truncateMiddle(marketId)}
+  </ExternalLink>
+)
+
+const OrderLink = ({ orderId }: { orderId: string }) => (
+  <ExternalLink className="text-vega-dark-400" href={`${config.network.explorer}/orders/${orderId}`}>
+    {truncateMiddle(orderId)}
+  </ExternalLink>
+)
+
+const Direction = ({ direction }: { direction: Side }) => {
+  return <>{direction === Side.SIDE_BUY ? 'Long' : 'Short'}</>
+}
 
 export const OrderTable = ({
   marketId,
@@ -10,15 +29,16 @@ export const OrderTable = ({
 }: Partial<{
   marketId: string
   orderId: string
-  direction: string
+  direction: Side
   size: string
   price: string
 }>) => {
   const columns = [
-    marketId ? ['Market', marketId] : null,
-    orderId ? ['Order', orderId] : null,
-    direction ? ['Direction', direction] : null,
+    marketId ? ['Market', <MarketLink marketId={marketId} />] : null,
+    orderId ? ['Order', <OrderLink orderId={orderId} />] : null,
+    direction ? ['Direction', <Direction direction={direction} />] : null,
     size ? ['Size', size] : null,
+    // TODO price makes no sense without decimals
     price ? ['Price', price] : null
   ]
   const data = columns.filter((c) => !!c) as [ReactNode, ReactNode][]

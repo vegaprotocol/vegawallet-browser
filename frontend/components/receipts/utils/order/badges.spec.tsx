a55781039088b69186437cb3be8bd2939a224c17
Dexter <dexter@vega.xyz>
2023-08-31 12:17:16 +0100

    docs: add order badges acs

--- frontend/components/receipts/utils/order/badges.spec.tsx
+++ frontend/components/receipts/utils/order/badges.spec.tsx
@@ -5,6 +5,7 @@ import { silenceErrors } from '../../../../test-helpers/silence-errors'
 
 describe('OrderBadges component', () => {
   test('renders Good til date badge when timeInForce is GTT', () => {
+    // 1114-RCPT-011 If time in force is GTT then I can see the expiry of the order
     const mockExpiresAt = (1e9).toString()
     render(
       <OrderBadges
@@ -19,6 +20,7 @@ describe('OrderBadges component', () => {
   })
 
   test('renders TIF badge when timeInForce is not GTT', () => {
+    // 1114-RCPT-010 I can see a badge of the order time in force
     render(
       <OrderBadges
         postOnly={false}
@@ -32,6 +34,7 @@ describe('OrderBadges component', () => {
   })
 
   test('renders post only badge when postOnly is true', () => {
+    // 1114-RCPT-008 I can see a badge if the order is post only
     render(
       <OrderBadges
         postOnly={true}
@@ -45,6 +48,7 @@ describe('OrderBadges component', () => {
   })
 
   test('renders reduce only badge when reduceOnly is true', () => {
+    // 1114-RCPT-009 I can see a badge if the order is reduce only
     render(
       <OrderBadges
         postOnly={false}

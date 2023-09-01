c54ae72c0755c28994c7768e98b59b7c4295e1df
Dexter <dexter@vega.xyz>
2023-09-01 11:57:44 +0100

    fix: support number enums

--- frontend/components/receipts/utils/order/direction.tsx
+++
@@ -1,9 +0,0 @@
-import { Side } from '@vegaprotocol/types'
-
-export const sideText: Record<Side, string> = {
-  SIDE_UNSPECIFIED: 'Unspecified',
-  SIDE_BUY: 'Long',
-  SIDE_SELL: 'Short'
-}
-
-export const Direction = ({ direction }: { direction: Side }) => <>{sideText[direction]}</>

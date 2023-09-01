5be0ce6921a029ebae2512a834cfe0e5fdaa0710
Dexter <dexter@vega.xyz>
2023-09-01 11:57:44 +0100

    Revert "fix: support number enums"
    This reverts commit c363c91028ebfa4acd1e2b262a08f6fee4cce01c.

--- frontend/components/receipts/utils/order-table.tsx
+++ frontend/components/receipts/utils/order-table.tsx
@@ -3,19 +3,19 @@ import { DataTable } from '../../data-table/data-table'
 import { truncateMiddle } from '@vegaprotocol/ui-toolkit'
 import { OrderType, Side } from '@vegaprotocol/types'
 import { MarketLink } from './order/market-link'
-import { Direction } from './order/side'
+import { Direction } from './order/direction'
 import { OrderTypeComponent } from './order/order-type'
 
 export const OrderTable = ({
   marketId,
-  side,
+  direction,
   orderId,
   reference,
   type
 }: Partial<{
   marketId: string
   orderId: string
-  side: Side
+  direction: Side
   size: string
   price: string
   reference: string
@@ -24,7 +24,7 @@ export const OrderTable = ({
   const columns = [
     marketId ? ['Market', <MarketLink key="order-details-market" marketId={marketId} />] : null,
     orderId ? ['Order', truncateMiddle(orderId)] : null,
-    side ? ['Direction', <Direction key="order-details-direction" side={side} />] : null,
+    direction ? ['Direction', <Direction key="order-details-direction" direction={direction} />] : null,
     type ? ['Type', <OrderTypeComponent key="order-details-type" type={type} />] : null,
     reference ? ['Reference', reference] : null
   ]

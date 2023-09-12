cf1988f476e5fd1aff898c6a1ee59a68e22ba394
Dexter <dexter@vega.xyz>
2023-09-12 15:36:31 +0800

    chore: reduce duplication

--- frontend/components/receipts/orders/stop/submission.spec.tsx
+++ frontend/components/receipts/orders/stop/submission.spec.tsx
@@ -9,6 +9,23 @@ jest.mock('../../utils/order/badges', () => ({
   OrderBadges: () => <div data-testid="order-badges" />
 }))
 
+const validateStopOrderDetails = (type: string) => {
+  const [triggerPrice, trailingPercentOffset, expiryStrategy, expiresAt] = screen.getAllByTestId(tableLocators.dataRow)
+
+  expect(screen.getByTestId(locators.sectionHeader)).toHaveTextContent(type)
+  expect(triggerPrice).toHaveTextContent('Trigger price')
+  expect(triggerPrice).toHaveTextContent('100')
+  expect(trailingPercentOffset).toHaveTextContent('Trailing offset')
+  expect(trailingPercentOffset).toHaveTextContent('5%')
+  expect(expiryStrategy).toHaveTextContent('Expiry strategy')
+  expect(expiryStrategy).toHaveTextContent('Submit')
+  expect(expiresAt).toHaveTextContent('Expires at')
+  expect(expiresAt).toHaveTextContent('1/1/1970, 12:27:18 AM')
+  expect(screen.getByTestId(locators.orderDetails)).toHaveTextContent('Order details')
+  expect(screen.getByTestId('order-table')).toBeTruthy()
+  expect(screen.getByTestId('order-badges')).toBeTruthy()
+}
+
 describe('StopOrderSubmission', () => {
   test('renders the component with "Rises above" details', () => {
     // 1114-RCPT-025 If a rises above order is present I see the rises above section
@@ -30,21 +47,7 @@ describe('StopOrderSubmission', () => {
     }
 
     render(<StopOrderSubmission transaction={transaction} />)
-    const [triggerPrice, trailingPercentOffset, expiryStrategy, expiresAt] = screen.getAllByTestId(
-      tableLocators.dataRow
-    )
-    expect(screen.getByTestId(locators.sectionHeader)).toHaveTextContent('Rises above ↗')
-    expect(triggerPrice).toHaveTextContent('Trigger price')
-    expect(triggerPrice).toHaveTextContent('100')
-    expect(trailingPercentOffset).toHaveTextContent('Trailing offset')
-    expect(trailingPercentOffset).toHaveTextContent('5%')
-    expect(expiryStrategy).toHaveTextContent('Expiry strategy')
-    expect(expiryStrategy).toHaveTextContent('Submit')
-    expect(expiresAt).toHaveTextContent('Expires at')
-    expect(expiresAt).toHaveTextContent('1/1/1970, 12:27:18 AM')
-    expect(screen.getByTestId(locators.orderDetails)).toHaveTextContent('Order details')
-    expect(screen.getByTestId('order-table')).toBeTruthy()
-    expect(screen.getByTestId('order-badges')).toBeTruthy()
+    validateStopOrderDetails('Rises above ↗')
   })
 
   test('renders the component with "Falls below" details', () => {
@@ -68,22 +71,7 @@ describe('StopOrderSubmission', () => {
 
     render(<StopOrderSubmission transaction={transaction} />)
 
-    const [triggerPrice, trailingPercentOffset, expiryStrategy, expiresAt] = screen.getAllByTestId(
-      tableLocators.dataRow
-    )
-
-    expect(screen.getByTestId(locators.sectionHeader)).toHaveTextContent('Falls below ↘')
-    expect(triggerPrice).toHaveTextContent('Trigger price')
-    expect(triggerPrice).toHaveTextContent('100')
-    expect(trailingPercentOffset).toHaveTextContent('Trailing offset')
-    expect(trailingPercentOffset).toHaveTextContent('5%')
-    expect(expiryStrategy).toHaveTextContent('Expiry strategy')
-    expect(expiryStrategy).toHaveTextContent('Submit')
-    expect(expiresAt).toHaveTextContent('Expires at')
-    expect(expiresAt).toHaveTextContent('1/1/1970, 12:27:18 AM')
-    expect(screen.getByTestId(locators.orderDetails)).toHaveTextContent('Order details')
-    expect(screen.getByTestId('order-table')).toBeTruthy()
-    expect(screen.getByTestId('order-badges')).toBeTruthy()
+    validateStopOrderDetails('Falls below ↘')
   })
 
   test('does not render falls below or rises above if not present', () => {

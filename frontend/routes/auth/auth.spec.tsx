66b264cb98a339d617c01a4f5fd63417a4222c13
sam-keen <samuel.kleinmann@gmail.com>
2023-09-13 19:04:11 +0100

    feat(134): setting up assets and markets stores

--- frontend/routes/auth/auth.spec.tsx
+++ frontend/routes/auth/auth.spec.tsx
@@ -4,6 +4,8 @@ import { MemoryRouter } from 'react-router-dom'
 import locators from '../../components/locators'
 
 import { useWalletStore } from '../../stores/wallets'
+import { useAssetsStore } from '../../stores/assets-store'
+import { useMarketsStore } from '../../stores/markets-store'
 import { FULL_ROUTES } from '../route-names'
 
 jest.mock('../../components/page-header', () => ({
@@ -21,6 +23,18 @@ jest.mock('../../contexts/json-rpc/json-rpc-context', () => ({
 
 jest.mock('../../stores/wallets', () => ({
   useWalletStore: jest.fn()
+}))
+
+jest.mock('../../stores/markets-store', () => ({
+  useMarketsStore: jest.fn().mockReturnValue({
+    fetchMarkets: jest.fn()
+  })
+}))
+
+jest.mock('../../stores/assets-store', () => ({
+  useAssetsStore: jest.fn().mockReturnValue({
+    fetchAssets: jest.fn()
+  })
 }))
 
 jest.mock('../../components/modals', () => ({
@@ -74,5 +88,28 @@ describe('Auth', () => {
     renderComponent(FULL_ROUTES.wallets)
 
     expect(screen.getByTestId('dapps-header')).toBeVisible()
+  })
+  it('fetches market data', () => {
+    const mockFetchMarkets = jest.fn()
+    ;(useMarketsStore as unknown as jest.Mock).mockImplementation((fn) => {
+      return fn({
+        fetchMarkets: mockFetchMarkets
+      })
+    })
+    renderComponent()
+
+    expect(mockFetchMarkets).toBeCalledTimes(1)
+  })
+
+  it('fetches assets data', () => {
+    const mockFetchAssets = jest.fn()
+    ;(useAssetsStore as unknown as jest.Mock).mockImplementation((fn) => {
+      return fn({
+        fetchAssets: mockFetchAssets
+      })
+    })
+    renderComponent()
+
+    expect(mockFetchAssets).toBeCalledTimes(1)
   })
 })

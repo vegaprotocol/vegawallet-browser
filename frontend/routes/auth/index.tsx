afae028bc38ca5a8307975fab2979c2c34138c68
sam-keen <samuel.kleinmann@gmail.com>
2023-09-13 19:04:11 +0100

    feat(134): setting up assets and markets stores

--- frontend/routes/auth/index.tsx
+++ frontend/routes/auth/index.tsx
@@ -6,6 +6,8 @@ import { ModalWrapper } from '../../components/modals'
 import { useEffect } from 'react'
 import { useJsonRpcClient } from '../../contexts/json-rpc/json-rpc-context'
 import { useWalletStore } from '../../stores/wallets'
+import { useMarketsStore } from '../../stores/markets-store'
+import { useAssetsStore } from '../../stores/assets-store'
 import { FULL_ROUTES } from '../route-names'
 import { DappsHeader } from '../../components/dapps-header/dapps-header'
 
@@ -17,9 +19,23 @@ export const Auth = () => {
     loadWallets: state.loadWallets
   }))
 
+  // Markets store
+  const { fetchMarkets, markets } = useMarketsStore((state) => ({
+    fetchMarkets: state.fetchMarkets,
+    markets: state.markets
+  }))
+
+  // Assets store
+  const { fetchAssets, assets } = useAssetsStore((state) => ({
+    fetchAssets: state.fetchAssets,
+    assets: state.assets
+  }))
+
   useEffect(() => {
     loadWallets(request)
-  }, [request, loadWallets])
+    // fetchMarkets(request)
+    fetchAssets(request)
+  }, [request, loadWallets, fetchMarkets, fetchAssets])
 
   const isWallets = !!useMatch(FULL_ROUTES.wallets)
 

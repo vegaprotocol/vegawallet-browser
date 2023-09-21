d4fe3fe18474da5e5d46126bfea915e35e9eb7c5
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-09-21 19:11:54 +0100

    test: remove comments

--- test/jest-config/jest.config-backend.js
+++ test/jest-config/jest.config-backend.js
@@ -21,34 +21,3 @@ let overrides = {
 }
 
 export default merge(backendConfig, overrides)
-
-// const backendConfig = {
-//   rootDir: '../..',
-//   roots: ['<rootDir>/web-extension'],
-//   transform: {
-//     '^.+\\.tsx?$': 'ts-jest',
-//     '^.+\\.jsx?$': 'ts-jest'
-//   },
-//   testEnvironment: 'jest-environment-node',
-//   testTimeout: 30000,
-//   reporters: [
-//     'default',
-//     [
-//       'jest-junit',
-//       {
-//         outputDirectory: './test/test-reports',
-//         outputName: 'backend-test-results.xml'
-//       }
-//     ]
-//   ],
-//   setupFiles: ['jest-webextension-mock', './web-extension/test/setup-tests.js'],
-//   collectCoverageFrom: ['**/*.js', '!**/node_modules/**'],
-//   coverageDirectory: 'coverage/backend',
-//   coverageReporters: ['html', 'lcov'],
-//   transformIgnorePatterns: ['/node_modules/(?!(@vegaprotocol)/protos)/'],
-//   moduleNameMapper: {
-//     '!/config': '<rootDir>/config/test.js'
-//   }
-// }
-
-// export default backendConfig

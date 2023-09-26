426b807e1d8b59a57507d1cf7228d1ad8e77ce5a
Dexter <dexter@vega.xyz>
2023-09-26 14:56:38 +0800

    test: add tests for accounts

---
+++ frontend/routes/auth/wallets/key-details/accounts-store.spec.tsx
@@ -0,0 +1,86 @@
+import { RpcMethods } from '../../../../lib/client-rpc-methods'
+import { useAccountsStore } from './accounts-store'
+
+const ACCOUNT_FIXTURE = {
+  owner: 'e81831b7412a9b97632093f651db892af859945ad7aa471835188ca24f54d143',
+  balance: '40000000000000000000',
+  asset: '84fff099818dc4f5319477f0812b4341565cfb32ccf735beede734e386b8108f',
+  marketId: '',
+  type: 'ACCOUNT_TYPE_GENERAL'
+}
+
+const ACCOUNT_RESPONSE_MOCK = {
+  accounts: {
+    edges: [
+      {
+        node: ACCOUNT_FIXTURE,
+        cursor: 'eyJpZCI6ImIzNDBjMTMwMDk2ODE5NDI4YTYyZTVkZjQwN2ZkNmFiZTY2ZTQ0NGI4OWFkNjRmNjcwYmViOTg2MjFjOWM2NjMifQ=='
+      }
+    ],
+    pageInfo: {
+      hasNextPage: false,
+      hasPreviousPage: false,
+      startCursor:
+        'eyJpZCI6ImZkZjBlYzExOGQ5ODM5M2E3NzAyY2Y3MmU0NmZjODdhZDY4MGIxNTJmNjRiMmFhYzU5ZTA5M2FjMmQ2ODhmYmIifQ==',
+      endCursor: 'eyJpZCI6IlhZWmFscGhhIn0='
+    }
+  }
+}
+
+const request = async (method: string) => {
+  if (method === RpcMethods.Fetch) {
+    return ACCOUNT_RESPONSE_MOCK
+  }
+  throw new Error('Tried to request unknown endpoint')
+}
+
+const initialState = useAccountsStore.getState()
+
+describe('AccountsStore', () => {
+  beforeEach(() => {
+    jest.useFakeTimers()
+    useAccountsStore.setState(initialState)
+  })
+  afterEach(() => {
+    jest.clearAllTimers()
+  })
+
+  it('should fetch accounts and group them by asset', async () => {
+    const id = '1'.repeat(64)
+    const { fetchAccounts } = useAccountsStore.getState()
+    await fetchAccounts(id, request)
+    const { accounts, accountsByAsset } = useAccountsStore.getState()
+    expect(accounts).toStrictEqual([ACCOUNT_FIXTURE])
+    expect(accountsByAsset).toStrictEqual({
+      [ACCOUNT_FIXTURE.asset]: [ACCOUNT_FIXTURE]
+    })
+  })
+  it('start poll should fetch accounts every 10 seconds', async () => {
+    const id = '1'.repeat(64)
+    const { startPoll, stopPoll } = useAccountsStore.getState()
+    const mockRequest = jest.fn().mockImplementation(request)
+    startPoll(id, mockRequest)
+    expect(mockRequest).not.toHaveBeenCalled()
+    jest.advanceTimersByTime(10000)
+    expect(mockRequest).toHaveBeenCalledTimes(1)
+    jest.advanceTimersByTime(30000)
+    expect(mockRequest).toHaveBeenCalledTimes(4)
+    stopPoll()
+    jest.advanceTimersByTime(30000)
+    expect(mockRequest).toHaveBeenCalledTimes(4)
+    expect(useAccountsStore.getState().interval).toBe(null)
+  })
+  it('reset should reset all state', async () => {
+    const { reset } = useAccountsStore.getState()
+    const id = '1'.repeat(64)
+    const mockRequest = jest.fn().mockImplementation(request)
+    await useAccountsStore.getState().fetchAccounts(id, mockRequest)
+    expect(useAccountsStore.getState().accounts).toStrictEqual([ACCOUNT_FIXTURE])
+    expect(useAccountsStore.getState().accountsByAsset).toStrictEqual({
+      [ACCOUNT_FIXTURE.asset]: [ACCOUNT_FIXTURE]
+    })
+    reset()
+    expect(useAccountsStore.getState().accounts).toStrictEqual([])
+    expect(useAccountsStore.getState().accountsByAsset).toStrictEqual({})
+  })
+})

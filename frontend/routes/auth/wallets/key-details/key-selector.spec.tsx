d7b2c142d4b97d73694af6dbbe3b2a3dddf62c93
Dexter <dexter@vega.xyz>
2023-09-27 12:49:58 +0800

    test: add tests for key selectors

---
+++ frontend/routes/auth/wallets/key-details/key-selector.spec.tsx
@@ -0,0 +1,47 @@
+import { render, screen } from '@testing-library/react'
+import { KeySelector, locators } from './key-selector'
+
+const ID = '1'.repeat(64)
+
+const MOCK_KEY = {
+  publicKey: ID,
+  name: 'test',
+  index: 0,
+  metadata: []
+}
+
+jest.mock('../../../../components/keys/vega-key', () => ({
+  VegaKey: () => <div data-testid="vega-key" />
+}))
+
+jest.mock('../../../../stores/wallets', () => ({
+  useWalletStore: jest.fn().mockImplementation((fn) => ({
+    keys: [
+      {
+        publicKey: '1'.repeat(64),
+        name: 'test',
+        index: 0,
+        metadata: []
+      },
+      {
+        publicKey: '2'.repeat(24),
+        name: 'test2',
+        index: 1,
+        metadata: []
+      },
+      {
+        publicKey: '3'.repeat(24),
+        name: 'test3',
+        index: 2,
+        metadata: []
+      }
+    ]
+  }))
+}))
+
+describe('KeySelector', () => {
+  it('renders the currently selected key', () => {
+    render(<KeySelector currentKey={MOCK_KEY} />)
+    expect(screen.getByTestId(locators.keySelectedCurrentKey)).toHaveTextContent('test')
+  })
+})
--- frontend/routes/auth/wallets/key-details/key-selector.tsx
+++ frontend/routes/auth/wallets/key-details/key-selector.tsx
@@ -5,6 +5,13 @@ import { Key, useWalletStore } from '../../../../stores/wallets'
 import { NavLink } from 'react-router-dom'
 import { FULL_ROUTES } from '../../../route-names'
 
+export const locators = {
+  keySelectedCurrentKey: 'key-selected-current-key',
+  keySelectedDropdown: 'key-selected-dropdown',
+  keySelectedDropdownItem: 'key-selected-dropdown-item',
+  keySelectorTrigger: 'key-selector-trigger'
+}
+
 export const KeySelector = ({ currentKey }: { currentKey: Key }) => {
   const { keys } = useWalletStore((state) => ({
     keys: state.wallets.flatMap((w) => w.keys)
@@ -14,18 +21,18 @@ export const KeySelector = ({ currentKey }: { currentKey: Key }) => {
       <DropdownMenu
         modal={false}
         trigger={
-          <DropdownMenuTrigger className="text-white">
-            <div className="flex items-center">
+          <DropdownMenuTrigger data-testid={locators.keySelectorTrigger} className="text-white">
+            <div data-testid={locators.keySelectedCurrentKey} className="flex items-center">
               <span className="mr-1 text-2xl">{currentKey.name}</span> <IconChevronDown size={16} />
             </div>
           </DropdownMenuTrigger>
         }
       >
         <DropdownMenuContent style={{ overflow: 'hidden', overflowY: 'auto', maxHeight: 360 }}>
-          <div className="m-4">
+          <div data-testid={locators.keySelectedDropdown} className="m-4">
             <h1 className="text-vega-dark-300 text-sm uppercase">Keys</h1>
             {keys.map((k) => (
-              <div className="my-3 text-base" key={k.publicKey}>
+              <div data-testid={locators.keySelectedDropdownItem} className="my-3 text-base" key={k.publicKey}>
                 <NavLink to={{ pathname: `${FULL_ROUTES.wallets}/${k.publicKey}` }}>
                   <VegaKey publicKey={k.publicKey} name={k.name} />
                 </NavLink>

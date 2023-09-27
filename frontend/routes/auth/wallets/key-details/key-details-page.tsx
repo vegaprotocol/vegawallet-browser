f6b6fe4086b916322440a0034bb942c16b2218b8
Dexter <dexter@vega.xyz>
2023-09-27 12:13:45 +0800

    test: add test for key details page

--- frontend/routes/auth/wallets/key-details/key-details-page.tsx
+++ frontend/routes/auth/wallets/key-details/key-details-page.tsx
@@ -5,6 +5,11 @@ import { AssetCard } from './asset-card'
 import { KeySelector } from './key-selector'
 import { useAccounts } from './use-accounts'
 
+export const locators = {
+  keyDetailsHeading: 'key-details-heading',
+  keyDetailsDescription: 'key-details-description'
+}
+
 export const KeyDetailsPage = ({ id }: { id: string }) => {
   const { accountsByAsset, key } = useAccounts(id)
   const { loading } = useAssetsStore((state) => ({
@@ -22,8 +27,10 @@ export const KeyDetailsPage = ({ id }: { id: string }) => {
         <VegaKey publicKey={key.publicKey} />
       </section>
       <VegaSection>
-        <h1 className="text-vega-dark-300 text-sm uppercase">Balances</h1>
-        <p className="text-vega-dark-400 my-3">
+        <h1 data-testid={locators.keyDetailsHeading} className="text-vega-dark-300 text-sm uppercase">
+          Balances
+        </h1>
+        <p data-testid={locators.keyDetailsDescription} className="text-vega-dark-400 my-3">
           Recent balance changes caused by your open positions may not be reflected below
         </p>
         {Object.entries(accountsByAsset).map(([assetId, val]) => (

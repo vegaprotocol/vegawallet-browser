45117371d4d8b6a6299f299f1b0a433d964b8eec
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-09-29 16:21:47 +0100

    test: move vega key assertion. Did not notice it was not rendered in other location. This makes more sense

--- frontend/routes/auth/wallets/key-details/key-details.spec.tsx
+++ frontend/routes/auth/wallets/key-details/key-details.spec.tsx
@@ -19,12 +19,10 @@ describe('KeyDetails', () => {
     expect(() => render(<KeyDetails />)).toThrowError('Id param not provided')
   })
   it('renders the KeyDetailsPage', () => {
-    // 1125-KEYD-007 In the key details screen I can see my currently selected key and associated info
     ;(useParams as unknown as jest.Mock).mockReturnValue({
       id: '1'
     })
     render(<KeyDetails />)
     expect(screen.getByTestId('key-details-page')).toBeInTheDocument()
-    expect(screen.getByTestId(locators.keyName)).toBeInTheDocument() // smoke to check VegaKey rendered, VegaKey testing checks correct info displayed
   })
 })

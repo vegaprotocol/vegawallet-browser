662f3f01001bed3be5466b4d64a4d751be639102
Dexter <dexter@vega.xyz>
2023-09-29 14:34:36 +0800

    feat: hover state and close menu on click

--- frontend/routes/auth/wallets/key-details/key-selector.tsx
+++ frontend/routes/auth/wallets/key-details/key-selector.tsx
@@ -4,6 +4,7 @@ import { IconChevronDown } from '../../../../components/icons/chevron-down'
 import { Key, useWalletStore } from '../../../../stores/wallets'
 import { NavLink } from 'react-router-dom'
 import { FULL_ROUTES } from '../../../route-names'
+import { useState } from 'react'
 
 export const locators = {
   keySelectedCurrentKey: 'key-selected-current-key',
@@ -16,12 +17,18 @@ export const KeySelector = ({ currentKey }: { currentKey: Key }) => {
   const { keys } = useWalletStore((state) => ({
     keys: state.wallets.flatMap((w) => w.keys)
   }))
+  const [open, setOpen] = useState<boolean>(false)
   return (
     <div className="item-center">
       <DropdownMenu
         modal={false}
+        open={open}
         trigger={
-          <DropdownMenuTrigger data-testid={locators.keySelectorTrigger} className="text-white">
+          <DropdownMenuTrigger
+            onClick={() => setOpen(true)}
+            data-testid={locators.keySelectorTrigger}
+            className="text-white"
+          >
             <div data-testid={locators.keySelectedCurrentKey} className="flex items-center">
               <span className="mr-1 text-2xl">{currentKey.name}</span> <IconChevronDown size={16} />
             </div>
@@ -29,11 +36,15 @@ export const KeySelector = ({ currentKey }: { currentKey: Key }) => {
         }
       >
         <DropdownMenuContent style={{ overflow: 'hidden', overflowY: 'auto', maxHeight: 360 }}>
-          <div data-testid={locators.keySelectedDropdown} className="m-4">
-            <h1 className="text-vega-dark-300 text-sm uppercase">Keys</h1>
+          <div data-testid={locators.keySelectedDropdown} className="my-4">
+            <h1 className="text-vega-dark-300 text-sm uppercase mx-4">Keys</h1>
             {keys.map((k) => (
-              <div data-testid={locators.keySelectedDropdownItem} className="my-3 text-base" key={k.publicKey}>
-                <NavLink to={{ pathname: `${FULL_ROUTES.wallets}/${k.publicKey}` }}>
+              <div
+                data-testid={locators.keySelectedDropdownItem}
+                className="p-3 text-base hover:bg-vega-dark-200"
+                key={k.publicKey}
+              >
+                <NavLink onClick={() => setOpen(false)} to={{ pathname: `${FULL_ROUTES.wallets}/${k.publicKey}` }}>
                   <VegaKey publicKey={k.publicKey} name={k.name} />
                 </NavLink>
               </div>

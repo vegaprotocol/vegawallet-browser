32abfc3f99db0a4297af40d57b55467248dea6b4
Dexter <dexter@vega.xyz>
2023-10-03 20:00:53 +0800

    docs: add TODO

--- frontend/routes/auth/settings/settings-header.tsx
+++ frontend/routes/auth/settings/settings-header.tsx
@@ -2,6 +2,7 @@ export const locators = {
   settingsHeader: 'settings-header'
 }
 
+// TODO generalize to subheader and use everywhere
 export const SettingsHeader = ({ text }: { text: string }) => (
   <div className="text-vega-dark-300 text-sm uppercase" data-testid={locators.settingsHeader}>
     {text}

03146a7767bbd2847e3417af8e178469149af6e1
Dexter <dexter@vega.xyz>
2023-10-04 13:34:09 +0800

    feat: add enriched withdrawal views

--- frontend/components/receipts/withdrawal/index.tsx
+++ frontend/components/receipts/withdrawal/index.tsx
@@ -1,17 +1,30 @@
-import { EthereumKey } from '../../keys/ethereum-key'
+import { useAssetsStore } from '../../../stores/assets-store'
 import { ReceiptComponentProps } from '../receipts'
-import { ReceiptWrapper } from '../utils/receipt-wrapper'
-
-export const locators = {
-  whenSection: 'when-section',
-  whenElement: 'when-element'
-}
+import { BasicWithdrawal } from './basic-withdrawal'
+import { EnrichedWithdrawal } from './enriched-withdrawal'
 
 export const Withdraw = ({ transaction }: ReceiptComponentProps) => {
+  const { loading } = useAssetsStore((state) => ({
+    loading: state.loading
+  }))
+
+  const { receiverAddress } = transaction.withdrawSubmission.ext.erc20
+
   if (!transaction.withdrawSubmission.ext?.erc20) return null
+  if (loading)
+    return (
+      <BasicWithdrawal
+        receiverAddress={receiverAddress}
+        amount={transaction.withdrawSubmission.amount}
+        asset={transaction.withdrawSubmission.asset}
+      />
+    )
+
   return (
-    <ReceiptWrapper>
-      <EthereumKey address={transaction.withdrawSubmission.ext.erc20.receiverAddress} />
-    </ReceiptWrapper>
+    <EnrichedWithdrawal
+      receiverAddress={receiverAddress}
+      amount={transaction.withdrawSubmission.amount}
+      asset={transaction.withdrawSubmission.asset}
+    />
   )
 }

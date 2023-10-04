aace72f0d98ecdd3ee28e26c56e91ef26c329a89
Dexter <dexter@vega.xyz>
2023-10-04 14:30:40 +0800

    test: add tests for withdrawals spec

--- frontend/components/receipts/withdrawal/withdrawal.spec.tsx
+++ frontend/components/receipts/withdrawal/withdrawal.spec.tsx
@@ -1,12 +1,24 @@
 import { render, screen } from '@testing-library/react'
-import { Withdraw } from '.'
+import { Withdraw } from './withdrawal'
+import { useAssetsStore } from '../../../stores/assets-store'
 
-jest.mock('../../keys/ethereum-key', () => ({
-  EthereumKey: () => <div data-testid="ethereum-key" />
+jest.mock('./basic-withdrawal', () => ({
+  BasicWithdrawal: () => <div data-testid="basic-withdrawal" />
 }))
+
+jest.mock('./enriched-withdrawal', () => ({
+  EnrichedWithdrawal: () => <div data-testid="enriched-withdrawal" />
+}))
+
+jest.mock('../../../stores/assets-store')
 
 describe('Withdrawal', () => {
   it('renders nothing if withdrawal is not of type erc20', () => {
+    ;(useAssetsStore as unknown as jest.Mock).mockImplementation((fn) =>
+      fn({
+        loading: true
+      })
+    )
     const { container } = render(
       <Withdraw
         transaction={{
@@ -16,21 +28,42 @@ describe('Withdrawal', () => {
     )
     expect(container).toBeEmptyDOMElement()
   })
-  it('renders ethereum address', () => {
-    // 1123-WITH-001 I can see the Ethereum key I am withdrawing the assets to
+  it('renders basic withdrawal view if loading', () => {
+    ;(useAssetsStore as unknown as jest.Mock).mockImplementation((fn) =>
+      fn({
+        loading: true
+      })
+    )
     render(
       <Withdraw
         transaction={{
           withdrawSubmission: {
-            ext: {
-              erc20: {
-                receiverAddress: '0x1111111111111111111111111111111111111111'
-              }
-            }
+            amount: '2'.repeat(18),
+            assetId: '1'.repeat(64),
+            ext: { erc20: { address: '0xcb84d72e61e383767c4dfeb2d8ff7f4fb89abc6e' } }
           }
         }}
       />
     )
-    expect(screen.getByTestId('ethereum-key')).toBeVisible()
+    expect(screen.getByTestId('basic-withdrawal')).toBeInTheDocument()
+  })
+  it('renders enriched withdrawal view if loading was successful', () => {
+    ;(useAssetsStore as unknown as jest.Mock).mockImplementation((fn) =>
+      fn({
+        loading: false
+      })
+    )
+    render(
+      <Withdraw
+        transaction={{
+          withdrawSubmission: {
+            amount: '2'.repeat(18),
+            assetId: '1'.repeat(64),
+            ext: { erc20: { address: '0xcb84d72e61e383767c4dfeb2d8ff7f4fb89abc6e' } }
+          }
+        }}
+      />
+    )
+    expect(screen.getByTestId('enriched-withdrawal')).toBeInTheDocument()
   })
 })

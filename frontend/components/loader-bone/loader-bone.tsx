d416fc0adbba6681364388a977ce76f60e0c0079
Dexter <dexter@vega.xyz>
2023-10-05 16:17:31 +0800

    chore: add tests for skeleton loader component

--- frontend/components/loader-bone/loader-bone.tsx
+++ frontend/components/loader-bone/loader-bone.tsx
@@ -8,7 +8,13 @@ const pseudoRandom = (seed: number) => {
   }
 }
 
-export const Bone = ({ width, height, baseSize = 1 }: { width: number; height: number; baseSize?: number }) => {
+export const locators = {
+  bone: 'bone',
+  boneCol: 'bone-col',
+  boneSquare: 'bone-square'
+}
+
+export const LoaderBone = ({ width, height, baseSize = 1 }: { width: number; height: number; baseSize?: number }) => {
   const [, forceRender] = useState(false)
   const generate = useMemo(() => pseudoRandom(1), [])
 
@@ -21,14 +27,14 @@ export const Bone = ({ width, height, baseSize = 1 }: { width: number; height: n
   }, [])
 
   return (
-    <div data-testid="bone">
+    <div data-testid={locators.bone}>
       <div className="flex flex-wrap">
         {new Array(width).fill(null).map((_, i) => (
-          <div key={`bone-row-${i}`} data-testid="bone-row">
+          <div key={`bone-col-${i}`} data-testid={locators.boneCol}>
             {new Array(height).fill(null).map((_, j) => (
               <div
                 key={`bone-sqaure-${j}`}
-                data-testid="bone-sqaure"
+                data-testid={locators.boneSquare}
                 style={{ height: baseSize, width: baseSize, opacity: generate() > 1.5 ? 1 : 0 }}
                 className="bg-white"
               />

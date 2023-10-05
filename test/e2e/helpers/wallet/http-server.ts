6fbfddc2f26103f57a3879c8e0b09838e9671ecf
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-10-05 16:12:31 +0100

    test: create the rest of the mock data from the objects

--- test/e2e/helpers/wallet/http-server.ts
+++ test/e2e/helpers/wallet/http-server.ts
@@ -1,6 +1,9 @@
 import http from 'node:http'
 import { Core, TradingData } from '@vegaprotocol/rest-clients'
+import { vegaAccountType } from '@vegaprotocol/rest-clients/dist/trading-data'
 
+const tdaiAssetId = 'b340c130096819428a62e5df407fd6abe66e444b89ad64f670beb98621c9c663'
+const vegaAssetId = 'fc7fd956078fb1fc9db5c19b88f0874c4299b2a7639ad05a47a28c0aef291b55'
 const blockHeightObj: Core.v1LastBlockHeightResponse = {
   height: '3144128',
   hash: '07d5343f3f5645400c13d9576f13ec2f74511236b5f826951646164485f04ba6'
@@ -14,7 +17,7 @@ const rawTransactionObj: Core.v1SubmitRawTransactionResponse = {
 const tradeableInstrument: TradingData.vegaTradableInstrument = {
   instrument: {
     future: {
-      settlementAsset: 'b340c130096819428a62e5df407fd6abe66e444b89ad64f670beb98621c9c663'
+      settlementAsset: tdaiAssetId
     }
   }
 }
@@ -23,7 +26,6 @@ const market: TradingData.vegaMarket = {
   id: '3ab4fc0ea7e6eabe74133fb14ef2d8934ff21dd894ff080a09ec9a3647ceb2a4',
   decimalPlaces: '5',
   tradableInstrument: tradeableInstrument
-  //need settlement asset here
 }
 
 const marketEdge: TradingData.v2MarketEdge = {
@@ -37,17 +39,72 @@ const marketsResponseObj: TradingData.v2ListMarketsResponse = {
   markets: marketConnection
 }
 
+const accountNode: TradingData.v2AccountBalance = {
+  owner: 'a44f8757128286b5c82271ae781ab819f18f3e4feb4a64014980aa98ec1fad0a',
+  balance: '40000000000000000000',
+  asset: tdaiAssetId,
+  marketId: '',
+  type: vegaAccountType.ACCOUNT_TYPE_GENERAL
+}
+
+const accountEdge: TradingData.v2AccountEdge = {
+  node: accountNode
+}
+const accountsResponseObj: TradingData.v2ListAccountsResponse = {
+  accounts: {
+    edges: [accountEdge]
+  }
+}
+
+const tdaiAsset: TradingData.vegaAsset = {
+  id: tdaiAssetId,
+  details: {
+    name: 'tDAI TEST',
+    symbol: 'tDAI',
+    decimals: '5',
+    quantum: '1',
+    erc20: {
+      contractAddress: '0x26223f9C67871CFcEa329975f7BC0C9cB8FBDb9b',
+      lifetimeLimit: '0',
+      withdrawThreshold: '0'
+    }
+  }
+}
+const vegaAsset: TradingData.vegaAsset = {
+  id: vegaAssetId,
+  details: {
+    name: 'Vega (fairground)',
+    symbol: 'VEGA',
+    decimals: '18',
+    quantum: '1',
+    erc20: {
+      contractAddress: '0xdf1B0F223cb8c7aB3Ef8469e529fe81E73089BD9',
+      lifetimeLimit: '0',
+      withdrawThreshold: '0'
+    }
+  }
+}
+
+const tdaiAssetEdge: TradingData.v2AssetEdge = {
+  node: tdaiAsset
+}
+
+const vegaAssetEdge: TradingData.v2AssetEdge = {
+  node: vegaAsset
+}
+
+const assetsConnection: TradingData.v2AssetsConnection = {
+  edges: [tdaiAssetEdge, vegaAssetEdge]
+}
+const assetsResponseObj: TradingData.v2ListAssetsResponse = {
+  assets: assetsConnection
+}
+
 const blockHeightResponse = () => JSON.stringify(blockHeightObj)
 const rawTransactionResponse = () => JSON.stringify(rawTransactionObj)
 const marketsResponse = () => JSON.stringify(marketsResponseObj)
-
-const ACCOUNT_FIXTURE = {
-  owner: 'a44f8757128286b5c82271ae781ab819f18f3e4feb4a64014980aa98ec1fad0a',
-  balance: '40000000000000000000',
-  asset: 'b340c130096819428a62e5df407fd6abe66e444b89ad64f670beb98621c9c663',
-  marketId: '',
-  type: 'ACCOUNT_TYPE_GENERAL'
-}
+const accountsResponse = () => JSON.stringify(accountsResponseObj)
+const assetsResponse = () => JSON.stringify(assetsResponseObj)
 
 export const server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*') /* @dev First, read about security */
@@ -72,26 +129,7 @@ export const server = http.createServer((req, res) => {
   }
 
   if (req.url?.includes('api/v2/accounts')) {
-    res.end(
-      JSON.stringify({
-        accounts: {
-          edges: [
-            {
-              node: ACCOUNT_FIXTURE,
-              cursor:
-                'eyJpZCI6ImIzNDBjMTMwMDk2ODE5NDI4YTYyZTVkZjQwN2ZkNmFiZTY2ZTQ0NGI4OWFkNjRmNjcwYmViOTg2MjFjOWM2NjMifQ=='
-            }
-          ],
-          pageInfo: {
-            hasNextPage: false,
-            hasPreviousPage: false,
-            startCursor:
-              'eyJpZCI6ImZkZjBlYzExOGQ5ODM5M2E3NzAyY2Y3MmU0NmZjODdhZDY4MGIxNTJmNjRiMmFhYzU5ZTA5M2FjMmQ2ODhmYmIifQ==',
-            endCursor: 'eyJpZCI6IlhZWmFscGhhIn0='
-          }
-        }
-      })
-    )
+    res.end(accountsResponse())
     return
   }
 
@@ -101,59 +139,7 @@ export const server = http.createServer((req, res) => {
   }
 
   if (req.url === '/api/v2/assets') {
-    res.end(
-      JSON.stringify({
-        assets: {
-          edges: [
-            {
-              node: {
-                id: 'b340c130096819428a62e5df407fd6abe66e444b89ad64f670beb98621c9c663',
-                details: {
-                  name: 'tDAI TEST',
-                  symbol: 'tDAI',
-                  decimals: '5',
-                  quantum: '1',
-                  erc20: {
-                    contractAddress: '0x26223f9C67871CFcEa329975f7BC0C9cB8FBDb9b',
-                    lifetimeLimit: '0',
-                    withdrawThreshold: '0'
-                  }
-                },
-                status: 'STATUS_ENABLED'
-              },
-              cursor:
-                'eyJpZCI6ImIzNDBjMTMwMDk2ODE5NDI4YTYyZTVkZjQwN2ZkNmFiZTY2ZTQ0NGI4OWFkNjRmNjcwYmViOTg2MjFjOWM2NjMifQ=='
-            },
-            {
-              node: {
-                id: 'fc7fd956078fb1fc9db5c19b88f0874c4299b2a7639ad05a47a28c0aef291b55',
-                details: {
-                  name: 'Vega (fairground)',
-                  symbol: 'VEGA',
-                  decimals: '18',
-                  quantum: '1',
-                  erc20: {
-                    contractAddress: '0xdf1B0F223cb8c7aB3Ef8469e529fe81E73089BD9',
-                    lifetimeLimit: '0',
-                    withdrawThreshold: '0'
-                  }
-                },
-                status: 'STATUS_ENABLED'
-              },
-              cursor:
-                'eyJpZCI6ImZjN2ZkOTU2MDc4ZmIxZmM5ZGI1YzE5Yjg4ZjA4NzRjNDI5OWIyYTc2MzlhZDA1YTQ3YTI4YzBhZWYyOTFiNTUifQ=='
-            }
-          ],
-          pageInfo: {
-            hasNextPage: false,
-            hasPreviousPage: false,
-            startCursor:
-              'eyJpZCI6ImZkZjBlYzExOGQ5ODM5M2E3NzAyY2Y3MmU0NmZjODdhZDY4MGIxNTJmNjRiMmFhYzU5ZTA5M2FjMmQ2ODhmYmIifQ==',
-            endCursor: 'eyJpZCI6IlhZWmFscGhhIn0='
-          }
-        }
-      })
-    )
+    res.end(assetsResponse())
     return
   }
 

bd686c3b7569d5b6e27372435ae2e887d12c5587
Dexter <dexter@vega.xyz>
2023-10-05 16:17:31 +0800

    chore: add tests for skeleton loader component

--- frontend/components/loader-bone/loader-bone.spec.tsx
+++ frontend/components/loader-bone/loader-bone.spec.tsx
@@ -1,5 +1,13 @@
+import { render, screen } from '@testing-library/react'
+import { LoaderBone, locators } from './loader-bone'
+
 describe('LoaderBone', () => {
-    it('shoiuld have tests', () => {
-        expect(false).toBe(true)
-    })
+  it('should have tests render a row for each row of width passed in', () => {
+    render(<LoaderBone width={2} height={1} />)
+    expect(screen.getAllByTestId(locators.boneCol)).toHaveLength(2)
+  })
+  it('should render a square for each row of width*height passed in', () => {
+    render(<LoaderBone width={2} height={2} />)
+    expect(screen.getAllByTestId(locators.boneSquare)).toHaveLength(4)
+  })
 })

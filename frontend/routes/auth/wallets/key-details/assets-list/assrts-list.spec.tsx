3e0356fabd31a73ce09bcd6c26c5ba5a07630df0
Dexter <dexter@vega.xyz>
2023-10-08 10:09:41 +0800

    test: add skeleton test files

---
+++ frontend/routes/auth/wallets/key-details/assets-list/assrts-list.spec.tsx
@@ -0,0 +1,3 @@
+describe('AssetsList', () => {
+    it('renders description and a card for each asset')
+})
---
+++ frontend/routes/auth/wallets/key-details/export-private-key-dialog/export-private-key-dialog.spec.tsx
@@ -0,0 +1,6 @@
+describe('ExportPrivateKeyDialog', () => {
+    it('renders export button')
+    it('open dialog when export button is pressed with title')
+    it('renders form when private key has yet to be loaded')
+    it('renders private key view when private key is loaded')
+})

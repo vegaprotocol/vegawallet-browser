d8c37f6c8363c09aad87e377a7f7934d98fc274a
Dexter <dexter@vega.xyz>
2023-10-09 09:39:19 +0800

    test: add tests for export form

--- frontend/routes/auth/wallets/key-details/export-private-key-dialog/export-private-key-form.spec.tsx
+++ frontend/routes/auth/wallets/key-details/export-private-key-dialog/export-private-key-form.spec.tsx
@@ -1,7 +1,91 @@
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import { ExportPrivateKeyForm, locators } from './export-private-key-form'
+import { useJsonRpcClient } from '../../../../../contexts/json-rpc/json-rpc-context'
+import { REJECTION_ERROR_MESSAGE } from '../../../../../lib/utils'
+
+jest.mock('../../../../../contexts/json-rpc/json-rpc-context')
+
 describe('ExportPrivateKeyForm', () => {
-    it('renders notification, passphrase input, export and close buttons')
-    it('renders error message if passphrase is incorrect')
-    it('renders error message if an unknown error occured')
-    it('calls onSuccess when password is correct')
-    it('calls onClose when close button is pressed')
+  it('renders notification, passphrase input, export and close buttons', () => {
+    ;(useJsonRpcClient as unknown as jest.Mock).mockReturnValue({
+      request: jest.fn().mockReturnValue({ privateKey: '0x123' })
+    })
+    render(<ExportPrivateKeyForm onSuccess={jest.fn()} onClose={jest.fn()} />)
+
+    expect(screen.getByTestId('notification')).toHaveTextContent(
+      'Warning: Never disclose this key. Exposing this key will give anyone who has it access to all assets in your account.'
+    )
+    expect(screen.getByTestId(locators.privateKeyModalPassphrase)).toBeInTheDocument()
+    expect(screen.getByTestId(locators.privateKeyModalSubmit)).toBeInTheDocument()
+    expect(screen.getByTestId(locators.privateKeyModalClose)).toBeInTheDocument()
+  })
+
+  it('renders error message if passphrase is incorrect', async () => {
+    ;(useJsonRpcClient as unknown as jest.Mock).mockReturnValue({
+      request: jest.fn().mockRejectedValue(new Error(REJECTION_ERROR_MESSAGE))
+    })
+    render(<ExportPrivateKeyForm onSuccess={jest.fn()} onClose={jest.fn()} />)
+    const passwordInput = screen.getByTestId(locators.privateKeyModalPassphrase)
+    fireEvent.change(passwordInput, { target: { value: 'password123' } })
+
+    const exportButton = screen.getByTestId(locators.privateKeyModalSubmit)
+    fireEvent.click(exportButton)
+    await screen.findByTestId('input-error-text')
+    expect(screen.getByTestId('input-error-text')).toHaveTextContent('Incorrect passphrase')
+  })
+
+  it('renders error message if an unknown error occurred', async () => {
+    ;(useJsonRpcClient as unknown as jest.Mock).mockReturnValue({
+      request: jest.fn().mockRejectedValue(new Error('Unknown error'))
+    })
+    render(<ExportPrivateKeyForm onSuccess={jest.fn()} onClose={jest.fn()} />)
+    const passwordInput = screen.getByTestId(locators.privateKeyModalPassphrase)
+    fireEvent.change(passwordInput, { target: { value: 'password123' } })
+
+    const exportButton = screen.getByTestId(locators.privateKeyModalSubmit)
+    fireEvent.click(exportButton)
+    await screen.findByTestId('input-error-text')
+    expect(screen.getByTestId('input-error-text')).toHaveTextContent('Unknown error occurred Error: Unknown error')
+  })
+
+  it('calls onSuccess when form is submitted successfully', async () => {
+    ;(useJsonRpcClient as unknown as jest.Mock).mockReturnValue({
+      request: jest.fn().mockReturnValue({ privateKey: '0x123' })
+    })
+    const onSuccess = jest.fn()
+    const onClose = jest.fn()
+    render(<ExportPrivateKeyForm onSuccess={onSuccess} onClose={onClose} />)
+
+    // Simulate user entering a password
+    const passwordInput = screen.getByTestId(locators.privateKeyModalPassphrase)
+    fireEvent.change(passwordInput, { target: { value: 'password123' } })
+
+    // Simulate form submission
+    const exportButton = screen.getByTestId(locators.privateKeyModalSubmit)
+    fireEvent.click(exportButton)
+
+    // Wait for the success callback to be called
+    await waitFor(() => expect(onSuccess).toHaveBeenCalledTimes(1))
+    expect(onSuccess).toHaveBeenCalledWith('0x123')
+    expect(onClose).not.toHaveBeenCalled()
+  })
+
+  it.only('calls onClose when close button is clicked', async () => {
+    ;(useJsonRpcClient as unknown as jest.Mock).mockReturnValue({
+      request: jest.fn().mockReturnValue({ privateKey: '0x123' })
+    })
+    const onSuccess = jest.fn()
+    const onClose = jest.fn()
+    render(<ExportPrivateKeyForm onSuccess={onSuccess} onClose={onClose} />)
+
+    // Simulate user clicking the close button
+    const closeButton = screen.getByTestId(locators.privateKeyModalClose)
+    fireEvent.click(closeButton)
+
+    // Check if the close callback is called
+    await waitFor(() => expect(onClose).toHaveBeenCalled())
+
+    // Check if the success callback is not called
+    expect(onSuccess).not.toHaveBeenCalled()
+  })
 })

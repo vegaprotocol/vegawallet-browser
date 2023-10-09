0a25e320a12a1f5f47b286cfdf9acf52ad007dab
Dexter <dexter@vega.xyz>
2023-10-09 15:51:44 +0800

    chore: use gets over optional chaining where appropriate

--- frontend/hooks/format-asset-amount/format-asset-amount.tsx
+++ frontend/hooks/format-asset-amount/format-asset-amount.tsx
@@ -1,13 +1,14 @@
 import { formatNumber, toBigNum } from '@vegaprotocol/utils'
 import { useAssetsStore } from '../../stores/assets-store'
+import get from 'lodash/get'
 
 export const useFormatAssetAmount = (assetId: string, amount: string) => {
   const { getAssetById } = useAssetsStore((state) => ({
     getAssetById: state.getAssetById
   }))
   const assetInfo = getAssetById(assetId)
-  const decimals = Number(assetInfo?.details?.decimals)
-  const symbol = assetInfo?.details?.symbol
+  const decimals = Number(get(assetInfo, 'details.decimals'))
+  const symbol = get(assetInfo, 'details.symbol')
   if (!symbol || !decimals)
     throw new Error(`Could not find amount, decimals or symbol when trying to render transaction for asset ${assetId}`)
   const formattedAmount = formatNumber(toBigNum(amount, decimals), decimals)

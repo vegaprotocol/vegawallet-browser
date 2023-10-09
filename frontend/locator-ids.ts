3c4a21ce6ba5009e8fc754f8a252a11b3175183e
Dexter <dexter@vega.xyz>
2023-10-09 14:26:28 +0800

    chore: remove locators that were actually unused

--- frontend/locator-ids.ts
+++ frontend/locator-ids.ts
@@ -1,17 +1,10 @@
 export const networkIndicator = 'network-indicator'
-export const revealRecoveryPhraseButton = 'reveal-recovery-phrase'
-export const copyRecoveryPhraseToClipboardButton = 'copy-recovery-phrase'
-export const secureWalletContinueButton = 'secure-wallet-continue'
-
-export const walletCreatedIcon = 'wallet-created'
 
 export const importMnemonic = 'import-mnemonic'
 export const importMnemonicSubmit = 'import-mnemonic-submit'
 export const importMnemonicDescription = 'import-mnemonic-description'
 
-export const reportBugsAndCrashesButtonMessage = 'report-bugs-and-crashes'
 export const errorMessage = 'input-error-text'
-export const viewWalletsHeader = 'view-wallets-header'
 
 export const connectionsNoConnections = 'connections-no-connections'
 export const connectionsHeader = 'connections-header'

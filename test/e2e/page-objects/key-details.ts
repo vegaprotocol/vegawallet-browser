3a450c3b387a66796ac602057d3d7c8f57d6c310
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-10-09 16:03:12 +0100

    test: failing test for now. Revisit after dex changes

--- test/e2e/page-objects/key-details.ts
+++ test/e2e/page-objects/key-details.ts
@@ -3,12 +3,14 @@ import { clickElement, getByDataTestID, getElementText, isElementDisplayed } fro
 import { locators as keyListLocators } from '../../../frontend/components/key-list'
 import { locators as keyDropdownLocators } from '../../../frontend/routes/auth/wallets/key-details/key-selector'
 import { locators as assetCardLocators } from '../../../frontend/routes/auth/wallets/key-details/assets-list/asset-card'
+import { locators as exportPrivateKeyLocators } from '../../../frontend/routes/auth/wallets/key-details/export-private-key-dialog/export-private-key-dialog'
 
 import generalLocators from '../../../frontend/components/locators'
 
 export class KeyDetails {
   private readonly keysDropdownMenu: By = getByDataTestID(keyDropdownLocators.keySelectorTrigger)
   private readonly assetBalance: By = getByDataTestID(assetCardLocators.assetHeaderTotal)
+  private readonly exportPrivateKey: By = getByDataTestID(exportPrivateKeyLocators.privateKeyTrigger)
   private readonly expandAssetBalanceDetails: By = getByDataTestID(generalLocators.dropdownArrow)
   private viewDetailsButton(keyName: string): By {
     return getByDataTestID(keyListLocators.viewDetails(keyName))
@@ -24,6 +26,11 @@ export class KeyDetails {
 
   async openKeyDetails(keyName: string) {
     await clickElement(this.driver, this.viewDetailsButton(keyName))
+  }
+
+  async openExportPrivateKeyDialog() {
+    await this.checkOnExpectedKeyDetails()
+    await clickElement(this.driver, this.exportPrivateKey)
   }
 
   async getAssetBalance() {

975e73d326ebe3df4580b028aa50b3bf6bda8d20
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-10-10 12:39:42 +0100

    test: add another test comment

--- test/e2e/helpers/driver.ts
+++ test/e2e/helpers/driver.ts
@@ -112,3 +112,5 @@ export const captureScreenshot = async (driver: WebDriver, testName: string) => 
   await fs.ensureDir(path.dirname(screenshotPath))
   fs.writeFileSync(screenshotPath, screenshotData, 'base64')
 }
+
+// dj khaled (another one)

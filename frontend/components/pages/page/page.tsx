8dc513fda887637d375a991108d1f14f1a454cf3
Dexter <dexter@vega.xyz>
2023-10-10 14:04:54 +0800

    chore: support on page back in the onboarding page component

--- frontend/components/pages/page/page.tsx
+++ frontend/components/pages/page/page.tsx
@@ -4,18 +4,19 @@ import { Icon } from '@vegaprotocol/ui-toolkit'
 import { useNavigate } from 'react-router-dom'
 
 export const locators = {
-  authPageBack: 'auth-page-back'
+  basePageBack: 'base-page-back'
 }
 
-export interface AuthPageProps {
+export interface BasePageProps {
   dataTestId: string
   title: ReactNode
   children: ReactNode
   backLocation?: string
   className?: string
+  onBack?: () => void
 }
 
-export const BasePage = ({ className, dataTestId, title, children, backLocation }: AuthPageProps) => {
+export const BasePage = ({ className, dataTestId, title, children, backLocation, onBack }: BasePageProps) => {
   const navigate = useNavigate()
   return (
     <section className={className} data-testid={dataTestId}>
@@ -23,9 +24,15 @@ export const BasePage = ({ className, dataTestId, title, children, backLocation 
         <Header
           content={
             <div className="flex">
-              <button data-testid={`${dataTestId}-back`} onClick={() => navigate(backLocation)}>
+              <button
+                data-testid={`${dataTestId}-back`}
+                onClick={() => {
+                  onBack?.()
+                  navigate(backLocation)
+                }}
+              >
                 <div
-                  data-testid={locators.authPageBack}
+                  data-testid={locators.basePageBack}
                   className="flex flex-col justify-center mr-2 text-vega-dark-300"
                 >
                   <Icon size={6} name="chevron-left" />

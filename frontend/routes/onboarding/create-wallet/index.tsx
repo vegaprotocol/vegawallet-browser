8e7bb09f697685596167b0758a1b7706c1c11bfc
Dexter <dexter@vega.xyz>
2023-10-10 12:29:24 +0800

    chore: use header in create wallet page

--- frontend/routes/onboarding/create-wallet/index.tsx
+++ frontend/routes/onboarding/create-wallet/index.tsx
@@ -2,6 +2,7 @@ import { Button } from '@vegaprotocol/ui-toolkit'
 import { Wallet } from '../../../components/icons/wallet'
 import { useNavigate } from 'react-router-dom'
 import { FULL_ROUTES } from '../../route-names'
+import { Header } from '../../../components/header'
 
 export const locators = {
   createNewWalletButton: 'create-new-wallet',
@@ -15,14 +16,14 @@ export const CreateWallet = () => {
       <div className="mx-auto pb-4 text-white">
         <Wallet size={48} squareFill="black" />
       </div>
-      <h1 className="text-2xl text-white pb-6">Create a wallet</h1>
+      <Header content="Create a wallet" />
       <Button
         autoFocus
         data-testid={locators.createNewWalletButton}
         onClick={() => {
           navigate(FULL_ROUTES.saveMnemonic)
         }}
-        className="mb-4"
+        className="mt-6 mb-4"
         variant="primary"
       >
         Create a wallet

77757d92aa7aa8853621b80aa66440a89eabf76d
Dexter <dexter@vega.xyz>
2023-10-10 14:04:54 +0800

    chore: support on page back in the onboarding page component

--- frontend/components/pages/onboarding-page/onboarding-page.tsx
+++ frontend/components/pages/onboarding-page/onboarding-page.tsx
@@ -5,13 +5,15 @@ export interface PageProps {
   name: string
   children: React.ReactElement
   backLocation?: string
+  onBack?: () => void
 }
 
-export const OnboardingPage = ({ name, children, backLocation }: PageProps) => {
+export const OnboardingPage = ({ onBack, name, children, backLocation }: PageProps) => {
   const testId = useMemo(() => name.replace(/ /g, '-').toLowerCase(), [name])
   return (
     <BasePage
       title={name}
+      onBack={onBack}
       className="pt-14 px-5 h-full pb-8 overflow-y-auto"
       backLocation={backLocation}
       dataTestId={testId}

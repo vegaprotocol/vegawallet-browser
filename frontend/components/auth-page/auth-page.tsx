f399a366dee51a5d863317ea9c39d56e5526afab
Dexter <dexter@vega.xyz>
2023-10-10 12:41:20 +0800

    chore: add support for back button on auth page component

--- frontend/components/auth-page/auth-page.tsx
+++ frontend/components/auth-page/auth-page.tsx
@@ -1,21 +1,44 @@
 import { ReactNode } from 'react'
 import { Header } from '../header'
+import { Icon } from '@vegaprotocol/ui-toolkit'
+import { useNavigate } from 'react-router-dom'
 
 export const locators = {
-  authPageTitle: 'auth-page-title'
+  authPageTitle: 'auth-page-title',
+  authPageBack: 'auth-page-back'
 }
 
 export interface AuthPageProps {
   dataTestId: string
   title: ReactNode
   children: ReactNode
+  backLocation?: string
 }
 
-export const AuthPage = ({ dataTestId, title, children }: AuthPageProps) => {
+export const AuthPage = ({ dataTestId, title, children, backLocation }: AuthPageProps) => {
+  const navigate = useNavigate()
   return (
     <section data-testid={dataTestId}>
-      <Header content={title} />
-      {children}
+      {backLocation ? (
+        <Header
+          content={
+            <div className="flex">
+              <button data-testid={`${dataTestId}-back`} onClick={() => navigate(backLocation)}>
+                <div
+                  data-testid={locators.authPageBack}
+                  className="flex flex-col justify-center mr-2 text-vega-dark-300"
+                >
+                  <Icon size={6} name="chevron-left" />
+                </div>
+              </button>
+              {title}
+            </div>
+          }
+        />
+      ) : (
+        <Header content={title} />
+      )}
+      <div>{children}</div>
     </section>
   )
 }

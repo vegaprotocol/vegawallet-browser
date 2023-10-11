876e46372e08c00be2850388e53739441e573f4d
Emil Bay <github@tixz.dk>
2023-10-11 10:34:47 +0200

    feat: export key backend

--- web-extension/backend/admin-ns.js
+++ web-extension/backend/admin-ns.js
@@ -6,13 +6,14 @@ import { createWindow } from './windows.js'
 
 const windows = globalThis.browser?.windows ?? globalThis.chrome?.windows
 
-function doValidate(validator, params) {
-  if (!validator(params))
+function doValidate (validator, params) {
+  if (!validator(params)) {
     throw new JSONRPCServer.Error(
       validator.errors[0].message,
       1,
       validator.errors.map((e) => e.message)
     )
+  }
 }
 
 /**
@@ -26,7 +27,7 @@ function doValidate(validator, params) {
  * @param {Function} onerror Error handler
  * @returns {JSONRPCServer}
  */
-export default function init({ encryptedStore, settings, wallets, networks, connections, onerror }) {
+export default function init ({ encryptedStore, settings, wallets, networks, connections, onerror }) {
   connections.listen((ev, connection) => {
     server.notify('admin.connections_change', {
       add: ev === 'set' ? [connection] : [],
@@ -46,7 +47,7 @@ export default function init({ encryptedStore, settings, wallets, networks, conn
   var server = new JSONRPCServer({
     onerror,
     methods: {
-      async 'admin.open_popout'(params) {
+      async 'admin.open_popout' (params) {
         doValidate(adminValidation.openPopout, params)
         if (handle == null) {
           const popout = await createWindow()
@@ -80,7 +81,7 @@ export default function init({ encryptedStore, settings, wallets, networks, conn
         }
       },
 
-      async 'admin.update_app_settings'(params) {
+      async 'admin.update_app_settings' (params) {
         doValidate(adminValidation.updateAppSettings, params)
         await settings.transaction(async (store) => {
           for (const [key, value] of Object.entries(params)) {
@@ -91,7 +92,7 @@ export default function init({ encryptedStore, settings, wallets, networks, conn
         return null
       },
 
-      async 'admin.create_passphrase'(params) {
+      async 'admin.create_passphrase' (params) {
         doValidate(adminValidation.createPassphrase, params)
 
         await encryptedStore.create(params.passphrase)
@@ -99,7 +100,7 @@ export default function init({ encryptedStore, settings, wallets, networks, conn
         return null
       },
 
-      async 'admin.update_passphrase'(params) {
+      async 'admin.update_passphrase' (params) {
         doValidate(adminValidation.updatePassphrase, params)
         if ((await encryptedStore.exists()) === false) throw new JSONRPCServer.Error('Encryption not initialised', 1)
         try {
@@ -112,39 +113,38 @@ export default function init({ encryptedStore, settings, wallets, networks, conn
         return null
       },
 
-      async 'admin.unlock'(params) {
+      async 'admin.unlock' (params) {
         doValidate(adminValidation.unlock, params)
         if ((await encryptedStore.exists()) === false) throw new JSONRPCServer.Error('Encryption not initialised', 1)
         try {
           await encryptedStore.unlock(params.passphrase)
         } catch (e) {
-          if (e.message === 'Invalid passphrase or corrupted storage')
-            throw new JSONRPCServer.Error('Invalid passphrase or corrupted storage', 1)
+          if (e.message === 'Invalid passphrase or corrupted storage') { throw new JSONRPCServer.Error('Invalid passphrase or corrupted storage', 1) }
           throw e
         }
 
         return null
       },
 
-      async 'admin.lock'(params) {
+      async 'admin.lock' (params) {
         doValidate(adminValidation.lock, params)
         await encryptedStore.lock()
 
         return null
       },
 
-      async 'admin.list_networks'(params) {
+      async 'admin.list_networks' (params) {
         doValidate(adminValidation.listNetworks, params)
         return { networks: await networks.list() }
       },
 
-      async 'admin.generate_recovery_phrase'(params) {
+      async 'admin.generate_recovery_phrase' (params) {
         doValidate(adminValidation.generateRecoveryPhrase, params)
 
         return { recoveryPhrase: await wallets.generateRecoveryPhrase() }
       },
 
-      async 'admin.import_wallet'(params) {
+      async 'admin.import_wallet' (params) {
         doValidate(adminValidation.importWallet, params)
 
         try {
@@ -156,25 +156,33 @@ export default function init({ encryptedStore, settings, wallets, networks, conn
         return null
       },
 
-      async 'admin.list_wallets'(params) {
+      async 'admin.list_wallets' (params) {
         doValidate(adminValidation.listWallets, params)
 
         return { wallets: await wallets.list() }
       },
 
-      async 'admin.list_keys'(params) {
+      async 'admin.list_keys' (params) {
         doValidate(adminValidation.listKeys, params)
 
         return { keys: Array.from(await wallets.listKeys(params)) }
       },
 
-      async 'admin.generate_key'(params) {
+      async 'admin.generate_key' (params) {
         doValidate(adminValidation.generateKey, params)
 
         return await wallets.generateKey(params)
       },
 
-      async 'admin.sign_message'(params) {
+      async 'admin.export_key' (params) {
+        doValidate(adminValidation.exportKey, params)
+
+        if ((await encryptedStore.verifyPassphrase(params.passphrase)) !== true) throw new JSONRPCServer.Error('Invalid passphrase', 1)
+
+        return await wallets.exportKey({ publicKey: params.publicKey })
+      },
+
+      async 'admin.sign_message' (params) {
         doValidate(adminValidation.signMessage, params)
 
         const key = await wallets.getKeypair({ publicKey: params.publicKey })
@@ -187,13 +195,13 @@ export default function init({ encryptedStore, settings, wallets, networks, conn
         return { signature: toBase64(signature) }
       },
 
-      async 'admin.list_connections'(params) {
+      async 'admin.list_connections' (params) {
         doValidate(adminValidation.listConnections, params)
 
         return { connections: await connections.list() }
       },
 
-      async 'admin.remove_connection'(params) {
+      async 'admin.remove_connection' (params) {
         doValidate(adminValidation.removeConnection, params)
 
         await connections.delete(params.origin)
@@ -201,7 +209,7 @@ export default function init({ encryptedStore, settings, wallets, networks, conn
         return null
       },
 
-      async 'admin.fetch'(params) {
+      async 'admin.fetch' (params) {
         doValidate(adminValidation.fetch, params)
 
         try {

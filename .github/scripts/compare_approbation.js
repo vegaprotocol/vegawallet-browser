03103bf54e7ed8b048a2c6e0d9248ddae233245d
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-10-11 20:30:08 +0100

    test: report where ac coverage has increased and decreased in a comment

---
+++ .github/scripts/compare_approbation.js
@@ -0,0 +1,97 @@
+import { createReadStream, createWriteStream } from 'fs';
+import csv from 'csv-parser';
+import path from 'path';
+
+function parseCSV(filePath) {
+  return new Promise((resolve, reject) => {
+    const data = [];
+
+    createReadStream(filePath)
+      .pipe(csv())
+      .on('data', (row) => {
+        data.push(row);
+      })
+      .on('end', () => {
+        resolve(data);
+      })
+      .on('error', (error) => {
+        reject(error);
+      });
+  });
+}
+
+async function compareCoverage(file1, file2, outputLogFile) {
+  const data1 = await parseCSV(file1);
+  const data2 = await parseCSV(file2);
+
+  const decreasedCoverage = [];
+  const increasedCoverage = [];
+  const newSpecs = [];
+
+  data1.forEach((row1) => {
+    const matchingRow2 = data2.find((row2) => row1.File === row2.File);
+    if (matchingRow2) {
+      const coverage1 = parseFloat(row1.Coverage);
+      const coverage2 = parseFloat(matchingRow2.Coverage);
+      const coverageDiff = coverage2 - coverage1;
+
+      if (coverageDiff > 0) {
+        increasedCoverage.push({ file: row1.File, change: coverageDiff });
+      } else if (coverageDiff < 0) {
+        decreasedCoverage.push({ file: row1.File, change: -coverageDiff });
+      }
+    }
+  });
+
+  data2.forEach((row2) => {
+    const matchingRow1 = data1.find((row1) => row2.File === row1.File);
+    if (!matchingRow1) {
+      newSpecs.push({ file: row2.File, coverage: parseFloat(row2.Coverage) });
+    }
+  });
+
+  const reportContent = [];
+
+  if (decreasedCoverage.length > 0) {
+    reportContent.push('Decreased in coverage:');
+    decreasedCoverage.forEach((change) => {
+      reportContent.push(`${change.file} by ${change.change.toFixed(2)}%`);
+    });
+  }
+
+  if (increasedCoverage.length > 0) {
+    reportContent.push('\nIncreased in coverage:');
+    increasedCoverage.forEach((change) => {
+      reportContent.push(`${change.file} by ${change.change.toFixed(2)}%`);
+    });
+  }
+
+  if (newSpecs.length > 0) {
+    reportContent.push('\nNew specs and coverage:');
+    newSpecs.forEach((spec) => {
+      reportContent.push(`${spec.file} ${spec.coverage}% coverage`);
+    });
+  }
+
+  if (decreasedCoverage.length > 0) {
+    reportContent.push("\n\n-------------\nWARNING: You have files with decreased AC. This should not be happening. Please investigate the following files:");
+    decreasedCoverage.forEach((change) => {
+      reportContent.push(change.file);
+    });
+  }
+
+  const outputLogStream = createWriteStream(outputLogFile, { flags: 'w' });
+  outputLogStream.write(reportContent.join('\n'));
+  outputLogStream.end();
+}
+
+const file1 = path.resolve(process.cwd(), process.argv[2]); // Resolve to CWD
+const file2 = path.resolve(process.cwd(), process.argv[3]); // Second CSV file from command line arguments
+const outputLogFile = '../../report.txt'; // Name of the output report file
+
+if (!file1 || !file2) {
+  console.error('Usage: node compareCoverage.js <file1.csv> <file2.csv>');
+} else {
+  compareCoverage(file1, file2, outputLogFile);
+  console.log(`Report saved to ${outputLogFile}`);
+}

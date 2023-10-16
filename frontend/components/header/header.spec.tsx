df93d05ac24b4572e5c71db15d3a61626fc7eb91
Dexter <dexter@vega.xyz>
2023-10-16 14:18:26 +0800

    style: lint

--- frontend/components/header/header.spec.tsx
+++ frontend/components/header/header.spec.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { Header, locators } from './header';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { Header, locators } from './header'
 
 describe('Header component', () => {
   test('renders header correctly', () => {
-    const content = 'My Header';
-    render(<Header content={content} />);
-    
-    const headerElement = screen.getByTestId(locators.header);
-    
-    expect(headerElement).toBeInTheDocument();
-    expect(headerElement).toHaveTextContent(content);
-    expect(headerElement).toHaveClass('flex', 'justify-center', 'flex-col', 'text-2xl', 'text-white');
-  });
-});
+    const content = 'My Header'
+    render(<Header content={content} />)
+
+    const headerElement = screen.getByTestId(locators.header)
+
+    expect(headerElement).toBeInTheDocument()
+    expect(headerElement).toHaveTextContent(content)
+    expect(headerElement).toHaveClass('flex', 'justify-center', 'flex-col', 'text-2xl', 'text-white')
+  })
+})

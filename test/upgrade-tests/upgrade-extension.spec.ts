b41bb91d4d19d962eae67c2b07eed1d670eec29c
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-10-17 16:44:21 +0100

    test: upgrade test now passing with slightly dodgy logic. Will tidy tomorrow

--- test/upgrade-tests/upgrade-extension.spec.ts
+++ test/upgrade-tests/upgrade-extension.spec.ts
@@ -7,11 +7,12 @@ import {
   oldExtensionDirectory
 } from '../e2e/helpers/driver'
 import { NavPanel } from '../e2e/page-objects/navpanel'
-import { windowHandleHasCount } from '../e2e/helpers/selenium-util'
+import { staticWait, switchWindowHandles, windowHandleHasCount } from '../e2e/helpers/selenium-util'
 import { navigateToExtensionLandingPage } from '../e2e/helpers/wallet/wallet-setup'
 import { copyDirectoryToNewLocation, updateOrAddJsonProperty } from '../e2e/helpers/file-system'
 import { chromePublicKey } from '../../rollup/postbuild'
 import { Settings } from '../e2e/page-objects/settings'
+import { Login } from '../e2e/page-objects/login'
 
 describe('Check migration of settings after upgrade', () => {
   let driver: WebDriver
@@ -29,13 +30,20 @@ describe('Check migration of settings after upgrade', () => {
     navPanel = new NavPanel(driver)
     await navPanel.goToSettings()
     await copyDirectoryToNewLocation(extensionPath + '/chrome', oldExtensionDirectory)
+    await updateOrAddJsonProperty(oldExtensionDirectory + '/manifest.json', 'key', chromePublicKey)
+    await driver.executeScript('await chrome.runtime.reload()')
+    await staticWait(2000)
+    const newHandles = await driver.getAllWindowHandles()
+    await switchWindowHandles(driver, false, newHandles[0])
     await navigateToExtensionLandingPage(driver, extensionID)
+    const login = new Login(driver)
+    await login.login()
     settingsPage = await navPanel.goToSettings()
   })
 
   afterEach(async () => {
     await captureScreenshot(driver, expect.getState().currentTestName as string)
-    //await driver.quit()
+    await driver.quit()
   })
 
   it('can navigate to settings and update telemetry opt in/out preference', async () => {

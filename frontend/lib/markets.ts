bca8a2499685f4b26357278b82516eb2175af9bb
Dexter <dexter@vega.xyz>
2023-10-17 16:37:43 +0800

    chore: add isActive helper function

--- frontend/lib/markets.ts
+++ frontend/lib/markets.ts
@@ -1,4 +1,4 @@
-import { vegaMarket } from '@vegaprotocol/rest-clients/dist/trading-data'
+import { MarketTradingMode, vegaMarket } from '@vegaprotocol/rest-clients/dist/trading-data'
 import get from 'lodash/get'
 
 export const getSettlementAssetId = (market: vegaMarket) => {
@@ -9,4 +9,11 @@ export const getSettlementAssetId = (market: vegaMarket) => {
     throw new Error(`Could not find settlement asset from market ${market.id}`)
   }
   return assetId
+}
+
+export const isActiveMarket = (market: vegaMarket) => {
+  if (!market.tradingMode) return false
+  return [MarketTradingMode.TRADING_MODE_CONTINUOUS, MarketTradingMode.TRADING_MODE_MONITORING_AUCTION].includes(
+    market.tradingMode
+  )
 }

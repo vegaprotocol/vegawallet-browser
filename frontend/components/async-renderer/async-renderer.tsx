81dbb0d423f5ec48f3d375abff5fdcfa111e017e
Dexter <dexter@vega.xyz>
2023-10-17 14:53:13 +0800

    chore: build out an async renderer view

---
+++ frontend/components/async-renderer/async-renderer.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react'
+
+export interface AsyncRendererProps {
+  loading: boolean
+  renderLoading?: () => ReactNode
+  error: Error | null
+  errorView?: (error: Error) => ReactNode
+  noData: boolean
+  renderNoData?: () => ReactNode
+  render: () => ReactNode
+}
+
+export function AsyncRenderer({
+  loading,
+  renderLoading,
+  error,
+  errorView,
+  noData,
+  renderNoData,
+  render
+}: AsyncRendererProps) {
+  if (loading) return renderLoading ? <>{renderLoading()}</> : null
+  if (error) return errorView ? <>{errorView(error)}</> : null
+  if (noData) return renderNoData ? <>{renderNoData()}</> : null
+  return <>{render()}</>
+}

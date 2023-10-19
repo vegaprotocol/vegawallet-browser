b752ccfdbf224862fe8e88056c56e4d581ec531a
sam-keen <samuel.kleinmann@gmail.com>
2023-10-19 14:20:21 +0100

    feat(418): mass order cancellation cases

--- frontend/components/receipts/orders/cancellation/cancellation.spec.tsx
+++ frontend/components/receipts/orders/cancellation/cancellation.spec.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import { Cancellation } from '.'
+import { Cancellation, CancellationNotification } from '.'
 
 describe('Cancellation', () => {
   it('should render orderId and marketId', () => {
@@ -15,5 +15,27 @@ describe('Cancellation', () => {
     )
     expect(screen.getByText('orderId')).toBeInTheDocument()
     expect(screen.getByText('marketId')).toBeInTheDocument()
+  })
+})
+
+describe('CancellationNotification', () => {
+  it('should display "Cancel all open orders in this market" when marketId is provided and orderId is not', () => {
+    render(<CancellationNotification orderId="" marketId="some-market-id" />)
+    expect(screen.getByText('Cancel all open orders in this market')).toBeInTheDocument()
+  })
+
+  it('should display "Cancel all open orders in all markets" when neither orderId nor marketId is provided', () => {
+    render(<CancellationNotification orderId="" marketId="" />)
+    expect(screen.getByText('Cancel all open orders in all markets')).toBeInTheDocument()
+  })
+
+  it('should not render any Notification when orderId is provided', () => {
+    const { container } = render(<CancellationNotification orderId="some-order-id" marketId="" />)
+    expect(container.firstChild).toBeNull()
+  })
+
+  it('should not render any Notification when both orderId and marketId are provided', () => {
+    const { container } = render(<CancellationNotification orderId="some-order-id" marketId="some-market-id" />)
+    expect(container.firstChild).toBeNull()
   })
 })

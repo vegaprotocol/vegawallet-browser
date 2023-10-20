fbe25bfa6fdafa1e0928f77ad7b73d5b208ed435
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-10-20 10:11:44 +0100

    test: fix comparison bracket syntax

--- .github/scripts/upgrade-test-download-management/download_old_extension.sh
+++ .github/scripts/upgrade-test-download-management/download_old_extension.sh
@@ -5,11 +5,11 @@ source "$CURRENT_DIR/common-values.sh"
 mkdir -p "$ZIP_EXTRACT_DESTINATION"
 wget -O "$ZIP_FILE" "$URL"
 
-if [ $? -eq 0 ]; then
+if [[ $? -eq 0 ]]; then
   echo "File downloaded successfully to $ZIP_FILE"
   unzip -o "$ZIP_FILE" -d "$ZIP_EXTRACT_DESTINATION"
 
-  if [ $? -eq 0 ]; then
+  if [[ $? -eq 0 ]]; then
     echo "File extracted successfully to $ZIP_EXTRACT_DESTINATION"
   else
     echo "Extraction failed. Check the ZIP file or the 'unzip' tool."

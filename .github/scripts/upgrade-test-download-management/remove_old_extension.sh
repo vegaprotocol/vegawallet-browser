9ee6eb1edfe34348dfc4d7e42e07c3351d4f289e
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-10-20 10:11:44 +0100

    test: fix comparison bracket syntax

--- .github/scripts/upgrade-test-download-management/remove_old_extension.sh
+++ .github/scripts/upgrade-test-download-management/remove_old_extension.sh
@@ -4,7 +4,7 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/common-values.sh"
 rm -r "$ZIP_EXTRACT_DESTINATION"
 
-if [ $? -eq 0 ]; then
+if [[ $? -eq 0 ]]; then
   echo "Removed the contents in $ZIP_EXTRACT_DESTINATION)"
 else
   echo "Removal failed. Check the directory or permissions."
@@ -12,7 +12,7 @@ fi
 
 rm "$ZIP_FILE"
 
-if [ $? -eq 0 ]; then
+if [[ $? -eq 0 ]]; then
   echo "Removed the downloaded ZIP file"
 else
   echo "Removal failed. Check the file or permissions."

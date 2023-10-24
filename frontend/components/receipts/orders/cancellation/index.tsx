f5ba3a401235ae3cedb157e31e1be3c27ec02fee
sam-keen <samuel.kleinmann@gmail.com>
2023-10-24 13:19:14 +0100

    feat(418): order columns built for order store data

--- frontend/components/receipts/orders/cancellation/index.tsx
+++ frontend/components/receipts/orders/cancellation/index.tsx
@@ -1,7 +1,10 @@
+import { useState, useEffect } from 'react'
 import { ReceiptComponentProps } from '../../receipts'
 import { OrderTable } from '../../utils/order-table'
 import { ReceiptWrapper } from '../../utils/receipt-wrapper'
 import { Intent, Notification } from '@vegaprotocol/ui-toolkit'
+import { useOrdersStore } from '../../../../stores/orders-store.ts'
+import { useJsonRpcClient } from '../../../../contexts/json-rpc/json-rpc-context.ts'
 
 export const CancellationNotification = ({ orderId, marketId }: { orderId: string; marketId: string }) => {
   if (orderId) return null
@@ -19,9 +22,34 @@ export const CancellationNotification = ({ orderId, marketId }: { orderId: strin
 
 export const CancellationView = ({ cancellation }: { cancellation: any }) => {
   const { orderId, marketId } = cancellation
+  const [orderDetails, setOrderDetails] = useState<{ order: any; lastUpdated: number | null }>({
+    order: null,
+    lastUpdated: null
+  })
+
+  const getOrderById = useOrdersStore((state) => state.getOrderById)
+  const { request } = useJsonRpcClient()
+
+  useEffect(() => {
+    if (orderId) {
+      getOrderById(orderId, request)
+        .then((result) => {
+          setOrderDetails({ order: result.order, lastUpdated: result.lastUpdated })
+        })
+        .catch((err) => {
+          console.log('Failed to fetch order details:', err)
+        })
+    }
+  }, [orderId, getOrderById, request])
+
   return (
     <>
-      <OrderTable {...cancellation} />
+      <OrderTable {...{ ...cancellation, ...orderDetails.order }} />
+      {orderDetails.lastUpdated && (
+        <div className="text-sm text-gray-500 mt-2">
+          Last Updated: {new Date(orderDetails.lastUpdated).toLocaleString()}
+        </div>
+      )}
       <CancellationNotification orderId={orderId} marketId={marketId} />
     </>
   )

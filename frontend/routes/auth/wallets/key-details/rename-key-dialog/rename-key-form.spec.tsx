10ad9e449f6635bb8b43b54d977743afd815319d
Dexter <dexter@vega.xyz>
2023-10-24 09:34:28 +0800

    docs: add ac coverage for form behaviour

--- frontend/routes/auth/wallets/key-details/rename-key-dialog/rename-key-form.spec.tsx
+++ frontend/routes/auth/wallets/key-details/rename-key-dialog/rename-key-form.spec.tsx
@@ -15,6 +15,7 @@ describe('RenameKeyForm', () => {
     jest.clearAllMocks()
   })
   it('renders the form with input and submit button', () => {
+    // 1125-KEYD-011 I can input the new name
     render(<RenameKeyForm onComplete={jest.fn()} keyName={'keyName'} publicKey="publicKey" />)
 
     const inputElement = screen.getByTestId(locators.renameKeyInput)
@@ -25,6 +26,7 @@ describe('RenameKeyForm', () => {
   })
 
   it('disables submit button when the form is empty', async () => {
+    // 1125-KEYD-013 Submit button is disabled when key name is not entered
     render(<RenameKeyForm onComplete={jest.fn()} keyName={'keyName'} publicKey="publicKey" />)
 
     const submitButton = screen.getByTestId(locators.renameKeySubmit)
@@ -33,6 +35,8 @@ describe('RenameKeyForm', () => {
   })
 
   it('displays an error message when input is too long', async () => {
+    // 1125-KEYD-014 Submit button is disabled when key name is longer than 30 chars
+    // 1125-KEYD-015 I see an error message when I enter a key name longer than 30 chars
     render(<RenameKeyForm onComplete={jest.fn()} keyName={'keyName'} publicKey="publicKey" />)
 
     const inputElement = screen.getByTestId(locators.renameKeyInput)
@@ -45,6 +49,8 @@ describe('RenameKeyForm', () => {
     await screen.findByTestId('input-error-text')
 
     expect(screen.getByTestId('input-error-text')).toHaveTextContent('Key name cannot be more than 30 character long')
+
+    expect(screen.getByTestId(locators.renameKeySubmit)).toBeDisabled()
   })
 
   it('calls the renameKey function with the new key name when submitted', async () => {
@@ -63,6 +69,7 @@ describe('RenameKeyForm', () => {
     })
   })
   it('calls onComplete when key is renamed', async () => {
+    // 1125-KEYD-012 When I press submit the dialog closes
     const mockNewKeyName = 'NewKeyName'
     const onComplete = jest.fn()
     render(<RenameKeyForm onComplete={onComplete} keyName={'keyName'} publicKey="publicKey" />)

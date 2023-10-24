43a4eb9d7541598c488d5766033a60fb3c578af6
Dexter <dexter@vega.xyz>
2023-10-24 11:29:20 +0800

    test: ensure jest tests can handle module remapping

--- test/test-config/jest.config-base-config.js
+++ test/test-config/jest.config-base-config.js
@@ -1,25 +1,26 @@
-const baseConfig =  {
-    rootDir: '../..',
-    transform: {
-      '^.+\\.tsx?$': 'ts-jest',
-      '^.+\\.jsx?$': 'ts-jest',
-    },
-    testEnvironment: 'jest-environment-node',
-    testTimeout: 30000,
-    reporters: [
-      'default',
-      [
-        'jest-junit',
-        {
-          outputDirectory: './test/test-reports',
-          outputName: 'override-me.xml',
-        },
-      ],
-    ],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    moduleNameMapper: {
-      '!/config': '<rootDir>/config/test.js',
-    },
-  };
+const baseConfig = {
+  rootDir: '../..',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'ts-jest'
+  },
+  testEnvironment: 'jest-environment-node',
+  testTimeout: 30000,
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: './test/test-reports',
+        outputName: 'override-me.xml'
+      }
+    ]
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '!/config': '<rootDir>/config/test.js',
+    '!/(.*)': '<rootDir>/frontend/$1'
+  }
+}
 
-export default baseConfig;
+export default baseConfig

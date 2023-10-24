81e679c26d3e50def730a6c2e081e46166c4feff
Dexter <dexter@vega.xyz>
2023-10-24 10:00:22 +0800

    feat: show notification when there are errors loading data

--- frontend/components/receipts/utils/receipt-wrapper.tsx
+++ frontend/components/receipts/utils/receipt-wrapper.tsx
@@ -1,15 +1,38 @@
 import { ReactNode } from 'react'
 import { VegaSection } from '../../vega-section'
+import { Intent, Notification } from '@vegaprotocol/ui-toolkit'
+import { useAssetsStore } from '../../../stores/assets-store'
+import { useMarketsStore } from '../../../stores/markets-store'
 
 export const locators = {
   receiptWrapper: 'receipt-wrapper'
 }
 
 // TODO handle data loading here, with render prop and loading/error states
-export const ReceiptWrapper = ({ children }: { children: ReactNode }) => {
+export const ReceiptWrapper = ({ children, errors = [] }: { children: ReactNode; errors?: Error[] }) => {
+  const { error: assetsError } = useAssetsStore((state) => ({
+    error: state.error
+  }))
+  const { error: marketsError } = useMarketsStore((state) => ({
+    error: state.error
+  }))
+  const allErrors = [assetsError, marketsError, ...errors].filter(Boolean) as Error[]
   return (
     <VegaSection>
       <section data-testid={locators.receiptWrapper}>{children}</section>
+      <div className="mt-4">
+        <Notification
+          intent={Intent.Warning}
+          title="Error loading data"
+          message="Additional data to display your transaction could not be loaded. The transaction can still be sent, but only transaction data can be shown."
+          buttonProps={{
+            action: () => {
+              navigator.clipboard.writeText(allErrors.map((e) => e.stack).join('. \n'))
+            },
+            text: 'Copy error message(s)'
+          }}
+        />
+      </div>
     </VegaSection>
   )
 }

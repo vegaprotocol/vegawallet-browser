7b475ef58ea8160423fdaa5a55a52ac840bad4c8
Dexter <dexter@vega.xyz>
2023-10-25 18:28:27 +0800

    test: add test for receipt error boundary

---
+++ frontend/components/receipts/receipt-error-boundary/receipt-error-boundray.spec.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect } from 'react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { ReceiptViewErrorBoundary } from './receipt-error-boundary'
+import locators from '../../locators'
+import { silenceErrors } from '../../../test-helpers/silence-errors'
+
+// Mock the Sentry captureException function
+jest.mock('@sentry/browser', () => ({
+  captureException: jest.fn()
+}))
+
+const BrokenComponent = ({ error }: { error: Error }) => {
+  useEffect(() => {
+    throw error
+  }, [error])
+  return <div data-testid="successful-render" />
+}
+
+describe('ReceiptViewErrorBoundary', () => {
+  it('renders children when there is no error', () => {
+    render(
+      <ReceiptViewErrorBoundary>
+        <div data-testid="child-content">Child content</div>
+      </ReceiptViewErrorBoundary>
+    )
+
+    const childContent = screen.getByTestId('child-content')
+    expect(childContent).toBeInTheDocument()
+  })
+
+  it('renders an error message when an error occurs', () => {
+    silenceErrors()
+    const error = new Error('Test error message')
+
+    render(
+      <ReceiptViewErrorBoundary>
+        <BrokenComponent error={error} />
+      </ReceiptViewErrorBoundary>
+    )
+    // Verify that the error message is displayed
+    const errorMessage = screen.getByText(
+      'An unexpected error occurred when rendering transaction receipt view. Please check your transaction has a valid format and data.'
+    )
+    expect(errorMessage).toBeInTheDocument()
+
+    expect(screen.getByTestId(locators.collapsiblePanelTitle)).toHaveTextContent('Error details')
+
+    fireEvent.click(screen.getByTestId(locators.collapsiblePanelButton))
+    const errorStack = screen.getByTestId(locators.collapsiblePanelContent)
+    expect(errorStack).toHaveTextContent(error.message)
+  })
+})

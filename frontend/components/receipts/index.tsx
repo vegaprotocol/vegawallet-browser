a07fff455304bd35a48a95e2268d12a6c13f07f2
Dexter <dexter@vega.xyz>
2023-10-25 15:38:42 +0800

    feat: add receipt view error boundary

--- frontend/components/receipts/index.tsx
+++ frontend/components/receipts/index.tsx
@@ -1,10 +1,16 @@
 import { TransactionKeys } from '../../lib/transactions'
+import { ReceiptViewErrorBoundary } from './error-boundary'
 import { ReceiptComponentProps } from './receipts'
 import { TransactionMap } from './transaction-map'
 
 export const TransactionSwitch = ({ transaction }: ReceiptComponentProps) => {
   const type = Object.keys(transaction)[0] as TransactionKeys
   const Component = TransactionMap[type]
-  if (Component) return <Component transaction={transaction} />
+  if (Component)
+    return (
+      <ReceiptViewErrorBoundary>
+        <Component transaction={transaction} />
+      </ReceiptViewErrorBoundary>
+    )
   return null
 }

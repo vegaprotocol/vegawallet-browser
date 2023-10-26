06ae9ebd8d9ba90f16ecd23a46c04e8067516c68
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-10-26 15:10:59 +0100

    test: resillience tests-key details with node/endpoint outages

---
+++ test/jest-config/jest.config-resillience.js
@@ -0,0 +1,21 @@
+import jestConfigE2E from "./jest.config-e2e.js";
+import cloneDeep from 'lodash/cloneDeep.js'
+import merge from 'lodash/merge.js'
+
+const jestUpgradeTests = cloneDeep(jestConfigE2E)
+
+let overrides = {
+roots: ['<rootDir>/test/resillience-tests'],
+setupFilesAfterEnv: [],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputName: `resillience.xml`
+      }
+    ]
+  ]
+}
+
+export default merge(jestUpgradeTests, overrides)

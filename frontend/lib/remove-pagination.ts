dfb514f198ea2c7ed5dd08c25ec06d129d30c736
Dexter <dexter@vega.xyz>
2023-10-26 08:09:43 +0800

    test: add coverage for remove pagination component

--- frontend/lib/remove-pagination.ts
+++ frontend/lib/remove-pagination.ts
@@ -1,5 +1,5 @@
 import compact from 'lodash/compact'
 
-export function removePaginationWrapper<T>(edges: Array<{ node: T } | null> | undefined | null): T[] {
+export function removePaginationWrapper<T>(edges: Array<{ node: T } | null | undefined> | undefined | null): T[] {
   return compact(edges?.map((edge) => edge?.node))
 }

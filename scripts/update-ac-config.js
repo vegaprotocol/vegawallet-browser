0bb8a03b66f6f345e959bdaa3c31a6b514e30387
Dexter <dexter@vega.xyz>
2023-11-02 16:19:28 +0000

    chore: add script to update AC config

---
+++ scripts/update-ac-config.js
@@ -0,0 +1,29 @@
+import fs from 'node:fs/promises'
+import path from 'node:path'
+
+const AC_PATH = path.resolve('.', './specs')
+const APPS_PATH = path.resolve('.', './specs/apps.json')
+const CATEGORIES_PATH = path.resolve('.', './specs/categories.json')
+
+const folderContents = await fs.readdir(AC_PATH)
+const files = folderContents.filter(async (file) => await (await fs.lstat(path.resolve(AC_PATH, file))).isFile())
+const acFiles = files.filter((file) => file.endsWith('.md') && file.startsWith('11'))
+const codes = acFiles.map((file) => {
+  const [number, code] = file.split('-')
+  return `${number}-${code}`
+})
+
+const apps = {
+  'Browser-Wallet': {
+    specs: codes
+  }
+}
+
+const categories = {
+  'Browser Wallet': {
+    specs: codes
+  }
+}
+
+await fs.writeFile(APPS_PATH, JSON.stringify(apps, null, 2))
+await fs.writeFile(CATEGORIES_PATH, JSON.stringify(categories, null, 2))

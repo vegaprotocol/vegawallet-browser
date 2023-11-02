552a6637295fd22d5bbf7a218af6534af4e0eff6
Dexter <dexter@vega.xyz>
2023-11-02 11:37:55 +0000

    fix: incorrect import, conflict issues

--- frontend/components/receipts/utils/order-table.tsx
+++ frontend/components/receipts/utils/order-table.tsx
@@ -3,7 +3,7 @@ import { formatNumber, toBigNum } from '@vegaprotocol/utils'
 import { vegaOrderType, vegaSide, vegaOrderStatus } from '@vegaprotocol/rest-clients/dist/trading-data'
 import { DataTable } from '../../data-table/data-table'
 import { PeggedOrderOptions } from '../../../types/transactions'
-import { useMarketsStore, getSettlementAssetId } from '../../../stores/markets-store'
+import { useMarketsStore } from '../../../stores/markets-store'
 import { useAssetsStore } from '../../../stores/assets-store'
 import {
   buildPriceColumn,
@@ -20,7 +20,7 @@ import {
   buildStatusColumn,
   buildVersionColumn
 } from './order/build-order-columns'
-import { getSettlementAssetId } from '../../../lib/markets.ts'
+import { getSettlementAssetId } from '../../../lib/markets'
 
 export const OrderTable = ({
   marketId,

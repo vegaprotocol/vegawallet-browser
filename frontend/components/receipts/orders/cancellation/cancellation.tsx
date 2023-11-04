e089ca2dff6f965733773320a875c1f5ea3eed88
Dexter <dexter@vega.xyz>
2023-11-04 08:11:16 +0000

    docs: cover final ACs for cancellation

--- frontend/components/receipts/orders/cancellation/cancellation.tsx
+++ frontend/components/receipts/orders/cancellation/cancellation.tsx
@@ -5,12 +5,12 @@ import { useJsonRpcClient } from '../../../../contexts/json-rpc/json-rpc-context
 import { ReceiptWrapper } from '../../utils/receipt-wrapper'
 import { CancellationView } from './cancellation-view'
 
-// TODO pass error to receipt view after error handling PR is merged
 export const Cancellation = ({ transaction }: ReceiptComponentProps) => {
-  const { getOrderById, lastUpdated, order } = useOrdersStore((state) => ({
+  const { error, getOrderById, lastUpdated, order } = useOrdersStore((state) => ({
     getOrderById: state.getOrderById,
     lastUpdated: state.lastUpdated,
-    order: state.order
+    order: state.order,
+    error: state.error
   }))
   const cancellation = transaction.orderCancellation
   const { orderId } = cancellation
@@ -23,7 +23,7 @@ export const Cancellation = ({ transaction }: ReceiptComponentProps) => {
   }, [orderId, getOrderById, request])
 
   return (
-    <ReceiptWrapper>
+    <ReceiptWrapper errors={[error]}>
       <CancellationView cancellation={cancellation} order={order} />
       {order && lastUpdated && (
         <div className="text-sm text-gray-500 mt-2">Last Updated: {new Date(lastUpdated).toLocaleString()}</div>

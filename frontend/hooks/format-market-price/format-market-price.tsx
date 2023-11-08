32992755ab5beda05bcaf089669f97add215024f
Dexter <dexter@vega.xyz>
2023-11-08 13:58:33 +0000

    chore: create hooks for formatting of prices, size and amount

---
+++ frontend/hooks/format-market-price/format-market-price.tsx
@@ -0,0 +1,15 @@
+import get from 'lodash/get'
+import { useMarketsStore } from '../../stores/markets-store'
+import { formatNumber, toBigNum } from '@vegaprotocol/utils'
+
+export const useMarketPrice = (marketId: string, price: string) => {
+  const { loading, getMarketById } = useMarketsStore((state) => ({
+    loading: state.loading,
+    getMarketById: state.getMarketById
+  }))
+  if (loading) return null
+  const market = getMarketById(marketId)
+  const marketDecimal = Number(get(market, 'marketDecimals'))
+  if (!market || !marketDecimal) throw new Error('Could not find market or positionDecimals on')
+  return formatNumber(toBigNum(price, marketDecimal), marketDecimal)
+}

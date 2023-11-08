43cdcc2d5bd44b15d6b867be511b0ec0d19b7973
Dexter <dexter@vega.xyz>
2023-11-08 13:58:33 +0000

    chore: create hooks for formatting of prices, size and amount

--- frontend/hooks/format-asset-amount/format-asset-amount.tsx
+++ frontend/hooks/format-asset-amount/format-asset-amount.tsx
@@ -3,9 +3,11 @@ import { useAssetsStore } from '../../stores/assets-store'
 import get from 'lodash/get'
 
 export const useFormatAssetAmount = (assetId: string, amount: string) => {
-  const { getAssetById } = useAssetsStore((state) => ({
-    getAssetById: state.getAssetById
+  const { getAssetById, loading } = useAssetsStore((state) => ({
+    getAssetById: state.getAssetById,
+    loading: state.loading
   }))
+  if (loading) return null
   const assetInfo = getAssetById(assetId)
   const decimals = Number(get(assetInfo, 'details.decimals'))
   const symbol = get(assetInfo, 'details.symbol')
---
+++ frontend/hooks/format-size-amount/format-size-amount.tsx
@@ -0,0 +1,17 @@
+import get from 'lodash/get'
+import { useMarketsStore } from '../../stores/markets-store'
+import { formatNumber, toBigNum } from '@vegaprotocol/utils'
+import { MAX_POSITION_SIZE } from '../../lib/transactions'
+
+export const useFormatSizeAmount = (marketId: string, size: string) => {
+  const { loading, getMarketById } = useMarketsStore((state) => ({
+    loading: state.loading,
+    getMarketById: state.getMarketById
+  }))
+  if (loading) return null
+  if (size === MAX_POSITION_SIZE) return 'Max'
+  const market = getMarketById(marketId)
+  const positionDecimals = Number(get(market, 'positionDecimalPlaces'))
+  if (!market || !positionDecimals) throw new Error('Could not find market or positionDecimals on')
+  return formatNumber(toBigNum(size, positionDecimals), positionDecimals)
+}

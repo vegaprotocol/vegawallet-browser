fd3f2c89083907bda250f0d79bf980731a978caf
Dexter <dexter@vega.xyz>
2023-11-08 15:42:17 +0000

    test: add tests for new hooks

--- frontend/hooks/format-asset-amount/format-asset-amount.spec.tsx
+++ frontend/hooks/format-asset-amount/format-asset-amount.spec.tsx
@@ -36,6 +36,21 @@ describe('formatAssetAmount', () => {
       'Could not find amount, decimals or symbol when trying to render transaction for asset foo'
     )
   })
+
+  it('returns null while loading', () => {
+    mockStore(useAssetsStore, {
+      loading: true,
+      getAssetById: () => ({
+        details: {
+          symbol: 'SYMBOL',
+          decimals: undefined
+        }
+      })
+    })
+    const { result } = renderHook(() => useFormatAssetAmount('foo', '123'))
+    expect(result.current).toBe(null)
+  })
+
   it('returns formatted amount and symbol', () => {
     mockStore(useAssetsStore, {
       getAssetById: () => ({
@@ -46,7 +61,7 @@ describe('formatAssetAmount', () => {
       })
     })
     const { result } = renderHook(() => useFormatAssetAmount('foo', '123'))
-    expect(result.current.formattedAmount).toBe('0.000000000123')
-    expect(result.current.symbol).toBe('SYMBOL')
+    expect(result.current?.formattedAmount).toBe('0.000000000123')
+    expect(result.current?.symbol).toBe('SYMBOL')
   })
 })

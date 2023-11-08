7397bc49585caaa24cfc0fa489573b1b0d431fce
Dexter <dexter@vega.xyz>
2023-11-08 10:45:16 +0000

    feat: add close position title behaviour

--- frontend/components/modals/transaction-modal/get-title.ts
+++ frontend/components/modals/transaction-modal/get-title.ts
@@ -2,24 +2,24 @@ import { TransactionKeys, TRANSACTION_TITLES, Transaction } from '../../../lib/t
 
 export const U_INT_64_MAX = '18446744073709551615'
 
-export const getTitle = (transaction: Transaction) => {
-  const type = Object.keys(transaction)[0] as TransactionKeys
-
+const processTitle = (type: TransactionKeys, data: any) => {
   // If we have a mass order cancellation (i.e. no order ID, with or without a market ID)
   // then we want to display a different title
-  if (
-    type === TransactionKeys.ORDER_CANCELLATION &&
-    transaction.orderCancellation &&
-    !transaction.orderCancellation.orderId
-  ) {
+  if (type === TransactionKeys.ORDER_CANCELLATION && data && data.orderId) {
     return 'Mass Order Cancellation'
-  } else if (
-    type === TransactionKeys.ORDER_SUBMISSION &&
-    transaction.orderSubmission.reduceOnly &&
-    transaction.orderSubmission.size === U_INT_64_MAX
-  ) {
-    return 'Close position'
+  } else if (type === TransactionKeys.ORDER_SUBMISSION && data && data.reduceOnly && data.size === U_INT_64_MAX) {
+    return 'Close Position'
   }
 
   return TRANSACTION_TITLES[type]
+}
+
+export const getBatchTitle = (type: TransactionKeys, data: any) => {
+  return processTitle(type, data)
+}
+
+export const getTitle = (transaction: Transaction) => {
+  const type = Object.keys(transaction)[0] as TransactionKeys
+
+  return processTitle(type, transaction[type])
 }

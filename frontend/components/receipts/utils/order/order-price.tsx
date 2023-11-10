8898db5c3cd017e31247c8827c667f8b945b6ab7
Dexter <dexter@vega.xyz>
2023-11-10 16:42:00 +0000

    chore: make order price handle its' own loading states

--- frontend/components/receipts/utils/order/order-price.tsx
+++ frontend/components/receipts/utils/order/order-price.tsx
@@ -1,32 +1,33 @@
 import { vegaOrderType } from '@vegaprotocol/rest-clients/dist/trading-data'
 import { PriceWithTooltip } from '../string-amounts/price-with-tooltip.tsx'
 import { AmountWithSymbol } from '../string-amounts/amount-with-symbol.tsx'
+import { useFormatMarketPrice } from '../../../../hooks/format-market-price/format-market-price.tsx'
+import { useMarketSettlementAsset } from '../../../../hooks/market-settlement-asset/market-settlement-asset.tsx'
+import get from 'lodash/get'
 
 export const locators = {
   orderDetailsMarketPrice: 'order-details-market-price'
 }
 
 export const OrderPriceComponent = ({
-  assetsLoading,
   price,
   marketId,
-  formattedPrice,
-  symbol,
   type
 }: {
-  assetsLoading: boolean
-  price: string | undefined
-  marketId: string | undefined
-  formattedPrice: string | undefined
-  symbol: string | undefined
+  price: string
+  marketId: string
   type: vegaOrderType | undefined
 }) => {
-  if (type === vegaOrderType.TYPE_MARKET) return <div data-testid="order-details-market-price">Market price</div>
+  const asset = useMarketSettlementAsset(marketId)
+  const symbol = get(asset, 'symbol')
+  const formattedPrice = useFormatMarketPrice(marketId, price)
+  if (type === vegaOrderType.TYPE_MARKET)
+    return <div data-testid="order-details-market-price">Market price</div>
 
   if (!price || !marketId) return null
 
-  if (assetsLoading || !formattedPrice || !symbol) {
-    return <PriceWithTooltip key="order-details-price" marketId={marketId} price={price} />
+  if (!formattedPrice || !symbol) {
+    return <PriceWithTooltip marketId={marketId} price={price} />
   }
 
   return <AmountWithSymbol amount={formattedPrice} symbol={symbol} />

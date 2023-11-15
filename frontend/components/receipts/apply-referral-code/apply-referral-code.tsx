793d4064fadceebe3e9fcbbd3b801e2ba99af3ec
Dexter <dexter@vega.xyz>
2023-11-15 10:48:01 +0000

    test: add tests for apply referral code

--- frontend/components/receipts/apply-referral-code/apply-referral-code.tsx
+++ frontend/components/receipts/apply-referral-code/apply-referral-code.tsx
@@ -1,11 +1,18 @@
 import { ReceiptComponentProps } from '../receipts'
 import { ReceiptWrapper } from '../utils/receipt-wrapper'
 
+export const locators = {
+  referralCodeTitle: 'referral-code-title',
+  referralCode: 'referral-code'
+}
+
 export const ApplyReferralCode = ({ transaction }: ReceiptComponentProps) => {
   return (
     <ReceiptWrapper>
-      <h1 className="text-vega-dark-300">Code</h1>
-      <p>{transaction.referralCode.id}</p>
+      <h1 className="text-vega-dark-300" data-testid={locators.referralCodeTitle}>
+        Code
+      </h1>
+      <p data-testid={locators.referralCode}>{transaction.applyReferralCode.id}</p>
     </ReceiptWrapper>
   )
 }

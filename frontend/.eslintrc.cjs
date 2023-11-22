ecbcbade343bc44a2fe68d6ed49965e38fad2647
Dexter <dexter@vega.xyz>
2023-11-22 13:14:36 +0000

    chore: make eslint js file for comments, add a11y rules

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  extends: ['react-app', 'react-app/jest'],
+  rules: {
+    'import/no-webpack-loader-syntax': 'off', // We do not use webpack
+    'no-restricted-imports': [
+      'warn',
+      {
+        paths: [
+          {
+            name: '@vegaprotocol/types',
+            message: 'Please use @vegaprotocol/rest-clients instead'
+          }, // These types come from GQL but are a hard dep of UI toolkit. The rest client types are accurate.
+          {
+            name: 'lodash',
+            message:
+              "Import the specific methods you need from lodash e.g. `import get from 'lodash/get'`. This helps with bundle sizing."
+          }
+        ]
+      }
+    ]
+  },
+  overrides: [
+    // Jest files
+    {
+      extends: ['plugin:jest/style'],
+      files: ['**/*.spec.ts', '**/*.spec.tsx'],
+      plugins: ['jest'],
+      rules: {
+        'jest/consistent-test-it': 'warn'
+      }
+    },
+    // JSX files
+    {
+      files: ['*.ts', '*.tsx', '*.js', '*.jsx'],
+      extends: ['plugin:jsx-a11y/strict'],
+      rules: {
+        'jsx-a11y/no-autofocus': 'off'
+      }
+    }
+  ]
+}

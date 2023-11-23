c4e5fdc1ffd4b4e6670af05ff444fb1bb104c8d7
Dexter <dexter@vega.xyz>
2023-11-23 10:40:01 +0000

    docs: add AC for hiding settings on mobile

--- frontend/routes/auth/settings/settings.spec.tsx
+++ frontend/routes/auth/settings/settings.spec.tsx
@@ -4,7 +4,10 @@ import { Settings } from '.'
 import { mockClient } from '../../../test-helpers/mock-client'
 import { locators } from '../../../../frontend/routes/auth/settings'
 import { MemoryRouter } from 'react-router-dom'
+import { mockStore } from '../../../test-helpers/mock-store'
+import { useGlobalsStore } from '../../../stores/globals'
 
+jest.mock('../../../stores/globals')
 jest.mock('./version-section', () => ({
   VersionSection: () => <div data-testid="version-section" />
 }))
@@ -14,7 +17,6 @@ jest.mock('./settings-form-elements/radio', () => ({
 jest.mock('./lock-section', () => ({
   LockSection: () => <div data-testid="lock-section" />
 }))
-
 jest.mock('!/config', () => ({
   ...jest.requireActual('../../../../config/test').default,
   closeWindowOnPopupOpen: true
@@ -31,6 +33,7 @@ const renderComponent = () =>
 
 describe('Settings', () => {
   it('renders settings page', () => {
+    mockStore(useGlobalsStore, { isMobile: false })
     // 1107-SETT-007 I can see the version # of the browser extension
     // 1107-SETT-008 I can see the feedback link
     mockClient()
@@ -38,6 +41,18 @@ describe('Settings', () => {
     expect(screen.getByTestId('version-section')).toBeVisible()
     expect(screen.getByTestId(locators.settingsPage)).toBeVisible()
     expect(screen.getAllByTestId('radio')).toHaveLength(2)
+    expect(screen.getByTestId('lock-section')).toBeVisible()
+  })
+
+  it('does not render pop out settings on mobile', () => {
+    // 1132-ANDR-002 If I am on mobile on settings I do not see the auto popout setting
+    mockStore(useGlobalsStore, { isMobile: true })
+
+    mockClient()
+    renderComponent()
+    expect(screen.getByTestId('version-section')).toBeVisible()
+    expect(screen.getByTestId(locators.settingsPage)).toBeVisible()
+    expect(screen.getAllByTestId('radio')).toHaveLength(1)
     expect(screen.getByTestId('lock-section')).toBeVisible()
   })
 })
--- specs/1132-ANDR-android_firefox.md
+++ specs/1132-ANDR-android_firefox.md
@@ -1,3 +1,4 @@
 # Android firefox
 
 - If I rotate the device, I see a warning telling me to rotate it back to portrait mode (<a name="1132-ANDR-001" href="#1132-ANDR-001">1132-ANDR-001</a>)
+- If I am on mobile on [settings](./1107-SETT-settings.md) I do not see the auto popout setting (<a name="1132-ANDR-002" href="#1132-ANDR-002">1132-ANDR-002</a>)

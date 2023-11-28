9e6c0bcf474fe099fd5611f0cc16f1f97f5f1790
Dexter <dexter@vega.xyz>
2023-11-28 12:19:23 +0000

    test: add test for missing code branc

--- frontend/routes/auth/wallets/key-details/assets-list/asset-list-empty-state.spec.tsx
+++ frontend/routes/auth/wallets/key-details/assets-list/asset-list-empty-state.spec.tsx
@@ -5,6 +5,7 @@ import { useAssetsStore } from '../../../../../stores/assets-store'
 import { useMarketsStore } from '../../../../../stores/markets-store'
 import { locators as subheaderLocators } from '../../../../../components/sub-header'
 import { locators as assetCardLocators } from './asset-card'
+import { MarketTradingMode } from '@vegaprotocol/rest-clients/dist/trading-data'
 
 // Mocking useAssetsStore and useMarketsStore
 jest.mock('../../../../../stores/assets-store')
@@ -93,5 +94,51 @@ describe('AssetListEmptyState Component', () => {
     expect(screen.getByTestId(subheaderLocators.subHeader)).toHaveTextContent('Balances')
     const assetCards = screen.queryAllByTestId(assetCardLocators.assetCard)
     expect(assetCards).toHaveLength(0)
+  })
+
+  it('should order by the total number of active markets', () => {
+    const assets = [
+      { id: '1', details: { decimals: 1, symbol: 'A1', name: 'Asset 1' } },
+      { id: '2', details: { decimals: 1, symbol: 'A2', name: 'Asset 2' } }
+    ]
+    mockStore(useAssetsStore, {
+      // TODO figure out why deep partial is not functional for arrays
+      // @ts-ignore
+      assets,
+      getAssetById: (assetId: string) => assets.find(({ id }) => id === assetId)
+    })
+    mockStore(useMarketsStore, {
+      getMarketsByAssetId: (id: string) => {
+        return id === assets[0].id
+          ? [
+              {
+                id: '1',
+                tradingMode: MarketTradingMode.TRADING_MODE_NO_TRADING
+              },
+              {
+                id: '2',
+                tradingMode: MarketTradingMode.TRADING_MODE_SUSPENDED_VIA_GOVERNANCE
+              }
+            ]
+          : [
+              {
+                id: '3',
+                tradingMode: MarketTradingMode.TRADING_MODE_CONTINUOUS
+              },
+              {
+                id: '4',
+                tradingMode: MarketTradingMode.TRADING_MODE_MONITORING_AUCTION
+              }
+            ]
+      }
+    })
+    render(<AssetListEmptyState publicKey="testPublicKey" />)
+
+    expect(screen.getByTestId(subheaderLocators.subHeader)).toHaveTextContent('Balances')
+    const assetCards = screen.queryAllByTestId(assetCardLocators.assetCard)
+    expect(assetCards).toHaveLength(2)
+    const [asset1, asset2] = assetCards
+    expect(asset1).toHaveTextContent('Asset 1')
+    expect(asset2).toHaveTextContent('Asset 2')
   })
 })

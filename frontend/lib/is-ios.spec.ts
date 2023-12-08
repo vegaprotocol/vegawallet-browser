2fbcf3a04f86a1cdf3236c587d72cccdcb4a0db7
Dexter <dexter@vega.xyz>
2023-12-08 16:18:59 +0000

    test: add tests

--- frontend/lib/is-ios.spec.ts
+++ frontend/lib/is-ios.spec.ts
@@ -2,6 +2,17 @@ import { isIos } from './is-ios'
 
 const navigator = window.navigator
 
+const mockNavigator = (userAgent?: string, platform?: string, maxTouchPoints?: number) => {
+  Object.defineProperty(window, 'navigator', {
+    writable: true,
+    value: {
+      userAgent,
+      platform,
+      maxTouchPoints
+    }
+  })
+}
+
 describe('isIos', () => {
   afterEach(() => {
     Object.defineProperty(window, 'navigator', {
@@ -11,50 +22,28 @@ describe('isIos', () => {
   })
 
   it('returns true when userAgent contains iPad', () => {
-    Object.defineProperty(window, 'navigator', {
-      writable: true,
-      value: {
-        userAgent: 'iPad'
-      }
-    })
-
+    mockNavigator('iPad')
     const result = isIos()
 
     expect(result).toBe(true)
   })
 
   it('returns true when userAgent contains iPhone', () => {
-    Object.defineProperty(window, 'navigator', {
-      writable: true,
-      value: {
-        userAgent: 'iPhone'
-      }
-    })
+    mockNavigator('iPhone')
     const result = isIos()
 
     expect(result).toBe(true)
   })
 
   it('returns true when userAgent contains iPod', () => {
-    Object.defineProperty(window, 'navigator', {
-      writable: true,
-      value: {
-        userAgent: 'iPod'
-      }
-    })
+    mockNavigator('iPod')
     const result = isIos()
 
     expect(result).toBe(true)
   })
 
   it('returns true when platform is MacIntel and maxTouchPoints > 1', () => {
-    Object.defineProperty(window, 'navigator', {
-      writable: true,
-      value: {
-        platform: 'MacIntel',
-        maxTouchPoints: 2
-      }
-    })
+    mockNavigator(undefined, 'MacIntel', 2)
 
     const result = isIos()
 
@@ -62,14 +51,7 @@ describe('isIos', () => {
   })
 
   it('returns false when userAgent and platform conditions are not met', () => {
-    Object.defineProperty(window, 'navigator', {
-      writable: true,
-      value: {
-        userAgent: 'Some non-iOS user agent',
-        platform: 'Some non-MacIntel platform'
-      }
-    })
-
+    mockNavigator('Android', 'Android')
     const result = isIos()
 
     expect(result).toBe(false)

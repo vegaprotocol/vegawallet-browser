2bd37bfa34b54a147f1d95469192de0056e0577f
Dexter <dexter@vega.xyz>
2023-12-15 15:50:06 +0000

    chore: correct locator name

--- frontend/components/vega-entities/node-link.spec.tsx
+++ frontend/components/vega-entities/node-link.spec.tsx
@@ -8,14 +8,14 @@ describe('NodeLink', () => {
   it('renders truncated id and link', () => {
     const id = '1'.repeat(64)
     render(<NodeLink nodeId={id} />)
-    expect(screen.getByTestId(locators.proposalLink)).toHaveTextContent('111111…1111')
-    expect(screen.getByTestId(locators.proposalLink)).toHaveAttribute(
+    expect(screen.getByTestId(locators.nodeLink)).toHaveTextContent('111111…1111')
+    expect(screen.getByTestId(locators.nodeLink)).toHaveAttribute(
       'href',
       `${config.network.governance}/validators/${id}`
     )
   })
   it('renders name if passed in', () => {
     render(<NodeLink nodeId={'1'.repeat(64)} name="foo" />)
-    expect(screen.getByTestId(locators.proposalLink)).toHaveTextContent('foo')
+    expect(screen.getByTestId(locators.nodeLink)).toHaveTextContent('foo')
   })
 })

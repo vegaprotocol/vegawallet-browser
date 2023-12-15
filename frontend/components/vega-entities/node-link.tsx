679968fb6e4d968fdc55b5fca1683f0100524ac1
Dexter <dexter@vega.xyz>
2023-12-15 15:50:06 +0000

    chore: correct locator name

--- frontend/components/vega-entities/node-link.tsx
+++ frontend/components/vega-entities/node-link.tsx
@@ -4,11 +4,11 @@ import config from '!/config'
 import { ExternalLink } from '@/components/external-link'
 
 export const locators = {
-  proposalLink: 'proposal-link'
+  nodeLink: 'node-link'
 }
 
 export const NodeLink = ({ nodeId, name }: { nodeId: string; name?: string }) => (
-  <ExternalLink data-testid={locators.proposalLink} href={`${config.network.governance}/validators/${nodeId}`}>
+  <ExternalLink data-testid={locators.nodeLink} href={`${config.network.governance}/validators/${nodeId}`}>
     {name ?? truncateMiddle(nodeId)}
   </ExternalLink>
 )

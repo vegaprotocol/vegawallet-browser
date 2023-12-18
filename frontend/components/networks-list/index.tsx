bc9872e0866c52df45a51471a5d464c28da3020a
Dexter <dexter@vega.xyz>
2023-12-18 16:18:54 +0000

    fix: call onClick with selected network on change

--- frontend/components/networks-list/index.tsx
+++ frontend/components/networks-list/index.tsx
@@ -6,10 +6,10 @@ import { SubHeader } from '../sub-header'
 
 export interface NetworkListProperties {
   networks: Network[]
-  onClick?: () => void
+  onClick?: (network: Network) => void
 }
 
-export const NetworksList = ({ networks }: NetworkListProperties) => {
+export const NetworksList = ({ networks, onClick }: NetworkListProperties) => {
   return (
     <section className="text-base mt-6">
       <SubHeader content="Networks" />
@@ -17,10 +17,13 @@ export const NetworksList = ({ networks }: NetworkListProperties) => {
         className="mt-2"
         idProp="name"
         items={networks}
-        renderItem={(k) => (
+        renderItem={(n) => (
           <div className="flex justify-between h-12">
-            <div className="flex items-center">{k.name}</div>
-            <button className="hover:bg-vega-dark-200 w-12 h-full border-l border-1 border-vega-dark-150 flex items-center justify-center">
+            <div className="flex items-center">{n.name}</div>
+            <button
+              onClick={() => onClick?.(n)}
+              className="hover:bg-vega-dark-200 w-12 h-full border-l border-1 border-vega-dark-150 flex items-center justify-center"
+            >
               <ChevronRight />
             </button>
           </div>

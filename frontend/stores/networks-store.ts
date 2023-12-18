93a16c3f1beb6a82782b9667eda78a22a734a1b0
Dexter <dexter@vega.xyz>
2023-12-18 12:54:39 +0000

    chore: add networks store

---
+++ frontend/stores/networks-store.ts
@@ -0,0 +1,32 @@
+import { create } from 'zustand'
+
+import { SendMessage } from '@/contexts/json-rpc/json-rpc-provider.tsx'
+import { RpcMethods } from '@/lib/client-rpc-methods.ts'
+
+interface Network {
+  name: string
+  rest: string[]
+  explorer: string
+  // TODO needed?
+  probing: boolean
+  preferredNode: string | null
+}
+
+interface NetworksResponse {
+  networks: Network[]
+}
+
+export type NetworksStore = {
+  networks: Network[]
+  loading: boolean
+  loadNetworks: (request: SendMessage<NetworksResponse>) => Promise<void>
+}
+
+export const useNetworksStore = create<NetworksStore>((set, get) => ({
+  networks: [],
+  loading: true,
+  async loadNetworks(request) {
+    const { networks } = await request(RpcMethods.ListNetworks)
+    set({ networks })
+  }
+}))

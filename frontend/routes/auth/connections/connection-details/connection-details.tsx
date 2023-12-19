e25d77dc9bb3ed352d8c43138de4288b966536cd
Dexter <dexter@vega.xyz>
2023-12-19 20:39:10 +0000

    feat: begin building out connection details page

--- frontend/routes/auth/connections/connection-details/connection-details.tsx
+++ frontend/routes/auth/connections/connection-details/connection-details.tsx
@@ -1,3 +1,38 @@
+import { getDateFormat, getTimeFormat } from '@vegaprotocol/utils'
+
+import { BasePage } from '@/components/pages/page'
+import { SubHeader } from '@/components/sub-header'
+import { VegaSection } from '@/components/vega-section'
+import { FULL_ROUTES } from '@/routes/route-names'
+
+export const locators = {
+  connectionDetails: 'connection-details'
+}
+
 export const ConnectionDetails = () => {
-  return <>This is the connection details page</>
+  const connection = {
+    origin: 'foo.com',
+    accessedAt: 0,
+    network: 'some-network'
+  }
+  return (
+    <BasePage
+      backLocation={FULL_ROUTES.networksSettings}
+      dataTestId={locators.connectionDetails}
+      title={connection.origin}
+    >
+      <VegaSection>
+        <SubHeader content="Last connected" />
+        <div className="text-white mt-1">
+          Last connected: {getDateFormat().format(connection.accessedAt)} ·{' '}
+          {getTimeFormat().format(connection.accessedAt)}
+        </div>
+      </VegaSection>
+      <VegaSection>
+        <SubHeader content="Network" />
+        {/* TODO this should be a link to the network details page */}
+        <div className="text-white mt-1">{connection.network}</div>
+      </VegaSection>
+    </BasePage>
+  )
 }

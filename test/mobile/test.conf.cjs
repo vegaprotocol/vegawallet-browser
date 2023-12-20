6c2a4486fb9106c7c9e8914461dc6e81b24d43cc
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-12-20 16:17:55 +0000

    test: ugly code but we are now installing and enabling the extensio through automation

--- test/mobile/test.conf.cjs
+++ test/mobile/test.conf.cjs
@@ -1,49 +1,64 @@
 exports.config = {
-    user: process.env.BROWSERSTACK_USERNAME || 'dalebennett_yHuwF2',
-    key: process.env.BROWSERSTACK_ACCESS_KEY || '5CwMxYSUKoMUJyLLNpFF',
-    hostname: 'hub.browserstack.com',
-    services: [
-      [
-        'browserstack',
-        { 
-          //app: 'bs://a25c4dc3e4c2ae24d67f8b47646044f281bbac23',
-          buildIdentifier: '${BUILD_NUMBER}',
-          //browserstackLocal: true,
-           //opts: { forcelocal: false, localIdentifier: "thisIsMyIdentifier" },
-          //gagagahahahah: process.env.BROWSERSTACK_APP_PATH || './VegaWallet-Fairground.ipa',
-          //browserName: 'safari',
-        }
-      ]
-    ],
-  
-    capabilities: [{
-      'appium:otherApps': ['bs://a25c4dc3e4c2ae24d67f8b47646044f281bbac23'],
-      'appium:includeSafariInWebviews': true,
-      "browserName": "safari",
-        'bstack:options': {
-          deviceName: 'iPhone 15',
-          platformVersion: '17',
-          platformName: 'ios',
-          //otherApps: ['bs://a25c4dc3e4c2ae24d67f8b47646044f281bbac23']
-        }}],
-  
-    updateJob: false,
-    specs: [
-      './tests/**/*.spec.js'
-    ],
-    exclude: [],
-  
-    logLevel: 'info',
-    coloredLogs: true,
-    screenshotPath: './errorShots/',
-    baseUrl: '',
-    waitforTimeout: 10000,
-    connectionRetryTimeout: 90000,
-    connectionRetryCount: 3,
-  
-    framework: 'mocha',
-    mochaOpts: {
-      ui: 'bdd',
-      timeout: 30000
+  user: process.env.BROWSERSTACK_USERNAME || 'dalebennett_yHuwF2',
+  key: process.env.BROWSERSTACK_ACCESS_KEY || '5CwMxYSUKoMUJyLLNpFF',
+  hostname: 'hub.browserstack.com',
+  services: [
+    [
+      'browserstack',
+      {
+        buildIdentifier: '${BUILD_NUMBER}',
+        browserstackLocal: true,
+        opts: { forcelocal: false, localIdentifier: "VegaWallet" },
+        app: "bs://a25c4dc3e4c2ae24d67f8b47646044f281bbac23"
+      }
+    ]
+  ],
+
+  capabilities: [{
+    'bstack:options': {
+      deviceName: "iPhone 15",
+      osVersion: "17"
     }
-  };
+  }, ],
+
+  commonCapabilities: {
+    
+    'bstack:options': {
+      projectName: "BrowserStack Samples",
+      buildName: 'browserstack build',
+      sessionName: 'BStack parallel webdriverio-appium',
+      debug: true,
+      networkLogs: true,
+      browserName: 'safari',
+    }
+  },
+
+  maxInstances: 10,
+
+  updateJob: false,
+  specs: [
+    './tests/**/*.spec.js'
+  ],
+  exclude: [],
+
+
+  logLevel: 'info',
+  coloredLogs: true,
+  screenshotPath: './errorShots/',
+  baseUrl: '',
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 90000,
+  connectionRetryCount: 3,
+
+  framework: 'mocha',
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: 40000
+  }
+};
+
+// Code to support common capabilities
+exports.config.capabilities.forEach(function(caps){
+  for(let key in exports.config.commonCapabilities)
+    caps[key] = { ...caps[key], ...exports.config.commonCapabilities[key]};
+});

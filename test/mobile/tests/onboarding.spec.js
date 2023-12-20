423f2c9bdd14acfd4f051a7b6cfc181475bf96bf
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-12-20 16:17:55 +0000

    test: ugly code but we are now installing and enabling the extensio through automation

--- test/mobile/tests/onboarding.spec.js
+++ test/mobile/tests/onboarding.spec.js
@@ -1,10 +1,31 @@
-describe('Safari URL Entry Test', () => {
-  
-  it('should enter a URL into Safari', async () => {
-      console.log("Before waiting");
-      await new Promise(resolve => setTimeout(resolve, 5000)); // Pause for 5000 milliseconds (5 seconds)
-      var searchSelector = await $(`//XCUIElementTypeTextField[@name="TabBarItemTitle"]`)
-      await searchSelector.waitForDisplayed({ timeout: 10000 });
-      await searchSelector.click();
+
+async function clickAcceptButton() {
+  const acceptButton = await $(`//form[@name="consent"]/div[2]/button[1]`);
+  await acceptButton.click();
+}
+
+describe('Safari test', () => {
+  it('should do something in safari', async () => {
+    console.log("Before waiting");
+    await browser.url('https://google.co.uk');
+    
+    await new Promise(resolve => setTimeout(resolve, 5000)); // Pause for 5000 milliseconds (5 seconds)
+    
+    // Utilize the custom function to click the Accept button
+    // await clickAcceptButton();
+    const contexts = await driver.getContexts();
+    console.log('Available Contexts:', contexts);
+    await driver.switchContext(contexts[0]);
+    var button = await $('~PageFormatMenuButton')
+    await button.click();
+    var manageExtensionsButton = await $('~ManageExtensions')
+    await manageExtensionsButton.click();
+
+    var vegaExtensionToggle = await $('//XCUIElementTypeSwitch[@name="Vega Wallet - Fairground"]');
+    await vegaExtensionToggle.click();
+
+    var doneButton = await $('~Done');
+    await doneButton.click();
+
   });
 });

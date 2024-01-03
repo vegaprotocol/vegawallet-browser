d3b5ae43f462137ac401fa6ecdfd0b154314f42c
Dexter <dexter@vega.xyz>
2024-01-03 07:28:56 +0000

    fix: export validation required for isConnected

--- web-extension/validation/client/index.js
+++ web-extension/validation/client/index.js
@@ -3,3 +3,4 @@ export { default as disconnectWallet } from "./disconnect-wallet.js";
 export { default as getChainId } from "./get-chain-id.js";
 export { default as listKeys } from "./list-keys.js";
 export { default as sendTransaction } from "./send-transaction.js";
+export { default as isConnected } from "./is-connected.js";

3ec7671854a9e655a0ad9c76fc2eadcb3acda5ac
Dexter <dexter@vega.xyz>
2024-01-09 13:30:49 +0100

    test: add tests for new connection modal behaviour

--- frontend/components/modals/connection-modal/connection-modal.spec.tsx
+++ frontend/components/modals/connection-modal/connection-modal.spec.tsx
@@ -1,13 +1,18 @@
 import { fireEvent, render, screen } from '@testing-library/react'
 
+import { useErrorStore } from '@/stores/error'
 import { useInteractionStore } from '@/stores/interaction-store'
+import { useNetworksStore } from '@/stores/networks-store'
 import { mockStore } from '@/test-helpers/mock-store'
 
+import { testingNetwork } from '../../../../config/well-known-networks'
 import locators from '../../locators'
 import { ConnectionModal } from './connection-modal'
 import { ConnectionSuccessProperties } from './connection-success'
 
 jest.mock('@/stores/interaction-store')
+jest.mock('@/stores/networks-store')
+jest.mock('@/stores/error')
 
 jest.mock('./connection-success', () => ({
   ConnectionSuccess: ({ onClose }: ConnectionSuccessProperties) => (
@@ -20,45 +25,93 @@ describe('ConnectionModal', () => {
     jest.clearAllMocks()
   })
   it('renders nothing when isOpen is false', () => {
+    mockStore(useErrorStore, {})
+    mockStore(useNetworksStore, {
+      networks: [testingNetwork]
+    })
     mockStore(useInteractionStore, { connectionModalOpen: false })
     const { container } = render(<ConnectionModal />)
     expect(container).toBeEmptyDOMElement()
   })
   it('renders connection details when open but not yet connected', () => {
+    mockStore(useErrorStore, {})
+    mockStore(useNetworksStore, {
+      networks: [testingNetwork]
+    })
     mockStore(useInteractionStore, { connectionModalOpen: true, currentConnectionDetails: {} })
     render(<ConnectionModal />)
     expect(screen.getByTestId(locators.connectionModalApprove)).toBeInTheDocument()
   })
   it('renders connection success when hasConnected is true', () => {
+    mockStore(useErrorStore, {})
+    mockStore(useNetworksStore, {
+      networks: [testingNetwork]
+    })
     mockStore(useInteractionStore, {
       connectionModalOpen: true,
       handleConnectionDecision: jest.fn(),
-      currentConnectionDetails: {}
+      currentConnectionDetails: {
+        chainId: testingNetwork.chainId
+      }
     })
     render(<ConnectionModal />)
     fireEvent.click(screen.getByTestId(locators.connectionModalApproveButton))
     expect(screen.getByTestId('connection-success')).toBeInTheDocument()
   })
   it('renders nothing if connection is not approved', () => {
+    mockStore(useErrorStore, {})
+    mockStore(useNetworksStore, {
+      networks: [testingNetwork]
+    })
     mockStore(useInteractionStore, {
       connectionModalOpen: true,
       handleConnectionDecision: jest.fn(),
-      currentConnectionDetails: {}
+      currentConnectionDetails: {
+        chainId: testingNetwork.chainId
+      }
     })
     render(<ConnectionModal />)
     fireEvent.click(screen.getByTestId(locators.connectionModalDenyButton))
     expect(screen.queryByTestId('connection-success')).not.toBeInTheDocument()
   })
   it('handle the interaction decision when connection is approve after showing success state', () => {
+    mockStore(useErrorStore, {})
+    mockStore(useNetworksStore, {
+      networks: [testingNetwork]
+    })
     const handleConnectionDecision = jest.fn()
     mockStore(useInteractionStore, {
       connectionModalOpen: true,
       handleConnectionDecision,
-      currentConnectionDetails: {}
+      currentConnectionDetails: {
+        chainId: testingNetwork.chainId
+      }
     })
     render(<ConnectionModal />)
     fireEvent.click(screen.getByTestId(locators.connectionModalApproveButton))
     fireEvent.click(screen.getByTestId('connection-success'))
-    expect(handleConnectionDecision).toHaveBeenCalledWith({ approved: true })
+    expect(handleConnectionDecision).toHaveBeenCalledWith({ approved: true, networkId: 'test' })
+  })
+  it('sets a global error if chainId could not be found', () => {
+    mockStore(useNetworksStore, {
+      networks: [testingNetwork]
+    })
+    const handleConnectionDecision = jest.fn()
+    mockStore(useInteractionStore, {
+      connectionModalOpen: true,
+      handleConnectionDecision,
+      currentConnectionDetails: {
+        chainId: 'foo'
+      }
+    })
+    const setError = jest.fn()
+    mockStore(useErrorStore, {
+      setError
+    })
+    render(<ConnectionModal />)
+    fireEvent.click(screen.getByTestId(locators.connectionModalApproveButton))
+    fireEvent.click(screen.getByTestId('connection-success'))
+    expect(setError).toHaveBeenCalledWith(new Error('Network could not be found with chainId foo'))
+    expect(handleConnectionDecision).not.toHaveBeenCalled()
   })
 })

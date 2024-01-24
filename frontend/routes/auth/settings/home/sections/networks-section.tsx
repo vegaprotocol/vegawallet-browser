d832747ff26bb2c011d9f7ab1d0f8d77b35eba8f
Dexter <dexter@vega.xyz>
2024-01-24 13:05:26 +0100

    chore: add tests for networks section

--- frontend/routes/auth/settings/home/sections/networks-section.tsx
+++ frontend/routes/auth/settings/home/sections/networks-section.tsx
@@ -6,6 +6,7 @@ import { VegaSection } from '@/components/vega-section'
 import { FULL_ROUTES } from '@/routes/route-names'
 
 export const locators = {
+  viewNetworksButton: 'view-networks-button',
   viewNetworks: 'view-networks'
 }
 
@@ -14,10 +15,12 @@ export const NetworksSection = () => {
     <VegaSection>
       <SubHeader content="Networks" />
       <div className="flex justify-between h-12">
-        <div className="flex items-center">View configured networks</div>
+        <div data-testid={locators.viewNetworks} className="flex items-center">
+          View configured networks
+        </div>
         <NavLink
           to={{ pathname: FULL_ROUTES.networksSettings }}
-          data-testid={locators.viewNetworks}
+          data-testid={locators.viewNetworksButton}
           className="hover:bg-vega-dark-200 w-12 h-full border-l border-1 border-vega-dark-150 flex items-center justify-center"
         >
           <ChevronRight />

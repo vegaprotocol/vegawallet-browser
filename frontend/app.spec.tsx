58763115ca715801774d10082c62a8d4a2994001
Dexter <dexter@vega.xyz>
2024-01-24 14:22:31 +0100

    chore: more consistency tidying

--- frontend/app.spec.tsx
+++ frontend/app.spec.tsx
@@ -27,9 +27,11 @@ jest.mock('@/stores/popover-store', () => ({
   }
 }))
 
-jest.mock('@/components/global-error-boundary', () => ({ children }: { children: ReactNode }) => (
-  <div data-testid="global-error-boundary">{children}</div>
-))
+jest.mock('@/components/global-error-boundary', () => ({
+  GlobalErrorBoundary: ({ children }: { children: ReactNode }) => (
+    <div data-testid="global-error-boundary">{children}</div>
+  )
+}))
 
 jest.mock('@/contexts/json-rpc/json-rpc-provider', () => ({
   JsonRPCProvider: ({ children }: { children: ReactNode }) => <div data-testid="json-rpc-provider">{children}</div>

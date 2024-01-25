1c197122e6e2713f1f139018556a6072066c1a22
Dexter <dexter@vega.xyz>
2024-01-25 11:37:08 +0100

    chore: add script to update validation based on message

---
+++ scripts/update-transaction-validation.js
@@ -0,0 +1,46 @@
+import messages from '@vegaprotocol/protos/messages.json' assert { type: 'json' }
+import fs from 'node:fs'
+
+const EXCLUDED_FIELDS = ['nonce', 'blockHeight']
+
+const inputs = messages.packages.vega.packages.commands.packages.v1.messages.find(({ name }) => name === 'InputData')
+console.log(inputs)
+const allowedMessages = inputs.fields
+  .filter(({ name }) => !EXCLUDED_FIELDS.includes(name))
+  .map((b) => ({
+    additionalProperties: false,
+    required: [b.name],
+    properties: {
+      [b.name]: {}
+    }
+  }))
+
+const sendTransactionValidation = {
+  type: 'object',
+  required: ['publicKey', 'transaction', 'sendingMode'],
+  additionalProperties: false,
+  errorMessage: '`client.send_transaction` must only be given `publicKey`, `transaction` and `sendingMode`',
+  properties: {
+    publicKey: {
+      type: 'string',
+      pattern: '^[0-9a-z]{64}$',
+      errorMessage: '`publicKey` must be a 64 character hex encoded publicKey'
+    },
+    sendingMode: {
+      enum: ['TYPE_ASYNC', 1, 'TYPE_SYNC', 2, 'TYPE_COMMIT', 3],
+      errorMessage:
+        'Only `TYPE_ASYNC` (1), `TYPE_SYNC` (2) and `TYPE_COMMIT` (3) are valid sending modes. The sendingMode must be the string name or numerical enum'
+    },
+    transaction: {
+      type: 'object',
+      errorMessage: 'Unsupported transaction type',
+      oneOf: allowedMessages
+    }
+  }
+}
+
+fs.writeFileSync(
+  'web-extension/schemas/client/send-transaction.json',
+  JSON.stringify(sendTransactionValidation, null, 2) + '\n',
+  { encoding: 'utf-8' }
+)

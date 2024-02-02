862887b810e70879bd579c90c92d736428b3e742
Dexter <dexter@vega.xyz>
2024-02-02 18:30:50 +0100

    chore: create party link entity

---
+++ frontend/components/vega-entities/party-link.tsx
@@ -0,0 +1,21 @@
+import { ExternalLink, truncateMiddle } from '@vegaprotocol/ui-toolkit'
+
+import { useNetwork } from '@/contexts/network/network-context'
+
+export const locators = {
+  partyLink: 'party-link'
+}
+
+export const PartyLink = ({ publicKey }: { publicKey: string }) => {
+  const { network } = useNetwork()
+
+  return (
+    <ExternalLink
+      className="text-vega-dark-400"
+      data-testid={locators.partyLink}
+      href={`${network.explorer}/parties/${publicKey}`}
+    >
+      {truncateMiddle(publicKey)}
+    </ExternalLink>
+  )
+}

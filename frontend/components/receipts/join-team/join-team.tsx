7afd2accf820b52d809b17bc9c6481e8f717591b
Dexter <dexter@vega.xyz>
2024-02-05 12:46:37 +0000

    chore: add id to join team

--- frontend/components/receipts/join-team/join-team.tsx
+++ frontend/components/receipts/join-team/join-team.tsx
@@ -1,4 +1,5 @@
 import { DataTable } from '@/components/data-table/data-table'
+import { TeamLink } from '@/components/vega-entities/team-link'
 import { VegaTeam } from '@/components/vega-entities/vega-team'
 
 import { ReceiptComponentProperties } from '../receipts'
@@ -7,7 +8,12 @@ import { ReceiptWrapper } from '../utils/receipt-wrapper'
 export const JoinTeam = ({ transaction }: ReceiptComponentProperties) => {
   return (
     <ReceiptWrapper>
-      <DataTable items={[['Team', <VegaTeam key="join-team-id" id={transaction.joinTeam.id} />]]} />
+      <DataTable
+        items={[
+          ['Team', <VegaTeam key="join-team-id" id={transaction.joinTeam.id} />],
+          ['Team Id', <TeamLink key="join-team-id" id={transaction.joinTeam.id} />]
+        ]}
+      />
     </ReceiptWrapper>
   )
 }

cc95888cb1f14dc78e9f2a92d6508c57082647db
Dexter <dexter@vega.xyz>
2024-02-05 12:00:39 +0000

    test: add tests for new beahviour

--- frontend/components/receipts/join-team/join-team.spec.tsx
+++ frontend/components/receipts/join-team/join-team.spec.tsx
@@ -2,14 +2,14 @@ import { render, screen } from '@testing-library/react'
 
 import { MockNetworkProvider } from '@/contexts/network/mock-network-provider'
 
-import { testingNetwork } from '../../../../config/well-known-networks'
-import { locators } from '../../vega-entities/team-link'
 import { JoinTeam } from './join-team'
+
+jest.mock('@/components/vega-entities/vega-team', () => ({
+  VegaTeam: () => <div data-testid="vega-team" />
+}))
 
 describe('JoinTeam', () => {
   it('returns a link to the team', () => {
-    // 1141-JNTM-001 I can see the id of the team I want to join
-    // 1141-JNTM-002 I can see a link to the team's page on console
     render(
       <MockNetworkProvider>
         <JoinTeam
@@ -21,10 +21,6 @@ describe('JoinTeam', () => {
         />
       </MockNetworkProvider>
     )
-    expect(screen.getByTestId(locators.teamLink)).toHaveTextContent('000000…0000')
-    expect(screen.getByTestId(locators.teamLink)).toHaveAttribute(
-      'href',
-      `${testingNetwork.console}/#/competitions/teams/${'0'.repeat(64)}`
-    )
+    expect(screen.getByTestId('vega-team')).toBeInTheDocument()
   })
 })
--- frontend/components/vega-entities/team-link.spec.tsx
+++ frontend/components/vega-entities/team-link.spec.tsx
@@ -7,6 +7,8 @@ import { locators, TeamLink } from './team-link'
 
 describe('TeamLink', () => {
   it('returns a link to the team', () => {
+    // 1141-JNTM-001 I can see the id of the team I want to join
+    // 1141-JNTM-002 I can see a link to the team's page on console
     render(
       <MockNetworkProvider>
         <TeamLink id={'0'.repeat(64)} />
@@ -16,5 +18,18 @@ describe('TeamLink', () => {
       'href',
       `${testingNetwork.console}/#/competitions/teams/${'0'.repeat(64)}`
     )
+    expect(screen.getByTestId(locators.teamLink)).toHaveTextContent('000000…0000')
+  })
+  it('renders name if passed in', () => {
+    render(
+      <MockNetworkProvider>
+        <TeamLink id={'0'.repeat(64)}>Test</TeamLink>
+      </MockNetworkProvider>
+    )
+    expect(screen.getByTestId(locators.teamLink)).toHaveAttribute(
+      'href',
+      `${testingNetwork.console}/#/competitions/teams/${'0'.repeat(64)}`
+    )
+    expect(screen.getByTestId(locators.teamLink)).toHaveTextContent('Test')
   })
 })

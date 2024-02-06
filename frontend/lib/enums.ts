3877d3069918d041ceaef835da19d21fc94a4b55
Dexter <dexter@vega.xyz>
2024-02-06 16:00:41 +0000

    feat: add util to decode enums

---
+++ frontend/lib/enums.ts
@@ -0,0 +1,42 @@
+import {
+  AccountType,
+  AccountTypeNumber,
+  MarginMode,
+  MarginModeNumber,
+  OrderStatus,
+  OrderStatusNumber,
+  OrderType,
+  OrderTypeNumber,
+  PeggedReference,
+  PeggedReferenceNumber,
+  Side,
+  SideNumber,
+  StopOrderExpiryStrategy,
+  StopOrderExpiryStrategyNumber,
+  UndelegateSubmissionMethod,
+  UndelegateSubmissionMethodNumber
+} from '@vegaprotocol/enums'
+
+function getEnumString<T, S>(map: T, numberMap: S) {
+  return (index: keyof T | number) => {
+    if (!Number.isNaN(Number(index))) {
+      for (const key in numberMap) {
+        if (numberMap[key] === index) {
+          // They keys here overlap completely between number and string enums
+          return map[key as unknown as keyof T]
+        }
+      }
+    }
+    // They keys here overlap completely between number and string enums
+    return map[index as unknown as keyof T]
+  }
+}
+
+export const processMarginMode = getEnumString(MarginMode, MarginModeNumber)
+export const processExpiryStrategy = getEnumString(StopOrderExpiryStrategy, StopOrderExpiryStrategyNumber)
+export const processUndelegateMethod = getEnumString(UndelegateSubmissionMethod, UndelegateSubmissionMethodNumber)
+export const processOrderStatus = getEnumString(OrderStatus, OrderStatusNumber)
+export const processOrderType = getEnumString(OrderType, OrderTypeNumber)
+export const processPeggedReference = getEnumString(PeggedReference, PeggedReferenceNumber)
+export const processSide = getEnumString(Side, SideNumber)
+export const processAccountType = getEnumString(AccountType, AccountTypeNumber)

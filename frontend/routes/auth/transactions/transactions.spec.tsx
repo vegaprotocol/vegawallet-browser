3fd03cbb81ee0be47e3eea58960addb58c53fe9a
Dexter <dexter@vega.xyz>
2024-02-07 15:24:48 +0000

    test: fix test

--- frontend/routes/auth/transactions/transactions.spec.tsx
+++ frontend/routes/auth/transactions/transactions.spec.tsx
@@ -2,8 +2,12 @@ import { render, screen } from '@testing-library/react'
 
 import { locators as partyLinkLocators } from '@/components/vega-entities/party-link'
 import { MockNetworkProvider } from '@/contexts/network/mock-network-provider'
+import { useWalletStore } from '@/stores/wallets'
+import { mockStore } from '@/test-helpers/mock-store'
 
 import { locators, Transactions } from './transactions'
+
+jest.mock('@/stores/wallets')
 
 const renderComponent = () => {
   render(
@@ -15,6 +19,16 @@ const renderComponent = () => {
 
 describe('Transactions', () => {
   it('renders title, description and links to the parties', () => {
+    mockStore(useWalletStore, {
+      wallets: [
+        {
+          keys: [
+            { publicKey: 'key1', name: 'Key 1' },
+            { publicKey: 'key2', name: 'Key 2' }
+          ]
+        }
+      ]
+    })
     renderComponent()
     expect(screen.getByTestId(locators.transactions)).toBeVisible()
     expect(screen.getByTestId(locators.transactionsDescription)).toHaveTextContent(

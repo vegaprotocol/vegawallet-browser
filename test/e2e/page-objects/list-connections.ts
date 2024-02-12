cbb8e0b3a4dcbcf46b3b5f38e0cad6f197fe4e9c
Dexter <dexter@vega.xyz>
2024-02-12 12:47:59 +0000

    test: fix remove connection

--- test/e2e/page-objects/list-connections.ts
+++ test/e2e/page-objects/list-connections.ts
@@ -1,6 +1,7 @@
 import { By, WebDriver } from 'selenium-webdriver'
 import {
   clickDescendantOfWebElement,
+  clickElement,
   getByDataTestID,
   getElements,
   getWebElementContainingText,
@@ -10,6 +11,7 @@ import {
 import { locators as noConnectionsLocators } from '../../../frontend/routes/auth/connections/home/no-dapps-connected'
 import { locators as connectionsLocators } from '../../../frontend/routes/auth/connections/home'
 import { locators as connectionsListLocators } from '../../../frontend/routes/auth/connections/home/connection-list'
+import { locators as connectionDetailsLocator } from '../../../frontend/routes/auth/connections/details/details'
 
 export class ListConnections {
   private readonly noConnections: By = getByDataTestID(noConnectionsLocators.connectionsNoConnections)
@@ -72,5 +74,6 @@ export class ListConnections {
   async disconnectConnection(connectionName: string) {
     const connection = await getWebElementContainingText(connectionName, this.driver, this.connections)
     await clickDescendantOfWebElement(this.driver, connection, this.connectionsRemoveConnection)
+    await clickElement(this.driver, getByDataTestID(connectionDetailsLocator.removeConnection))
   }
 }

7ed28779f24432fced76e188eed0095470dbce38
Dexter <dexter@vega.xyz>
2024-02-12 17:06:16 +0000

    chore: remove dead code branches, fix unit tests

--- frontend/components/navbar/navbar.tsx
+++ frontend/components/navbar/navbar.tsx
@@ -14,26 +14,13 @@ export interface NavButtonProperties {
   icon: ReactNode
   text: string
   to: To
-  isFairground: boolean
 }
 
-const TEXT_COLOR = {
-  fairground: {
-    active: 'text-black',
-    inactive: 'text-neutral-600'
-  },
-  default: {
-    active: 'text-white',
-    inactive: 'text-neutral-600'
-  }
-}
-
-export const NavButton = ({ icon, text, to, isFairground }: NavButtonProperties) => {
-  const textColors = isFairground ? TEXT_COLOR.fairground : TEXT_COLOR.default
+export const NavButton = ({ icon, text, to }: NavButtonProperties) => {
   return (
     <NavLink data-testid={locators.navButton} to={to} className="text-center">
       {({ isActive }) => {
-        const textColor = isActive ? textColors.active : textColors.inactive
+        const textColor = isActive ? 'text-white' : 'text-neutral-600'
 
         return (
           <div className="h-full grid gap-0 grid-rows-[1fr_auto_auto]">
@@ -42,8 +29,7 @@ export const NavButton = ({ icon, text, to, isFairground }: NavButtonProperties)
             <div
               data-testid={locators.linkActive}
               className={classnames('h-1 w-full mt-2', {
-                'bg-vega-yellow': isActive && !isFairground,
-                'bg-black': isActive && isFairground
+                'bg-vega-yellow': isActive
               })}
             />
           </div>
@@ -53,35 +39,28 @@ export const NavButton = ({ icon, text, to, isFairground }: NavButtonProperties)
   )
 }
 
-export const NavBar = ({ isFairground }: { isFairground: boolean }) => {
+export const NavBar = () => {
   return (
     <nav
       data-testid={locators.navBar}
-      className={classnames('z-[5] w-full h-20 grid gap-0 grid-cols-4 border-t border-vega-dark-200', {
-        'bg-black': !isFairground,
-        'bg-vega-yellow-500': isFairground
-      })}
+      className={'z-[5] w-full h-20 grid gap-0 grid-cols-4 border-t border-vega-dark-200 bg-black'}
     >
       <NavButton
-        isFairground={isFairground}
-        icon={<Wallet className="m-auto" squareFill={isFairground ? '#D7FB50' : 'black'} />}
+        icon={<Wallet className="m-auto" squareFill={'black'} />}
         to={{ pathname: FULL_ROUTES.wallets }}
         text="Wallets"
       />
       <NavButton
-        isFairground={isFairground}
         icon={<UpDownArrows className="m-auto" />}
         to={{ pathname: FULL_ROUTES.connections }}
         text="Connections"
       />
       <NavButton
-        isFairground={isFairground}
         icon={<LeftRightArrows className="m-auto" />}
         to={{ pathname: FULL_ROUTES.transactions }}
         text="Transactions"
       />
       <NavButton
-        isFairground={isFairground}
         icon={<Settings className="m-auto" />}
         to={{ pathname: FULL_ROUTES.settings }}
         text="Settings"

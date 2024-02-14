25f78e0e987b37517359c311345533e35d486973
Dexter <dexter@vega.xyz>
2024-02-14 05:20:27 +0000

    test: null or any

--- test/upgrade-tests/upgrade-extension.spec.ts
+++ test/upgrade-tests/upgrade-extension.spec.ts
@@ -14,8 +14,92 @@ import { chromePublicKey } from '../../rollup/postbuild'
 import { Settings } from '../e2e/page-objects/settings'
 import { Login } from '../e2e/page-objects/login'
 import { APIHelper } from '../e2e/helpers/wallet/wallet-api'
-import { fairground, mainnet } from '../../config/well-known-networks'
 import config from '../../config/testnet'
+
+expect.extend({
+  nullOrAny(received, expected) {
+    if (received === null) {
+      return {
+        pass: true,
+        message: () =>
+          `expected null or instance of ${this.utils.printExpected(expected)}, but received ${this.utils.printReceived(
+            received
+          )}`
+      }
+    }
+
+    if (expected == String) {
+      return {
+        pass: typeof received == 'string' || received instanceof String,
+        message: () =>
+          `expected null or instance of ${this.utils.printExpected(expected)}, but received ${this.utils.printReceived(
+            received
+          )}`
+      }
+    }
+
+    if (expected == Number) {
+      return {
+        pass: typeof received == 'number' || received instanceof Number,
+        message: () =>
+          `expected null or instance of ${this.utils.printExpected(expected)}, but received ${this.utils.printReceived(
+            received
+          )}`
+      }
+    }
+
+    if (expected == Function) {
+      return {
+        pass: typeof received == 'function' || received instanceof Function,
+        message: () =>
+          `expected null or instance of ${this.utils.printExpected(expected)}, but received ${this.utils.printReceived(
+            received
+          )}`
+      }
+    }
+
+    if (expected == Object) {
+      return {
+        pass: received !== null && typeof received == 'object',
+        message: () =>
+          `expected null or instance of ${this.utils.printExpected(expected)}, but received ${this.utils.printReceived(
+            received
+          )}`
+      }
+    }
+
+    if (expected == Boolean) {
+      return {
+        pass: typeof received == 'boolean',
+        message: () =>
+          `expected null or instance of ${this.utils.printExpected(expected)}, but received ${this.utils.printReceived(
+            received
+          )}`
+      }
+    }
+
+    /* jshint -W122 */
+    /* global Symbol */
+    if (typeof Symbol != 'undefined' && this.expectedObject == Symbol) {
+      return {
+        pass: typeof received == 'symbol',
+        message: () =>
+          `expected null or instance of ${this.utils.printExpected(expected)}, but received ${this.utils.printReceived(
+            received
+          )}`
+      }
+    }
+    /* jshint +W122 */
+
+    return {
+      pass: received instanceof expected,
+      message: () =>
+        `expected null or instance of ${this.utils.printExpected(expected)}, but received ${this.utils.printReceived(
+          received
+        )}`
+    }
+  }
+})
 
 describe('Check migration of settings after upgrade', () => {
   let driver: WebDriver
@@ -64,9 +148,12 @@ describe('Check migration of settings after upgrade', () => {
         .sort(({ id: id1 }, { id: id2 }) => id1.localeCompare(id2))
         .map((n) => ({
           ...n,
-          _nodeTimeout: expect.any(Number),
-          preferredNode: expect.any(String),
-          probing: expect.any(Boolean)
+          // @ts-ignore
+          _nodeTimeout: expect.nullOrAny(Number),
+          // @ts-ignore
+          preferredNode: expect.nullOrAny(String),
+          // @ts-ignore
+          probing: expect.nullOrAny(Boolean)
         }))
     )
   })

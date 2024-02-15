e02c50ca42b0749cdb2f56aa5c58f4ed35d61529
Dexter <dexter@vega.xyz>
2024-02-15 14:11:12 +0000

    chore: migrate referral set infromation

--- frontend/components/receipts/utils/referral-set-information.tsx
+++ frontend/components/receipts/utils/referral-set-information.tsx
@@ -1,42 +1,40 @@
-import { ReactNode } from 'react'
-
 import { CollapsiblePanel } from '@/components/collapsible-panel'
-import { DataTable } from '@/components/data-table/data-table'
+import { ConditionalDataTable, RowConfig } from '@/components/data-table/conditional-data-table'
 import { ExternalLink } from '@/components/external-link'
 import { PartyLink } from '@/components/vega-entities/party-link'
 import { TeamLink } from '@/components/vega-entities/team-link'
 import { VegaSection } from '@/components/vega-section'
 
 export const ReferralSetInformation = ({ referralSetData }: { referralSetData: any }) => {
-  const { isTeam, team, id } = referralSetData
-  const { name, teamUrl, avatarUrl, closed, allowList } = team || {}
-  const items = [
-    id == null ? null : ['Id', <TeamLink key="referral-set-information-id" id={id} />],
-    isTeam == null ? null : ['Team', isTeam ? 'Yes' : 'No'],
-    name == null ? name : ['Name', name],
-    teamUrl == null
-      ? null
-      : [
-          'Team URL',
-          <ExternalLink key="referral-set-information-team" href={teamUrl}>
-            {teamUrl}
-          </ExternalLink>
-        ],
-    avatarUrl == null
-      ? null
-      : [
-          'Avatar URL',
-          <ExternalLink key="referral-set-information-avatar" href={avatarUrl}>
-            {avatarUrl}
-          </ExternalLink>
-        ],
-    closed == null ? null : ['Closed', closed ? 'Yes' : 'No']
+  const allowList = referralSetData?.team?.allowList
+  const items: RowConfig<typeof referralSetData>[] = [
+    { prop: 'id', render: (data) => ['Id', <TeamLink key="referral-set-information-id" id={data.id} />] },
+    { prop: 'isTeam', render: (data) => ['Team', data.isTeam ? 'Yes' : 'No'] },
+    { prop: 'team.name', render: (data) => ['Name', data.team.name] },
+    {
+      prop: 'team.teamUrl',
+      render: (data) => [
+        'Team URL',
+        <ExternalLink key="referral-set-information-team" href={data.team.teamUrl}>
+          {data.team.teamUrl}
+        </ExternalLink>
+      ]
+    },
+    {
+      prop: 'team.avatarUrl',
+      render: (data) => [
+        'Avatar URL',
+        <ExternalLink key="referral-set-information-avatar" href={data.team.avatarUrl}>
+          {data.team.avatarUrl}
+        </ExternalLink>
+      ]
+    },
+    { prop: 'team.closed', render: (data) => ['Closed', data.team.closed ? 'Yes' : 'No'] }
   ]
-  const dataTableItems = items.filter((item) => !!item) as [ReactNode, ReactNode][]
   const allowedPublicKeys = allowList as string[]
   return (
     <>
-      <DataTable items={dataTableItems} />
+      <ConditionalDataTable items={items} data={referralSetData} />
       {allowedPublicKeys && (
         <VegaSection>
           <CollapsiblePanel

81958d2b97ffd8a82f29baa4ba216483b2612c89
Dexter <dexter@vega.xyz>
2024-02-15 14:00:12 +0000

    chore: migrate undelegate submission

--- frontend/components/receipts/undelegate-submission/undelegate-submission.tsx
+++ frontend/components/receipts/undelegate-submission/undelegate-submission.tsx
@@ -1,7 +1,6 @@
 import { formatNumber, toBigNum } from '@vegaprotocol/utils'
-import { ReactNode } from 'react'
 
-import { DataTable } from '@/components/data-table'
+import { ConditionalDataTable, RowConfig } from '@/components/data-table/conditional-data-table'
 import { processUndelegateMethod, UNDELEGATE_METHOD_MAP } from '@/lib/enums'
 
 import { NodeLink } from '../../vega-entities/node-link'
@@ -10,18 +9,25 @@ import { ReceiptWrapper } from '../utils/receipt-wrapper'
 import { AmountWithSymbol } from '../utils/string-amounts/amount-with-symbol'
 
 export const UndelegateSubmission = ({ transaction }: ReceiptComponentProperties) => {
-  const { nodeId, amount, method } = transaction.undelegateSubmission
+  const { amount } = transaction.undelegateSubmission
   // We know that VEGA always has 18 decimals
   const formattedAmount = formatNumber(toBigNum(amount, 18), 18)
   const undelegateMethod = processUndelegateMethod(method)
-  const items = [
-    ['Node Id', <NodeLink nodeId={nodeId} />],
-    ['Amount', <AmountWithSymbol amount={formattedAmount} symbol={'VEGA'} />],
-    ['Method', <>{UNDELEGATE_METHOD_MAP[undelegateMethod]}</>]
-  ] as [ReactNode, ReactNode][]
+
+  const items: RowConfig<typeof transaction.undelegateSubmission>[] = [
+    { prop: 'nodeId', render: (data) => ['Node Id', <NodeLink nodeId={data.nodeId} />] },
+    {
+      prop: 'amount',
+      render: () => ['Amount', <AmountWithSymbol amount={formattedAmount} symbol={'VEGA'} />]
+    },
+    {
+      prop: 'method',
+      render: (data) => ['Method', <>{UNDELEGATE_METHOD_MAP[processUndelegateMethod(data.method)]}</>]
+    }
+  ]
   return (
     <ReceiptWrapper>
-      <DataTable items={items} />
+      <ConditionalDataTable items={items} data={transaction.undelegateSubmission} />
     </ReceiptWrapper>
   )
 }

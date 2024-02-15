e765b009131b6e610bcd35db0fb82508d1889bdd
Dexter <dexter@vega.xyz>
2024-02-15 14:19:44 +0000

    chore: migrate delegate submission

--- frontend/components/receipts/delegate-submission/delegate-submission.tsx
+++ frontend/components/receipts/delegate-submission/delegate-submission.tsx
@@ -1,7 +1,6 @@
 import { formatNumber, toBigNum } from '@vegaprotocol/utils'
-import { ReactNode } from 'react'
 
-import { DataTable } from '@/components/data-table'
+import { ConditionalDataTable, RowConfig } from '@/components/data-table/conditional-data-table'
 
 import { NodeLink } from '../../vega-entities/node-link'
 import { ReceiptComponentProperties } from '../receipts'
@@ -9,17 +8,20 @@ import { ReceiptWrapper } from '../utils/receipt-wrapper'
 import { AmountWithSymbol } from '../utils/string-amounts/amount-with-symbol'
 
 export const DelegateSubmission = ({ transaction }: ReceiptComponentProperties) => {
-  const { nodeId, amount } = transaction.delegateSubmission
+  const { amount } = transaction.delegateSubmission
   // We know that VEGA always has 18 decimals
   const formattedAmount = formatNumber(toBigNum(amount, 18), 18)
 
-  const items = [
-    ['Node Id', <NodeLink nodeId={nodeId} />],
-    ['Amount', <AmountWithSymbol amount={formattedAmount} symbol={'VEGA'} />]
-  ] as [ReactNode, ReactNode][]
+  const items: RowConfig<typeof transaction.delegateSubmission>[] = [
+    { prop: 'nodeId', render: (data) => ['Node Id', <NodeLink nodeId={data.nodeId} />] },
+    {
+      prop: 'amount',
+      render: () => ['Amount', <AmountWithSymbol amount={formattedAmount} symbol={'VEGA'} />]
+    }
+  ]
   return (
     <ReceiptWrapper>
-      <DataTable items={items} />
+      <ConditionalDataTable items={items} data={transaction.delegateSubmission} />
     </ReceiptWrapper>
   )
 }

ada4d75c5ce4409a482e2357c0c1dd8db5485dc5
Dexter <dexter@vega.xyz>
2024-02-15 14:19:13 +0000

    chore: migrate stop order cancellations

--- frontend/components/receipts/orders/stop-cancellation/cancellation.tsx
+++ frontend/components/receipts/orders/stop-cancellation/cancellation.tsx
@@ -1,19 +1,23 @@
 import { truncateMiddle } from '@vegaprotocol/ui-toolkit'
-import { ReactNode } from 'react'
 
-import { DataTable } from '../../../data-table'
+import { ConditionalDataTable, RowConfig } from '@/components/data-table/conditional-data-table'
+
 import { MarketLink } from '../../../vega-entities/market-link'
 import { ReceiptComponentProperties } from '../../receipts'
 import { ReceiptWrapper } from '../../utils/receipt-wrapper'
 
 export const StopOrderCancellationView = ({ stopOrdersCancellation }: { stopOrdersCancellation: any }) => {
-  const { marketId, stopOrderId } = stopOrdersCancellation
-  const columns = [
-    marketId ? ['Market', <MarketLink key="order-details-market" marketId={marketId} />] : null,
-    stopOrderId ? ['Stop Order', truncateMiddle(stopOrderId)] : null
+  const items: RowConfig<typeof stopOrdersCancellation>[] = [
+    {
+      prop: 'marketId',
+      render: (data) => ['Market', <MarketLink key="order-details-market" marketId={data.marketId} />]
+    },
+    {
+      prop: 'stopOrderId',
+      render: (data) => ['Stop Order', truncateMiddle(data.stopOrderId)]
+    }
   ]
-  const data = columns.filter((c) => !!c) as [ReactNode, ReactNode][]
-  return <DataTable items={data} />
+  return <ConditionalDataTable items={items} data={stopOrdersCancellation} />
 }
 
 export const StopOrderCancellation = ({ transaction }: ReceiptComponentProperties) => {

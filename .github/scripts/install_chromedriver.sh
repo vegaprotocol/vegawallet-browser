4b086ae6fb8d93159774182d5c9f0ffeb73db727
Dexter <dexter@vega.xyz>
2024-02-19 14:26:40 +0000

    ci: correct variable name

--- .github/scripts/install_chromedriver.sh
+++ .github/scripts/install_chromedriver.sh
@@ -4,6 +4,6 @@ current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source "$current_dir/get_latest_stable_chrome.sh"
 
 chromedriver_filename="chromedriver-linux64"
-download_url="https://storage.googleapis.com/chrome-for-testing-public/$latest_stable_chrome/linux64/$chrome_filename.zip"
+download_url="https://storage.googleapis.com/chrome-for-testing-public/$latest_stable_chrome/linux64/$chromedriver_filename.zip"
 
 "$current_dir/download_and_unzip.sh" "$download_url" "$chromedriver_filename"

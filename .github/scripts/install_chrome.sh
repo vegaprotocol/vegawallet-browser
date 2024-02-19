926696628cafc95a0f7f26659b79721a732d07ae
Dexter <dexter@vega.xyz>
2024-02-19 14:15:49 +0000

    ci: fix download path

--- .github/scripts/install_chrome.sh
+++ .github/scripts/install_chrome.sh
@@ -4,6 +4,6 @@ current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source "$current_dir/get_latest_stable_chrome.sh"
 
 chrome_filename="chrome-linux64"
-download_url="https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$latest_stable_chrome/linux64/${chrome_filename}.zip"
+download_url="https://storage.googleapis.com/chrome-for-testing-public/$latest_stable_chrome/linux64/$chrome_filename.zip"
 
 "$current_dir/download_and_unzip.sh" "$download_url" "$chrome_filename"

a98e5fc19ff5755e0380ee71384b7c8a13ecdcf2
Dexter <dexter@vega.xyz>
2024-02-20 18:21:52 +0000

    test: add test for new data table functionality

--- frontend/components/data-table/conditional-data-table.spec.tsx
+++ frontend/components/data-table/conditional-data-table.spec.tsx
@@ -42,4 +42,36 @@ describe('ConditionalDataTable', () => {
     render(<ConditionalDataTable items={rows} data={{ foo: 'foo', bar: 'bar' }} />)
     expect(screen.getByText('bar')).toBeInTheDocument()
   })
+
+  it('should not render if props contains at least one undefined field', () => {
+    const rows: RowConfig<{ foo: string; bar?: string | null }>[] = [
+      {
+        prop: 'foo',
+        render: () => ['foo', <div></div>]
+      },
+      {
+        prop: 'foo',
+        props: ['foo', 'bar'],
+        render: () => ['bar', <div></div>]
+      }
+    ]
+    render(<ConditionalDataTable items={rows} data={{ foo: 'foo' }} />)
+    expect(screen.queryByText('bar')).not.toBeInTheDocument()
+  })
+
+  it('should render if all fields in props are defined', () => {
+    const rows: RowConfig<{ foo: string; bar: string | undefined | null }>[] = [
+      {
+        prop: 'foo',
+        render: () => ['foo', <div></div>]
+      },
+      {
+        prop: 'foo',
+        props: ['foo', 'bar'],
+        render: () => ['bar', <div></div>]
+      }
+    ]
+    render(<ConditionalDataTable items={rows} data={{ foo: 'foo', bar: 'bar' }} />)
+    expect(screen.getByText('bar')).toBeInTheDocument()
+  })
 })

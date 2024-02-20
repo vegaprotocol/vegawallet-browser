6243b256f3906349c09453acfc4720ed5b6a7950
Dexter <dexter@vega.xyz>
2024-02-20 17:18:47 +0000

    feat: for conditional data table allow requiring multiple properties

--- frontend/components/data-table/conditional-data-table.tsx
+++ frontend/components/data-table/conditional-data-table.tsx
@@ -5,22 +5,31 @@ import { ReactNode } from 'react'
 import { DataTable } from './data-table'
 
 export interface RowConfig<T> {
-  render: (data: T) => [ReactNode, ReactNode]
-  prop: string
+  render: (property: any, properties: any, data: T) => [ReactNode, ReactNode]
+  prop: keyof T
+  props?: (keyof T)[]
 }
 
 export function ConditionalDataTable<T>({ items, data }: { items: RowConfig<T>[]; data: T }) {
   const filteredItems = items
-    .filter(({ prop }) => {
-      if (!has(data, prop)) {
+    .filter(({ prop, props }) => {
+      if (prop && !has(data, prop)) {
         return false
+      }
+      if (props) {
+        const hasAllProperties = props.every((p) => has(data, p))
+        if (!hasAllProperties) {
+          return false
+        }
       }
       const value = get(data, prop)
       // Exclude 0 because 0 could be permitted for certain values
       return value !== undefined && value !== null && value !== ''
     })
-    .map(({ render }) => {
-      return render(data)
+    .map(({ render, prop, props }) => {
+      const property = get(data, prop)
+      const properties = Object.fromEntries(props?.map((p) => [p, get(data, p)]) || [])
+      return render(property, properties, data)
     })
   return <DataTable items={filteredItems} />
 }

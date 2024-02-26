0bf977bb5b6d3c03f14eb86634e83c45b41bdf36
Dexter <dexter@vega.xyz>
2024-02-26 23:37:08 +0300

    test: add delete wallet warning

--- frontend/routes/auth/settings/home/sections/delete-wallet-section/delete-wallet.spec.tsx
+++ frontend/routes/auth/settings/home/sections/delete-wallet-section/delete-wallet.spec.tsx
@@ -1,11 +1,17 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
-import { DeleteWallet } from './delete-wallet'
+import { DeleteWallet, locators } from './delete-wallet'
+
+jest.mock('./delete-wallet-warning', () => ({
+  DeleteWalletWarning: () => <div data-testid="delete-wallet-warning" />
+}))
 
 describe('DeleteWallet', () => {
-  it('should render the component', () => {
+  it('should render trigger, title and component', async () => {
     render(<DeleteWallet />)
-    const form = screen.getByRole('form')
-    expect(form).toBeInTheDocument()
+    expect(screen.getByTestId(locators.deleteWalletTrigger)).toBeInTheDocument()
+    fireEvent.click(screen.getByTestId(locators.deleteWalletTrigger))
+    await screen.findByTestId('delete-wallet-warning')
+    expect(screen.getByTestId(locators.deleteWalletTitle)).toHaveTextContent('Delete Wallet')
   })
 })

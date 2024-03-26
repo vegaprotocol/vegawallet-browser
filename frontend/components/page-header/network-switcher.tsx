5b4bc664387bdb7fffebf03aac09ad9d9387b8e0
Dexter <dexter@vega.xyz>
2024-03-26 11:28:12 +0530

    chore: build out a warning for the network switcher

--- frontend/components/page-header/network-switcher.tsx
+++ frontend/components/page-header/network-switcher.tsx
@@ -1,7 +1,12 @@
+import { getIntentBackground, Intent, Tooltip } from '@vegaprotocol/ui-toolkit'
+import classNames from 'classnames'
+
 import { useJsonRpcClient } from '@/contexts/json-rpc/json-rpc-context'
 import { useNetwork } from '@/contexts/network/network-context'
+import { useConnectionStore } from '@/stores/connections'
 import { useGlobalsStore } from '@/stores/globals'
 import { useNetworksStore } from '@/stores/networks-store'
+import { useTabStore } from '@/stores/tab-store'
 
 import { Dropdown } from '../dropdown'
 import { Header } from '../header'
@@ -51,16 +56,73 @@ const NetworkDropdown = () => {
   )
 }
 
+const Indicator = ({ intent }: { intent: Intent }) => {
+  const background = getIntentBackground(intent)
+  return (
+    <div
+      className={classNames(
+        'border-1 border-vega-dark-200 border inline-block w-3 h-3 mt-1 mr-2 rounded-full text-black',
+        background
+      )}
+      data-testid="indicator"
+    />
+  )
+}
+
+const IndicatorTooltip = ({ intent, description }: { description: string; intent: Intent }) => {
+  return (
+    <Tooltip description={description}>
+      <span>
+        <Indicator intent={intent} />
+      </span>
+    </Tooltip>
+  )
+}
+
+const NetworkWarning = () => {
+  const { network } = useNetwork()
+  const { currentTab } = useTabStore((state) => ({
+    currentTab: state.currentTab
+  }))
+  const { connections, loading } = useConnectionStore((state) => ({
+    connections: state.connections,
+    loading: state.loading
+  }))
+  if (loading) return null
+  if (currentTab?.url && connections.length > 0) {
+    const origin = new URL(currentTab.url).origin
+    const connection = connections.find((c) => c.origin === origin)
+    if (connection) {
+      const chainId = connection?.chainId
+      return network.chainId === chainId ? (
+        <IndicatorTooltip intent={Intent.Success} description={`You are currently connected to ${origin}.`} />
+      ) : (
+        <IndicatorTooltip
+          intent={Intent.Warning}
+          description={`You are connected to ${origin}, however you are currently viewing data for a different network. To view data for the network you are connected to select a different network in the network switcher.`}
+        />
+      )
+    }
+    return <IndicatorTooltip intent={Intent.None} description={`You are not currently connected to ${origin}.`} />
+  }
+  return <IndicatorTooltip intent={Intent.None} description={`You are not currently connected to any sites.`} />
+}
+
 export const NetworkSwitcher = () => {
   const { interactionMode, network } = useNetwork()
 
   return (
-    <div
-      data-testid={locators.networkSwitcher}
-      className="flex flex-col justify-center border rounded-md text-sm px-2 h-6"
-      style={{ borderColor: network.secondaryColor, color: network.secondaryColor }}
-    >
-      {interactionMode ? <div>{network.name}</div> : <NetworkDropdown />}
+    <div className="flex flex-row">
+      <div className="flex flex-col justify-center">
+        <NetworkWarning />
+      </div>
+      <div
+        data-testid={locators.networkSwitcher}
+        className="flex flex-col justify-center border rounded-md text-sm px-2 h-6"
+        style={{ borderColor: network.secondaryColor, color: network.secondaryColor }}
+      >
+        {interactionMode ? <div>{network.name}</div> : <NetworkDropdown />}
+      </div>
     </div>
   )
 }

877e97e4982c7ecc55ffab72b4976ea2db56e99d
Dexter <dexter@vega.xyz>
2024-03-26 14:24:58 +0530

    feat: build transaction modal footer

--- frontend/components/modals/transaction-modal/transaction-modal.tsx
+++ frontend/components/modals/transaction-modal/transaction-modal.tsx
@@ -1,6 +1,11 @@
-import { Button } from '@vegaprotocol/ui-toolkit'
+import { Button, Checkbox } from '@vegaprotocol/ui-toolkit'
+import { useState } from 'react'
 import ReactTimeAgo from 'react-time-ago'
 
+import { useJsonRpcClient } from '@/contexts/json-rpc/json-rpc-context'
+import { RpcMethods } from '@/lib/client-rpc-methods'
+import { TransactionMessage } from '@/lib/transactions'
+import { useConnectionStore } from '@/stores/connections'
 import { useInteractionStore } from '@/stores/interaction-store'
 
 import { PageHeader } from '../../page-header'
@@ -14,6 +19,64 @@ export const locators = {
   transactionTimeAgo: 'transaction-time-ago',
   transactionModalDenyButton: 'transaction-deny-button',
   transactionModalApproveButton: 'transaction-approve-button'
+}
+
+const TransactionModalFooter = ({
+  handleTransactionDecision,
+  details
+}: {
+  handleTransactionDecision: (decision: boolean) => void
+  details: TransactionMessage
+}) => {
+  const { request } = useJsonRpcClient()
+  const { connections } = useConnectionStore((state) => ({
+    connections: state.connections
+  }))
+  const connection = connections.find((c) => c.origin === details?.origin)
+  if (!connection) throw new Error(`Could not find connection with origin ${details.origin}`)
+  const [autoConsent, setAutoConsent] = useState(connection.autoConsent)
+
+  const handleDecision = async (decision: boolean) => {
+    handleTransactionDecision(decision)
+    if (connection && autoConsent !== connection.autoConsent) {
+      await request(RpcMethods.UpdateConnection, {
+        origin: connection.origin,
+        autoConsent
+      })
+    }
+  }
+
+  return (
+    <div className="fixed bottom-0 py-4 bg-black z-[15] px-5 border-t border-vega-dark-200 w-full">
+      <div className="mt-2 grid grid-cols-[1fr_1fr] justify-between gap-4">
+        <Button data-testid={locators.transactionModalDenyButton} onClick={() => handleDecision(false)}>
+          Reject
+        </Button>
+        <Button
+          data-testid={locators.transactionModalApproveButton}
+          variant="primary"
+          onClick={() => handleDecision(true)}
+        >
+          Confirm
+        </Button>
+      </div>
+      <div>
+        <Checkbox
+          label={
+            <span className="text-xs">
+              Allow this site to automatically approve order and vote transactions. This can be turned off in
+              "Connections".
+            </span>
+          }
+          checked={autoConsent}
+          onCheckedChange={() => {
+            setAutoConsent(!autoConsent)
+          }}
+          name={'autoConsent'}
+        />
+      </div>
+    </div>
+  )
 }
 
 export const TransactionModal = () => {
@@ -42,18 +105,7 @@ export const TransactionModal = () => {
           </div>
         </section>
       </Splash>
-      <div className="fixed bottom-0 grid grid-cols-[1fr_1fr] justify-between gap-4 py-4 bg-black z-[15] px-5 border-t border-vega-dark-200 w-full">
-        <Button data-testid={locators.transactionModalDenyButton} onClick={() => handleTransactionDecision(false)}>
-          Reject
-        </Button>
-        <Button
-          data-testid={locators.transactionModalApproveButton}
-          variant="primary"
-          onClick={() => handleTransactionDecision(true)}
-        >
-          Confirm
-        </Button>
-      </div>
+      <TransactionModalFooter handleTransactionDecision={handleTransactionDecision} details={details} />
     </>
   )
 }

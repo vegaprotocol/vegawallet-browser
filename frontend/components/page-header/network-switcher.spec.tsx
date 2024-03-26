0ebc5151a3b80129bd692e88a5a33d6b5798e61e
Dexter <dexter@vega.xyz>
2024-03-26 11:52:56 +0530

    test: split out dropdown and switcher tests into their own files

--- frontend/components/page-header/network-switcher.spec.tsx
+++ frontend/components/page-header/network-switcher.spec.tsx
@@ -1,14 +1,12 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import { useJsonRpcClient } from '@/contexts/json-rpc/json-rpc-context'
 import { MockNetworkProvider } from '@/contexts/network/mock-network-provider'
-import { useGlobalsStore } from '@/stores/globals'
 import { useNetworksStore } from '@/stores/networks-store'
 import { mockStore } from '@/test-helpers/mock-store'
 
 import { devnet, fairground, testingNetwork } from '../../../config/well-known-networks'
-import { NetworkListProperties } from '../networks-list'
-import { locators, NetworkSwitcher } from './network-switcher'
+import { NetworkSwitcher } from './network-switcher'
 
 jest.mock('@/stores/globals')
 jest.mock('@/stores/networks-store')
@@ -17,16 +15,12 @@ jest.mock('@/contexts/json-rpc/json-rpc-context', () => ({
   useJsonRpcClient: jest.fn()
 }))
 
-jest.mock('../networks-list', () => ({
-  NetworksList: (properties: NetworkListProperties) => (
-    <div data-testid="networks-list">
-      {properties.networks.map((n) => (
-        <button key={n.id} onClick={() => properties.onClick?.(n)} data-testid={n.id}>
-          {n.name}
-        </button>
-      ))}
-    </div>
-  )
+jest.mock('./network-dropdown', () => ({
+  NetworkDropdown: () => <div data-testid="network-dropdown" />
+}))
+
+jest.mock('./network-indicator', () => ({
+  NetworkIndicator: () => <div data-testid="network-indicator" />
 }))
 
 const renderComponent = (interactionMode?: boolean) => {
@@ -37,10 +31,9 @@ const renderComponent = (interactionMode?: boolean) => {
   )
 }
 
-const mockStores = ({ showHiddenNetworks = false } = {}) => {
+const mockStores = () => {
   const loadGlobals = jest.fn()
   const setSelectedNetwork = jest.fn()
-  mockStore(useGlobalsStore, { loadGlobals: loadGlobals, globals: { settings: { showHiddenNetworks } } })
   mockStore(useNetworksStore, {
     networks: [testingNetwork, fairground, devnet],
     selectedNetwork: testingNetwork,
@@ -53,31 +46,12 @@ const mockStores = ({ showHiddenNetworks = false } = {}) => {
 }
 
 describe('NetworkSwitcher', () => {
-  it('renders message explaining network selector and networks list', async () => {
+  it('renders network indicator and network dropdown', async () => {
     mockStores()
-    const mockRequest = jest.fn()
-    ;(useJsonRpcClient as unknown as jest.Mock).mockReturnValue({ request: mockRequest })
     renderComponent()
-    fireEvent.click(screen.getByTestId(locators.networkSwitcherCurrentNetwork))
-    await screen.findByTestId('networks-list')
-    expect(screen.getByTestId(locators.networkSwitcherMessage)).toHaveTextContent(
-      'Your selected network is for display purposes only, you can connect and place transactions on any configured network regardless of what network you have selected.'
-    )
-    expect(screen.getByTestId('networks-list')).toBeInTheDocument()
+    expect(screen.getByTestId('network-indicator')).toBeInTheDocument()
+    expect(screen.getByTestId('network-dropdown')).toBeInTheDocument()
   })
-
-  it('sets selected network and reloads the globals on network change', async () => {
-    const { setSelectedNetwork, loadGlobals } = mockStores()
-    const mockRequest = jest.fn()
-    ;(useJsonRpcClient as unknown as jest.Mock).mockReturnValue({ request: mockRequest })
-    renderComponent()
-    fireEvent.click(screen.getByTestId(locators.networkSwitcherCurrentNetwork))
-    await screen.findByTestId('networks-list')
-    fireEvent.click(screen.getByTestId(fairground.id))
-    await waitFor(() => expect(loadGlobals).toHaveBeenCalledTimes(1))
-    expect(setSelectedNetwork).toHaveBeenCalledTimes(1)
-  })
-
   it('does not render drop down and only renders name in interaction mode', async () => {
     // 1142-NWSW-002 Prevents switching displayed network when connecting
     // 1142-NWSW-005 Prevents switching displayed network when transacting
@@ -85,21 +59,7 @@ describe('NetworkSwitcher', () => {
     const mockRequest = jest.fn()
     ;(useJsonRpcClient as unknown as jest.Mock).mockReturnValue({ request: mockRequest })
     renderComponent(true)
-    expect(screen.queryByTestId(locators.networkSwitcherCurrentNetwork)).not.toBeInTheDocument()
+    expect(screen.queryByTestId('network-dropdown')).not.toBeInTheDocument()
     expect(screen.getByText(testingNetwork.name)).toBeInTheDocument()
-  })
-
-  it('renders development networks if setting is set', async () => {
-    // 1142-NWSW-009 Shows hidden networks when setting is turned on
-    // 1142-NWSW-010 Hides hidden networks when setting is turned off
-    mockStores({
-      showHiddenNetworks: true
-    })
-    const mockRequest = jest.fn()
-    ;(useJsonRpcClient as unknown as jest.Mock).mockReturnValue({ request: mockRequest })
-    renderComponent()
-    fireEvent.click(screen.getByTestId(locators.networkSwitcherCurrentNetwork))
-    await screen.findByTestId('networks-list')
-    expect(screen.getByText(devnet.name)).toBeInTheDocument()
   })
 })

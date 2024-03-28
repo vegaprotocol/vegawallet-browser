ca842d27f719dcfcf5515a41d04d28cdfb60ff8d
Dexter <dexter@vega.xyz>
2024-03-28 16:15:55 +0530

    chore: render basic error

--- frontend/routes/auth/transactions/details/details.tsx
+++ frontend/routes/auth/transactions/details/details.tsx
@@ -1,10 +1,18 @@
 import { useParams } from 'react-router-dom'
 
+import { TransactionSwitch } from '@/components/receipts'
+import { useTransactionsStore } from '@/stores/transactions-store'
+
 export const TransactionDetails = () => {
   const { id } = useParams<{ id: string }>()
+  const { transactions } = useTransactionsStore((state) => ({
+    transactions: state.transactions
+  }))
+  const transaction = transactions.find((tx) => tx.id === id)
   return (
     <div>
       <p className="text-sm">Transaction details</p>
+      <TransactionSwitch transaction={transaction} />
     </div>
   )
 }

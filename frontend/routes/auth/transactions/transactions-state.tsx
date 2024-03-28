a60e04c91d91cb51184e862694d4185dc8c733cf
Dexter <dexter@vega.xyz>
2024-03-28 17:05:25 +0530

    chore: generalise transaction state

---
+++ frontend/routes/auth/transactions/transactions-state.tsx
@@ -0,0 +1,12 @@
+import { TransactionState } from '@/types/backend'
+
+const TRANSACTION_STATE_COLOR = {
+  Confirmed: 'text-vega-blue-500',
+  Rejected: 'text-vega-dark-300',
+  Error: 'text-vega-red-500'
+}
+
+export const VegaTransactionState = ({ state }: { state: TransactionState }) => {
+  const color = TRANSACTION_STATE_COLOR[state]
+  return <span className={color}>{state}</span>
+}

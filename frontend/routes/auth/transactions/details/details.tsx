8aa72605c3d74641263af78b214191ead9430cf7
Dexter <dexter@vega.xyz>
2024-03-28 16:21:03 +0530

    feat:basic transaction page

--- frontend/routes/auth/transactions/details/details.tsx
+++ frontend/routes/auth/transactions/details/details.tsx
@@ -1,6 +1,9 @@
 import { useParams } from 'react-router-dom'
 
+import { getTitle } from '@/components/modals/transaction-modal/get-title'
+import { BasePage } from '@/components/pages/page'
 import { TransactionSwitch } from '@/components/receipts'
+import { FULL_ROUTES } from '@/routes/route-names'
 import { useTransactionsStore } from '@/stores/transactions-store'
 
 export const TransactionDetails = () => {
@@ -9,10 +12,17 @@ export const TransactionDetails = () => {
     transactions: state.transactions
   }))
   const transaction = transactions.find((tx) => tx.id === id)
+  if (!transaction) {
+    throw new Error(`Could not find transaction with id ${id}`)
+  }
+
   return (
-    <div>
-      <p className="text-sm">Transaction details</p>
-      <TransactionSwitch transaction={transaction} />
-    </div>
+    <BasePage
+      backLocation={FULL_ROUTES.transactions}
+      dataTestId={'transaction'}
+      title={getTitle(transaction.transaction)}
+    >
+      <TransactionSwitch transaction={transaction.transaction} />
+    </BasePage>
   )
 }

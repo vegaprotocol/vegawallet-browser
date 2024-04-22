714506965db341318665046da69dfca8ba7a54f4
Dexter <dexter@vega.xyz>
2024-04-22 10:32:21 +0700

    refactor: make transaction details more simple as it was doing too much

---
+++ frontend/routes/auth/transactions/details/transaction-data.tsx
@@ -0,0 +1,19 @@
+import { RawTransaction } from '@/components/modals/transaction-modal/raw-transaction'
+import { TransactionSwitch } from '@/components/receipts'
+import { SubHeader } from '@/components/sub-header'
+import { VegaSection } from '@/components/vega-section'
+import { StoredTransaction } from '@/types/backend'
+
+interface TransactionSectionProperties {
+  transaction: StoredTransaction
+}
+
+export const TransactionData = ({ transaction }: TransactionSectionProperties) => {
+  return (
+    <VegaSection>
+      <SubHeader content="Transaction data" />
+      <TransactionSwitch transaction={transaction.transaction} />
+      <RawTransaction transaction={transaction.transaction} />
+    </VegaSection>
+  )
+}

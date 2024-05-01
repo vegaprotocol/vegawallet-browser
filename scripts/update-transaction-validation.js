78408ff80aa0b3730fff9c9e8447a1ce44cd8955
Dexter <dexter@vega.xyz>
2024-05-01 09:30:09 +0100

    chore: add check transaction validation

--- scripts/update-transaction-validation.js
+++ scripts/update-transaction-validation.js
@@ -4,7 +4,7 @@ import fs from 'node:fs'
 const EXCLUDED_FIELDS = ['nonce', 'blockHeight']
 
 const inputs = messages.packages.vega.packages.commands.packages.v1.messages.find(({ name }) => name === 'InputData')
-console.log(inputs)
+
 const allowedMessages = inputs.fields
   .filter(({ name }) => !EXCLUDED_FIELDS.includes(name))
   .map((b) => ({
@@ -39,8 +39,36 @@ const sendTransactionValidation = {
   }
 }
 
+const checkTransactionValidation = {
+  type: 'object',
+  required: ['publicKey', 'transaction', 'origin'],
+  additionalProperties: false,
+  errorMessage: '`client.send_transaction` must only be given `publicKey`, `transaction` and `origin`',
+  properties: {
+    publicKey: {
+      type: 'string',
+      pattern: '^[0-9a-z]{64}$',
+      errorMessage: '`publicKey` must be a 64 character hex encoded publicKey'
+    },
+    origin: {
+      type: 'string'
+    },
+    transaction: {
+      type: 'object',
+      errorMessage: 'Unsupported transaction type',
+      oneOf: allowedMessages
+    }
+  }
+}
+
 fs.writeFileSync(
   'web-extension/schemas/client/send-transaction.json',
   JSON.stringify(sendTransactionValidation, null, 2) + '\n',
+  { encoding: 'utf-8' }
+)
+
+fs.writeFileSync(
+  'web-extension/schemas/admin/check-transaction.json',
+  JSON.stringify(checkTransactionValidation, null, 2) + '\n',
   { encoding: 'utf-8' }
 )

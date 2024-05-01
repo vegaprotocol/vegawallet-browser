f7e04ee5cae46a7c0c6078e02231de87416458e0
Dexter <dexter@vega.xyz>
2024-05-01 09:16:45 +0100

    chore: remove validations for checkTx

--- web-extension/validation/client/index.js
+++ web-extension/validation/client/index.js
@@ -4,4 +4,3 @@ export { default as getChainId } from "./get-chain-id.js";
 export { default as listKeys } from "./list-keys.js";
 export { default as sendTransaction } from "./send-transaction.js";
 export { default as isConnected } from "./is-connected.js";
-export { default as checkTransaction } from './check-transaction.js'

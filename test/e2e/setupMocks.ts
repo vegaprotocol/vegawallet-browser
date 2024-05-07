e3d4402e804e92afd896625fbc7c8de96dee002d
Dexter <dexter@vega.xyz>
2024-05-07 13:31:19 +0100

    test: add logging

--- test/e2e/setupMocks.ts
+++ test/e2e/setupMocks.ts
@@ -2,9 +2,11 @@ import test from '../../config/test'
 import { closeServerAndWait, server } from './helpers/wallet/http-server'
 
 beforeAll(() => {
+  console.log('Setting up server...')
   server.listen(test.test.mockPort)
 })
 
 afterAll(async () => {
+  console.log('Closing server...')
   await closeServerAndWait()
 })

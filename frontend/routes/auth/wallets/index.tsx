ac3a42470bcc1ec728f4f38049781bafb3d78bf2
Dexter <dexter@vega.xyz>
2024-05-13 14:03:28 +0100

    docs: remove todo

--- frontend/routes/auth/wallets/index.tsx
+++ frontend/routes/auth/wallets/index.tsx
@@ -6,8 +6,6 @@ export const WalletsRoot = () => {
   const { loading } = useWalletStore((store) => ({
     loading: store.loading
   }))
-  // TODO remove this now we are requiring wallets to be loaded at auth
-
   if (loading) return null
   return <Outlet />
 }

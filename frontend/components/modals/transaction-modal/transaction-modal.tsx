ccb3f525ff6e5abaafe5ce1c542c3fc28bfa776b
Dexter <dexter@vega.xyz>
2024-06-03 09:33:05 +0100

    feat: add notification for transaction modal

--- frontend/components/modals/transaction-modal/transaction-modal.tsx
+++ frontend/components/modals/transaction-modal/transaction-modal.tsx
@@ -4,6 +4,7 @@ import { useInteractionStore } from '@/stores/interaction-store'
 
 import { PageHeader } from '../../page-header'
 import { Splash } from '../../splash'
+import { TransactionNotAutoApproved } from './sections/auto-approval-notification'
 import { CheckTransaction } from './sections/check-transaction'
 import { EnrichedDetails } from './sections/enriched-details'
 import { RawTransaction } from './sections/raw-transaction'
@@ -27,6 +28,7 @@ export const TransactionModal = () => {
     <>
       <Splash data-testid={locators.transactionWrapper}>
         <PageHeader />
+
         <section className="pb-4 pt-2 px-5">
           <TransactionHeader
             origin={details.origin}
@@ -34,6 +36,7 @@ export const TransactionModal = () => {
             name={details.name}
             transaction={details.transaction}
           />
+          <TransactionNotAutoApproved details={details} />
           <CheckTransaction publicKey={details.publicKey} transaction={details.transaction} origin={details.origin} />
           <EnrichedDetails transaction={details.transaction} />
           <RawTransaction transaction={details.transaction} />

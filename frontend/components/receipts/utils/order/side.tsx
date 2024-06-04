cd32e5ab27dd06772f490746e57c50c61a16f8d0
Dexter <dexter@vega.xyz>
2024-06-04 11:46:52 +0100

    chore: add side component that figures out the side based on market type

--- frontend/components/receipts/utils/order/side.tsx
+++ frontend/components/receipts/utils/order/side.tsx
@@ -1,8 +1,26 @@
 import type { Side as vegaSide } from '@vegaprotocol/enums'
 
-import { processSide, SIDE_MAP } from '@/lib/enums'
+import { DERIVATIVE_SIDE_MAP, processSide, SPOT_SIDE_MAP } from '@/lib/enums'
+import { isSpotMarket } from '@/lib/markets'
+import { useMarketsStore } from '@/stores/markets-store'
 
-export const Side = ({ side }: { side: vegaSide }) => {
+export const DerivativeSide = ({ side }: { side: vegaSide }) => {
   const sideValue = processSide(side)
-  return <>{SIDE_MAP[sideValue]}</>
+  return <>{DERIVATIVE_SIDE_MAP[sideValue]}</>
+}
+
+export const SpotSide = ({ side }: { side: vegaSide }) => {
+  const sideValue = processSide(side)
+  return <>{SPOT_SIDE_MAP[sideValue]}</>
+}
+
+export const OrderSide = ({ side, marketId }: { side: vegaSide; marketId: string }) => {
+  const { loading, getMarketById } = useMarketsStore((state) => ({
+    loading: state.loading,
+    getMarketById: state.getMarketById
+  }))
+  const market = getMarketById(marketId)
+  if (loading || !market) return <DerivativeSide side={side} />
+  const isSpot = isSpotMarket(market)
+  return <>{isSpot ? <SpotSide side={side} /> : <DerivativeSide side={side} />}</>
 }

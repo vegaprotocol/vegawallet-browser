f15b870860efb25b756a3f2b28ee9fbe9b6ec85c
Dexter <dexter@vega.xyz>
2024-06-04 14:13:36 +0100

    chore: add some untility hooks to clean up the code

--- frontend/hooks/format-market-price/format-market-price.tsx
+++ frontend/hooks/format-market-price/format-market-price.tsx
@@ -1,16 +1,12 @@
 import { formatNumber, toBigNum } from '@vegaprotocol/utils'
 import get from 'lodash/get'
 
-import { useMarketsStore } from '@/stores/markets-store'
+import { useMarket } from '../use-market'
 
 export const useFormatMarketPrice = (marketId?: string, price?: string) => {
-  const { loading, getMarketById } = useMarketsStore((state) => ({
-    loading: state.loading,
-    getMarketById: state.getMarketById
-  }))
-  if (loading || !marketId || !price) return
-  const market = getMarketById(marketId)
+  const market = useMarket(marketId)
+  if (!market || !price) return
   const marketDecimal = Number(get(market, 'decimalPlaces'))
-  if (!market || !marketDecimal) throw new Error('Could not find market or decimalPlaces')
+  if (!marketDecimal) throw new Error('Could not find market or decimalPlaces')
   return formatNumber(toBigNum(price, marketDecimal), marketDecimal)
 }
--- frontend/hooks/format-size-amount/format-size-amount.tsx
+++ frontend/hooks/format-size-amount/format-size-amount.tsx
@@ -2,19 +2,16 @@ import { formatNumber, toBigNum } from '@vegaprotocol/utils'
 import get from 'lodash/get'
 
 import { HALF_MAX_POSITION_SIZE } from '@/lib/transactions'
-import { useMarketsStore } from '@/stores/markets-store'
+
+import { useMarket } from '../use-market'
 
 export const useFormatSizeAmount = (marketId?: string, size?: string) => {
-  const { loading, getMarketById } = useMarketsStore((state) => ({
-    loading: state.loading,
-    getMarketById: state.getMarketById
-  }))
-  if (loading || !marketId || !size) return
+  const market = useMarket(marketId)
+  if (!market || !size) return
   if (size === HALF_MAX_POSITION_SIZE) return 'Max'
-  const market = getMarketById(marketId)
   const positionDecimals = Number(get(market, 'positionDecimalPlaces'))
   const noPositionDecimals = !positionDecimals && positionDecimals !== 0
 
-  if (!market || noPositionDecimals) throw new Error('Could not find market or positionDecimals')
+  if (noPositionDecimals) throw new Error('Could not find market or positionDecimals')
   return formatNumber(toBigNum(size, positionDecimals), positionDecimals)
 }

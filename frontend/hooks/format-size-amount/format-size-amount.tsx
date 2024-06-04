aa50431d0e3e788367cda487f5ba35cdc275bce8
Dexter <dexter@vega.xyz>
2024-06-04 15:08:49 +0100

    test: revert changes for the format size helper

--- frontend/hooks/format-size-amount/format-size-amount.tsx
+++ frontend/hooks/format-size-amount/format-size-amount.tsx
@@ -2,16 +2,19 @@ import { formatNumber, toBigNum } from '@vegaprotocol/utils'
 import get from 'lodash/get'
 
 import { HALF_MAX_POSITION_SIZE } from '@/lib/transactions'
-
-import { useMarket } from '../use-market'
+import { useMarketsStore } from '@/stores/markets-store'
 
 export const useFormatSizeAmount = (marketId?: string, size?: string) => {
-  const market = useMarket(marketId)
-  if (!market || !size) return
+  const { loading, getMarketById } = useMarketsStore((state) => ({
+    loading: state.loading,
+    getMarketById: state.getMarketById
+  }))
+  if (loading || !marketId || !size) return
   if (size === HALF_MAX_POSITION_SIZE) return 'Max'
+  const market = getMarketById(marketId)
   const positionDecimals = Number(get(market, 'positionDecimalPlaces'))
   const noPositionDecimals = !positionDecimals && positionDecimals !== 0
 
-  if (noPositionDecimals) throw new Error('Could not find market or positionDecimals')
+  if (!market || noPositionDecimals) throw new Error('Could not find market or positionDecimals')
   return formatNumber(toBigNum(size, positionDecimals), positionDecimals)
 }

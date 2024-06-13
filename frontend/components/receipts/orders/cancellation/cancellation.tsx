9f7fe197d1c891d0b4049ea630e2a80ce7289ec6
Dexter <dexter@vega.xyz>
2024-06-13 14:05:55 +0100

    fix: cancellation notifications not working as intended

--- frontend/components/receipts/orders/cancellation/cancellation.tsx
+++ frontend/components/receipts/orders/cancellation/cancellation.tsx
@@ -1,32 +1,18 @@
 import { formatDateWithLocalTimezone } from '@vegaprotocol/utils'
-import { useEffect } from 'react'
 
-import { useJsonRpcClient } from '@/contexts/json-rpc/json-rpc-context'
-import { useNetwork } from '@/contexts/network/network-context'
+import { useOrder } from '@/hooks/order'
 import { nanoSecondsToMilliseconds } from '@/lib/utils'
-import { useOrdersStore } from '@/stores/orders-store'
 
 import type { ReceiptComponentProperties } from '../../receipts'
 import { ReceiptWrapper } from '../../utils/receipt-wrapper'
 import { CancellationView } from './cancellation-view'
 
 export const Cancellation = ({ transaction }: ReceiptComponentProperties) => {
-  const { network } = useNetwork()
-  const { error, getOrderById, lastUpdated, order, loading } = useOrdersStore((state) => ({
-    getOrderById: state.getOrderById,
-    lastUpdated: state.lastUpdated,
-    order: state.order,
-    error: state.error,
-    loading: state.loading
-  }))
   const cancellation = transaction.orderCancellation
   const { orderId } = cancellation
-  const { request } = useJsonRpcClient()
-
-  useEffect(() => {
-    getOrderById(request, orderId, network.id)
-  }, [orderId, getOrderById, request, network.id])
-  if (!order && !loading && lastUpdated) throw new Error('Order not found')
+  const { data: order, loading, error, lastUpdated } = useOrder(orderId)
+  console.log(order, loading, error, lastUpdated)
+  if (loading) return null
   return (
     <ReceiptWrapper errors={[error]}>
       <CancellationView cancellation={cancellation} order={order} />

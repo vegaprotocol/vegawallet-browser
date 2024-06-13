f6becfe34c78d5d07f96a04fc427793a0edc6b5f
Dexter <dexter@vega.xyz>
2024-06-13 14:05:55 +0100

    fix: cancellation notifications not working as intended

--- frontend/components/receipts/orders/cancellation/cancellation-notification.tsx
+++ frontend/components/receipts/orders/cancellation/cancellation-notification.tsx
@@ -7,23 +7,30 @@ export const locators = {
   cancellationNotification: 'cancellation-notification'
 }
 
-export const CancellationNotification = ({ orderId, marketId }: { orderId: string; marketId: string }) => {
+const AllOrdersInMarketNotification = ({ marketId }: { marketId: string }) => {
   const { getMarketById, loading } = useMarketsStore((state) => ({
     getMarketById: state.getMarketById,
     loading: state.loading
   }))
-  if (orderId || loading) return null
+  if (loading) return null
   const market = getMarketById(marketId)
+  return (
+    <Notification
+      intent={Intent.Warning}
+      message={`Cancel ALL open orders in ${
+        get(market, 'tradableInstrument.instrument.code') || truncateMiddle(marketId)
+      }`}
+    />
+  )
+}
+
+export const CancellationNotification = ({ orderId, marketId }: { orderId: string; marketId: string }) => {
+  if (orderId) return null
 
   return (
     <div className="mt-2" data-testid={locators.cancellationNotification}>
       {marketId ? (
-        <Notification
-          intent={Intent.Warning}
-          message={`Cancel ALL open orders in ${
-            get(market, 'tradableInstrument.instrument.code') || truncateMiddle(marketId)
-          }`}
-        />
+        <AllOrdersInMarketNotification marketId={marketId} />
       ) : (
         <Notification intent={Intent.Warning} message={'Cancel ALL orders in ALL markets'} />
       )}

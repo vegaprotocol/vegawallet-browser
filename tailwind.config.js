028e6f26e6c0c5a14b405ab103d57a0bc5082c9b
Dexter <dexter@vega.xyz>
2024-08-20 15:32:48 +0200

    fix: class scraping imports

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ import { theme, vegaCustomClasses } from '@vegaprotocol/tailwindcss-config'
 export default {
   content: [
     join(__dirname, 'frontend/**/*.{js,ts,jsx,tsx}'),
-    join(__dirname, './node_modules/@vegaprotocol/ui-toolkit/index.js')
+    join(__dirname, './node_modules/@vegaprotocol/ui-toolkit/index.cjs.js')
   ],
   darkMode: 'class',
   theme: {
